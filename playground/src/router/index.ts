c8c08d0067af558b8b830935b8628a6c564ea9be
Dominic Da Costa <dominic@thecollaboration.studio>
2023-09-28 14:42:34 +0200

    housekeeping

--- playground/src/router/index.ts
+++ playground/src/router/index.ts
@@ -38,11 +38,6 @@ const router = createRouter({
       name: "home",
       component: HomeView
     },
-    {
-      path: "/about",
-      name: "about",
-      component: () => import("@/views/AboutView.vue")
-    },
     // ---
     ...routes
   ]
