0d95d6937f3ddc7e76bcabe6a823707ed187998d
Dominic Da Costa <dominic@thecollaboration.studio>
2023-10-10 09:45:44 +0200

    make brand parallel

--- packages/flow/src/modules/brand/brand.machine.ts
+++ packages/flow/src/modules/brand/brand.machine.ts
@@ -8,6 +8,7 @@ import type { BrandContext, BrandEvent } from "./types.d";
 // --- utils
 import { useBrandParser } from "./utils";
 import { useTime } from "../../utils";
+import { set, unset } from "lodash-es";
 
 // --------------------------------------------------------
 
@@ -23,87 +24,230 @@ export default createMachine(
       //  we dont have a set type for this yet as its 100% dynamic from the API
       //  on fetch we will inject the data into the context
       // ---
-      error: null
+      error: {}
     } as BrandContext,
+
     states: {
       processing: {
         id: "processing",
-        initial: "organisation",
+        type: "parallel",
         states: {
           organisation: {
-            invoke: {
-              src: "fetchOrganisationConfig",
-              onDone: {
-                target: "settings",
-                actions: ["setOrganisation"]
+            initial: "fetching",
+            states: {
+              fetching: {
+                invoke: {
+                  src: "fetchOrganisationConfig",
+                  onDone: {
+                    target: "success",
+                    actions: ["setOrganisation"]
+                  },
+                  onError: {
+                    target: "error",
+                    actions: assign({
+                      error: (
+                        { error }: BrandContext,
+                        { data }: BrandEvent
+                      ) => {
+                        set(error, "organisation", data || "Unknown error");
+                        debugger;
+                        return error;
+                      }
+                    })
+                  }
+                }
               },
-              onError: {
-                target: "#error",
-                actions: ["setError"]
+              success: { type: "final" },
+              error: {
+                on: {
+                  RETRY: {
+                    target: "fetching",
+                    actions: assign({
+                      error: ({ error }: BrandContext) => {
+                        unset(error, "organisation");
+                        return error;
+                      }
+                    })
+                  }
+                }
               }
             }
           },
           settings: {
-            invoke: {
-              src: "fetchBrandSettings",
-              onDone: {
-                target: "config",
-                actions: ["setSettings"]
+            initial: "fetching",
+            states: {
+              fetching: {
+                invoke: {
+                  src: "fetchBrandSettings",
+                  onDone: {
+                    target: "success",
+                    actions: ["setSettings"]
+                  },
+                  onError: {
+                    target: "error",
+                    actions: assign({
+                      error: (
+                        { error }: BrandContext,
+                        { data }: BrandEvent
+                      ) => {
+                        debugger;
+                        set(error, "settings", data || "Unknown error");
+                        return error;
+                      }
+                    })
+                  }
+                }
               },
-              onError: {
-                target: "#error",
-                actions: ["setError"]
+              success: { type: "final" },
+              error: {
+                on: {
+                  RETRY: {
+                    target: "fetching",
+                    actions: assign({
+                      error: ({ error }: BrandContext) => {
+                        unset(error, "settings");
+                        return error;
+                      }
+                    })
+                  }
+                }
               }
             }
+
             // Brand Settings
             // /brand/settings?lang=en
           },
           config: {
-            invoke: {
-              src: "fetchBrandConfig",
-              onDone: {
-                target: "modules",
-                actions: ["setConfig"]
+            initial: "fetching",
+            states: {
+              fetching: {
+                invoke: {
+                  src: "fetchBrandConfig",
+                  onDone: {
+                    target: "success",
+                    actions: ["setConfig"]
+                  },
+                  onError: {
+                    target: "error",
+                    actions: assign({
+                      error: (
+                        { error }: BrandContext,
+                        { data }: BrandEvent
+                      ) => {
+                        set(error, "config", data || "Unknown error");
+                        return error;
+                      }
+                    })
+                  }
+                }
               },
-              onError: {
-                target: "#error",
-                actions: ["setError"]
+              success: { type: "final" },
+              error: {
+                on: {
+                  RETRY: {
+                    target: "fetching",
+                    actions: assign({
+                      error: ({ error }: BrandContext) => {
+                        unset(error, "config");
+                        return error;
+                      }
+                    })
+                  }
+                }
               }
             }
+
             // Brand values
             // /config/brand/values?keys=analytics.google.measurement_id,analytics.gtm.container_id,ui.basket.default_currency,billing.gateway.force_auto_payment_for_stored_details,billing.gateway.force_card_storage,ui.checkout.checkout_flow,ui.checkout.hide_promotions_field,ui.checkout.checkout_summary_color_stop1,ui.checkout.checkout_summary_color_stop2,ui.checkout.checkout_summary_contrast_mode,ui.client_area.allow_vault,ui.client_area.homepage,ui.client_area.hide_registration_forms,billing.gateway.allow_card_removal_replacement,ui.client_registration.require_phone,ui.basket.truncate_product_description,ui.client_area.show_catalog,tickets.support.support_pin_enabled,ui.client_area.disable_support_system,ui.client_area.page_after_login,ui.client_area.payment_term_descriptions,ui.client_area.enter_key_action,ui.client_area.price_before_discount_position&lang=en
           },
           modules: {
-            invoke: {
-              src: "fetchModules",
-              onDone: {
-                target: "currencies",
-                actions: ["setModules"]
+            initial: "fetching",
+            states: {
+              fetching: {
+                invoke: {
+                  src: "fetchModules",
+                  onDone: {
+                    target: "success",
+                    actions: ["setModules"]
+                  },
+                  onError: {
+                    target: "error",
+                    actions: assign({
+                      error: (
+                        { error }: BrandContext,
+                        { data }: BrandEvent
+                      ) => {
+                        set(error, "modules", data || "Unknown error");
+                        return error;
+                      }
+                    })
+                  }
+                }
               },
-              onError: {
-                target: "#error",
-                actions: ["setError"]
+              success: { type: "final" },
+              error: {
+                on: {
+                  RETRY: {
+                    target: "fetching",
+                    actions: assign({
+                      error: ({ error }: BrandContext) => {
+                        unset(error, "modules");
+                        return error;
+                      }
+                    })
+                  }
+                }
               }
             }
+
             // Modules
             // /org/modules?lang=en
           },
           currencies: {
-            invoke: {
-              src: "fetchCurrencies",
-              onDone: {
-                target: "#processed",
-                actions: ["setCurrencies"]
+            initial: "fetching",
+            states: {
+              fetching: {
+                invoke: {
+                  src: "fetchCurrencies",
+                  onDone: {
+                    target: "success",
+                    actions: ["setCurrencies"]
+                  },
+                  onError: {
+                    target: "error",
+                    actions: assign({
+                      error: (
+                        { error }: BrandContext,
+                        { data }: BrandEvent
+                      ) => {
+                        set(error, "currencies", data || "Unknown error");
+                        return error;
+                      }
+                    })
+                  }
+                }
               },
-              onError: {
-                target: "#error",
-                actions: ["setError"]
+              success: { type: "final" },
+              error: {
+                on: {
+                  RETRY: {
+                    target: "fetching",
+                    actions: assign({
+                      error: ({ error }: BrandContext) => {
+                        unset(error, "currencies");
+                        return error;
+                      }
+                    })
+                  }
+                }
               }
             }
+
             // Currencies
             // /currencies?limit=0&lang=en
           }
-        }
+        },
+        onDone: "#processed"
       },
       // Use a transient state to indicate a successful process
       // We have an imperceptible delay to allow the components to understand the process is complete
@@ -116,14 +260,6 @@ export default createMachine(
 
       complete: {
         type: "final"
-      },
-
-      // Handle errors
-      error: {
-        id: "error",
-        on: {
-          RETRY: { target: "processing", actions: ["clearError"] }
-        }
       }
     }
   },
@@ -143,13 +279,8 @@ export default createMachine(
       ),
       setCurrencies: assign({
         currencies: (_context: BrandContext, { data }: BrandEvent) => data
-      }),
+      })
       // ---
-      setError: assign({
-        error: (_context: BrandContext, { data }: BrandEvent) =>
-          data || "Unknown error"
-      }),
-      clearError: assign({ error: null })
     },
     guards: {},
     delays: {
--- packages/flow/src/modules/brand/brand.machine.typegen.ts
+++ packages/flow/src/modules/brand/brand.machine.typegen.ts
@@ -3,60 +3,40 @@
 export interface Typegen0 {
   "@@xstate/typegen": true;
   internalEvents: {
-    "done.invoke.brandManager.processing.config:invocation[0]": {
-      type: "done.invoke.brandManager.processing.config:invocation[0]";
+    "done.invoke.brandManager.processing.config.fetching:invocation[0]": {
+      type: "done.invoke.brandManager.processing.config.fetching:invocation[0]";
       data: unknown;
       __tip: "See the XState TS docs to learn how to strongly type this.";
     };
-    "done.invoke.brandManager.processing.currencies:invocation[0]": {
-      type: "done.invoke.brandManager.processing.currencies:invocation[0]";
+    "done.invoke.brandManager.processing.currencies.fetching:invocation[0]": {
+      type: "done.invoke.brandManager.processing.currencies.fetching:invocation[0]";
       data: unknown;
       __tip: "See the XState TS docs to learn how to strongly type this.";
     };
-    "done.invoke.brandManager.processing.modules:invocation[0]": {
-      type: "done.invoke.brandManager.processing.modules:invocation[0]";
+    "done.invoke.brandManager.processing.modules.fetching:invocation[0]": {
+      type: "done.invoke.brandManager.processing.modules.fetching:invocation[0]";
       data: unknown;
       __tip: "See the XState TS docs to learn how to strongly type this.";
     };
-    "done.invoke.brandManager.processing.organisation:invocation[0]": {
-      type: "done.invoke.brandManager.processing.organisation:invocation[0]";
+    "done.invoke.brandManager.processing.organisation.fetching:invocation[0]": {
+      type: "done.invoke.brandManager.processing.organisation.fetching:invocation[0]";
       data: unknown;
       __tip: "See the XState TS docs to learn how to strongly type this.";
     };
-    "done.invoke.brandManager.processing.settings:invocation[0]": {
-      type: "done.invoke.brandManager.processing.settings:invocation[0]";
+    "done.invoke.brandManager.processing.settings.fetching:invocation[0]": {
+      type: "done.invoke.brandManager.processing.settings.fetching:invocation[0]";
       data: unknown;
       __tip: "See the XState TS docs to learn how to strongly type this.";
-    };
-    "error.platform.brandManager.processing.config:invocation[0]": {
-      type: "error.platform.brandManager.processing.config:invocation[0]";
-      data: unknown;
-    };
-    "error.platform.brandManager.processing.currencies:invocation[0]": {
-      type: "error.platform.brandManager.processing.currencies:invocation[0]";
-      data: unknown;
-    };
-    "error.platform.brandManager.processing.modules:invocation[0]": {
-      type: "error.platform.brandManager.processing.modules:invocation[0]";
-      data: unknown;
-    };
-    "error.platform.brandManager.processing.organisation:invocation[0]": {
-      type: "error.platform.brandManager.processing.organisation:invocation[0]";
-      data: unknown;
-    };
-    "error.platform.brandManager.processing.settings:invocation[0]": {
-      type: "error.platform.brandManager.processing.settings:invocation[0]";
-      data: unknown;
     };
     "xstate.after(wait)#processed": { type: "xstate.after(wait)#processed" };
     "xstate.init": { type: "xstate.init" };
   };
   invokeSrcNameMap: {
-    fetchBrandConfig: "done.invoke.brandManager.processing.config:invocation[0]";
-    fetchBrandSettings: "done.invoke.brandManager.processing.settings:invocation[0]";
-    fetchCurrencies: "done.invoke.brandManager.processing.currencies:invocation[0]";
-    fetchModules: "done.invoke.brandManager.processing.modules:invocation[0]";
-    fetchOrganisationConfig: "done.invoke.brandManager.processing.organisation:invocation[0]";
+    fetchBrandConfig: "done.invoke.brandManager.processing.config.fetching:invocation[0]";
+    fetchBrandSettings: "done.invoke.brandManager.processing.settings.fetching:invocation[0]";
+    fetchCurrencies: "done.invoke.brandManager.processing.currencies.fetching:invocation[0]";
+    fetchModules: "done.invoke.brandManager.processing.modules.fetching:invocation[0]";
+    fetchOrganisationConfig: "done.invoke.brandManager.processing.organisation.fetching:invocation[0]";
   };
   missingImplementations: {
     actions: never;
@@ -70,47 +50,61 @@ export interface Typegen0 {
       | "fetchOrganisationConfig";
   };
   eventsCausingActions: {
-    clearError: "RETRY";
-    setConfig: "done.invoke.brandManager.processing.config:invocation[0]";
-    setCurrencies: "done.invoke.brandManager.processing.currencies:invocation[0]";
-    setError:
-      | "error.platform.brandManager.processing.config:invocation[0]"
-      | "error.platform.brandManager.processing.currencies:invocation[0]"
-      | "error.platform.brandManager.processing.modules:invocation[0]"
-      | "error.platform.brandManager.processing.organisation:invocation[0]"
-      | "error.platform.brandManager.processing.settings:invocation[0]";
-    setModules: "done.invoke.brandManager.processing.modules:invocation[0]";
-    setOrganisation: "done.invoke.brandManager.processing.organisation:invocation[0]";
-    setSettings: "done.invoke.brandManager.processing.settings:invocation[0]";
+    setConfig: "done.invoke.brandManager.processing.config.fetching:invocation[0]";
+    setCurrencies: "done.invoke.brandManager.processing.currencies.fetching:invocation[0]";
+    setModules: "done.invoke.brandManager.processing.modules.fetching:invocation[0]";
+    setOrganisation: "done.invoke.brandManager.processing.organisation.fetching:invocation[0]";
+    setSettings: "done.invoke.brandManager.processing.settings.fetching:invocation[0]";
   };
   eventsCausingDelays: {
-    wait: "done.invoke.brandManager.processing.currencies:invocation[0]";
+    wait: "done.state.processing";
   };
   eventsCausingGuards: {};
   eventsCausingServices: {
-    fetchBrandConfig: "done.invoke.brandManager.processing.settings:invocation[0]";
-    fetchBrandSettings: "done.invoke.brandManager.processing.organisation:invocation[0]";
-    fetchCurrencies: "done.invoke.brandManager.processing.modules:invocation[0]";
-    fetchModules: "done.invoke.brandManager.processing.config:invocation[0]";
+    fetchBrandConfig: "RETRY" | "xstate.init";
+    fetchBrandSettings: "RETRY" | "xstate.init";
+    fetchCurrencies: "RETRY" | "xstate.init";
+    fetchModules: "RETRY" | "xstate.init";
     fetchOrganisationConfig: "RETRY" | "xstate.init";
   };
   matchesStates:
     | "complete"
-    | "error"
     | "processed"
     | "processing"
     | "processing.config"
+    | "processing.config.error"
+    | "processing.config.fetching"
+    | "processing.config.success"
     | "processing.currencies"
+    | "processing.currencies.error"
+    | "processing.currencies.fetching"
+    | "processing.currencies.success"
     | "processing.modules"
+    | "processing.modules.error"
+    | "processing.modules.fetching"
+    | "processing.modules.success"
     | "processing.organisation"
+    | "processing.organisation.error"
+    | "processing.organisation.fetching"
+    | "processing.organisation.success"
     | "processing.settings"
+    | "processing.settings.error"
+    | "processing.settings.fetching"
+    | "processing.settings.success"
     | {
         processing?:
           | "config"
           | "currencies"
           | "modules"
           | "organisation"
-          | "settings";
+          | "settings"
+          | {
+              config?: "error" | "fetching" | "success";
+              currencies?: "error" | "fetching" | "success";
+              modules?: "error" | "fetching" | "success";
+              organisation?: "error" | "fetching" | "success";
+              settings?: "error" | "fetching" | "success";
+            };
       };
   tags: never;
 }
