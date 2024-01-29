c6bb0802f4efada60ecd1a1788c2115f034d30ce
Dominic Da Costa <dominic@thecollaboration.studio>
2024-01-29 09:51:47 +0200

    WIP client companies

--- packages/flow/src/modules/company/services.ts
+++ packages/flow/src/modules/company/services.ts
@@ -1,5 +1,4 @@
 // --- external
-import { Loader } from "@googlemaps/js-api-loader";
 
 // --- internal
 import { useApi } from "../api";
@@ -7,40 +6,24 @@ import { useSystem } from "../";
 import { useSession } from "../session";
 
 // --- utils
-import { usePlaceDetailParser } from "./utils";
 import { useValidation } from "../../utils";
-import { some, first, isEmpty, find, get } from "lodash-es";
+import { some, isEmpty, find, get } from "lodash-es";
 
 // --- types
 import type {
-  PlaceEvent,
-  PlaceContext,
-  PlacesEvents,
-  PlacesContext
+  CompanyEvent,
+  CompanyContext,
+  CompaniesEvents,
+  CompaniesContext
 } from "./types.d";
 
 // --------------------------------------------------------
-// ENUMS
-export const AddressTypes = [
-  { key: 1, value: "home" },
-  { key: 2, value: "office" },
-  { key: 3, value: "holiday" }
-];
-// --------------------------------------------------------
-
-const autocompleteApi = {};
-
-async function getClientId() {
-  const { getUser } = useSession();
-  const client = await getUser();
-  return client?.id;
-}
 
 // --------------------------------------------------------
 // SERVICE METHODS
 // Invoked by machines, providing context and event data
 
-// async function getEnums({ field }: PlaceContext, _event: PlaceEvent) {
+// async function getEnums({ field }: CompanyContext, _event: CompanyEvent) {
 //   const { getConfig } = useBrand();
 
 //   const brandPaymentPeriod: DefaultPaymentPeriod | any = await getConfig(
@@ -50,14 +33,15 @@ async function getClientId() {
 //   );
 // }
 
-async function load(_context: PlacesContext, { data }: PlacesEvents) {
+async function load(_context: CompaniesContext, { data }: CompaniesEvents) {
   const { get, useUrl } = useApi();
+  const { getUserId } = useSession();
 
-  const clientId = await getClientId();
+  const clientId = await getUserId();
 
   return get({
-    url: useUrl(`clients/${clientId}/addresses`, {
-      // with: ["country", "region"].join(),
+    url: useUrl(`clients/${clientId}/companies`, {
+      // with: [].join(),
       limit: 0
     }),
     withAccessToken: true,
@@ -66,199 +50,76 @@ async function load(_context: PlacesContext, { data }: PlacesEvents) {
   }).then(({ data }) => data);
 }
 
-async function add({ model }: PlacesContext, _event: PlacesEvents) {
-  const { post, useUrl } = useApi();
+// --------------------------------------------------------
 
-  const clientId = await getClientId();
+async function add({ model }: CompanyContext, _event: CompanyEvent) {
+  const { post, useUrl } = useApi();
+  const { getUserId } = useSession();
+
+  const clientId = await getUserId();
 
   return post({
-    url: useUrl(`clients/${clientId}/addresses`),
+    url: useUrl(`clients/${clientId}/companies`),
     data: model,
     withAccessToken: true
   }).then(({ data }) => data);
 }
 
-async function update({ model }: PlacesContext, _event: PlacesEvents) {
+async function update({ model }: CompanyContext, _event: CompanyEvent) {
   const { put, useUrl } = useApi();
+  const { getUserId } = useSession();
 
-  const clientId = await getClientId();
+  const clientId = await getUserId();
 
   return put({
-    url: useUrl(`clients/${clientId}/addresses/${model.id}`),
+    url: useUrl(`clients/${clientId}/companies/${model.id}`),
     data: model,
     withAccessToken: true
   }).then(({ data }) => data);
 }
 
-async function setDefault({ model }: PlacesContext, _event: PlacesEvents) {
+async function setDefault({ model }: CompanyContext, _event: CompanyEvent) {
   const { put, useUrl } = useApi();
+  const { getUserId } = useSession();
 
-  const clientId = await getClientId();
+  const clientId = await getUserId();
 
   return put({
-    url: useUrl(`clients/${clientId}/addresses/${model.id}`),
+    url: useUrl(`clients/${clientId}/companies/${model.id}`),
     data: { default: true },
     withAccessToken: true
   }).then(({ data }) => data);
 }
 
-async function remove({ model }: PlacesContext, _event: PlacesEvents) {
+async function remove({ model }: CompanyContext, _event: CompanyEvent) {
   const { del, useUrl } = useApi();
+  const { getUserId } = useSession();
 
-  const clientId = await getClientId();
+  const clientId = await getUserId();
 
   return del({
-    url: useUrl(`clients/${clientId}/addresses/${model.id}`),
+    url: useUrl(`clients/${clientId}/companies/${model.id}`),
     withAccessToken: true
   }).then(({ data }) => data);
 }
 
 // --------------------------------------------------------
 
-async function configureAutocomplete(
-  _context: PlaceContext,
-  _event: PlaceEvent
+async function validate(
+  { schema, model }: CompanyContext,
+  _event: CompanyEvent
 ) {
-  const loader = new Loader({
-    apiKey: import.meta.env.VITE_APP_GOOGLE_MAPS_API_KEY,
-    version: "weekly"
-  });
-
-  if (!isEmpty(autocompleteApi)) return Promise.resolve(true);
-
-  const api = await loader.importLibrary("places").catch(error => {
-    return Promise.reject(error);
-  });
-
-  autocompleteApi.places = new api.PlacesService(document.createElement("div"));
-  autocompleteApi.service = new api.AutocompleteService();
-  autocompleteApi.AutocompleteSessionToken = api.AutocompleteSessionToken;
-  autocompleteApi.sessionToken = new autocompleteApi.AutocompleteSessionToken();
-  autocompleteApi.statuses = api.PlacesServiceStatus;
-
-  return Promise.resolve(true);
-}
-
-async function search(_context: PlaceContext, { data }: PlaceEvent) {
-  return new Promise((resolve, reject) => {
-    if (!autocompleteApi?.service)
-      return reject("Autocomplete service not configured");
-
-    // if we dont have any data, then just return an empty array
-    if (!data?.search?.length) resolve([]);
-
-    autocompleteApi.service.getPlacePredictions(
-      {
-        input: data?.search,
-        sessionToken: autocompleteApi.sessionToken,
-        fields: ["address_components"]
-      },
-      (result, status) => {
-        if (status === autocompleteApi.statuses.OK) {
-          resolve(result);
-        } else if (status === autocompleteApi.statuses.ZERO_RESULTS) {
-          resolve([]);
-        } else {
-          reject(status);
-        }
-      }
-    );
-  });
-}
-
-async function loadPlaceDetails(_context: PlaceContext, { data }: PlaceEvent) {
-  return new Promise((resolve, reject) => {
-    if (!autocompleteApi?.service)
-      reject("Autocomplete service not configured");
-
-    // if we dont have any data, then just return an empty array
-    if (!data?.place?.length) reject(null);
-
-    autocompleteApi.places.getDetails(
-      {
-        placeId: data?.place,
-        sessionToken: autocompleteApi.sessionToken,
-        fields: ["address_components", "name"]
-      },
-      (result, status) => {
-        autocompleteApi.sessionToken =
-          new autocompleteApi.AutocompleteSessionToken();
-
-        console.log("loadPlaceDetails", "callback", { result, status });
-
-        if (status === autocompleteApi.statuses.OK) {
-          usePlaceDetailParser(result).then(place => {
-            resolve(place);
-          });
-        } else if (status === autocompleteApi.statuses.ZERO_RESULTS) {
-          resolve({});
-        } else {
-          reject(status);
-        }
-      }
-    );
-  });
-}
-
-async function loadConstants({ model }: PlaceContext, { data }: PlaceEvent) {
-  const { fetchCountries, fetchRegions, getDefaultCountry } = useSystem();
-
-  // we have to do this synchronously as we need the values to be available for the model
-  // these could/should be cached in the system machine, so theres no worry about performance
-
-  const countries = await fetchCountries();
-  const regions = await fetchRegions(model?.country_id);
-
-  const baseModel = {
-    ...model,
-    country_id: getDefaultCountry(),
-    type: first(AddressTypes)?.key
-  };
-
-  return new Promise((resolve, reject) => {
-    if (countries && regions) {
-      resolve({ countries, regions, baseModel });
-    } else {
-      reject("Failed to load countries and regions");
-    }
-  });
-}
-
-async function validate({ schema, model, regions }: PlaceContext, _event) {
-  // This NOT only validates the model,
-  // but also potentially updates the regions list based on the selected country ( if its changed )
-
   // ---
 
-  // NB:only do these checks if we have data
-  if (!isEmpty(model)) {
-    const { fetchRegions } = useSystem();
-
-    // lets check if the country has changed or the regions dont match
-    // if so, then we need to fetch the regions for the new country
-    if (!some(regions, ["country_id", model.country_id])) {
-      regions = await fetchRegions(model.country_id);
-    }
-
-    // now lets check our regions list to see if we have a match
-    // if so, then we need to update the model with the new region id
-    // otherwise the region_id is reset to null
-    const region = find(regions, ["id", model?.region_id]);
-    model.region_id = get(region, "id", null);
-  }
-  // ---
   // Now validate the model as per normal
   const { validate } = useValidation();
 
-  // because we are possibly mutating the regions list and the model/data
-  // we need to return the updated values as part of the promise
-  // regardless of whether the validation passes or fails
   return new Promise((resolve, reject) => {
     const errors = validate(schema, model);
     if (errors?.length) {
-      reject({ error: errors, model, regions });
+      reject(error);
     } else {
-      resolve({ model, regions });
+      resolve(model);
     }
   });
 }
@@ -267,11 +128,7 @@ async function validate({ schema, model, regions }: PlaceContext, _event) {
 // EXPORTS
 
 export default {
-  configureAutocomplete,
-  search,
-  loadPlaceDetails,
   load,
-  loadConstants,
   validate,
   setDefault,
   add,
