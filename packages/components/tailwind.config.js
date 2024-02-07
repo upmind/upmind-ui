ddef14b0f5e9d4fc5abf336701f2e9baedd576a9
Dominic Da Costa <dominic@thecollaboration.studio>
2024-02-07 14:53:26 +0200

    take II

--- packages/components/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/* global require, module*/
-module.exports = {
-  content: ["./src/**/*.{vue,js,ts}"],
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
-  daisyui: {
-    themes: ["light", "dark"]
-  }
-};
