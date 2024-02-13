1f7f5da451c3bb8e0ecf8a562ecaf4eea3fd3c15
Dominic Da Costa <dominic@thecollaboration.studio>
2024-02-13 17:28:31 +0200

    working payment dynamic schemas

--- packages/flow/src/modules/basket/payment/utils.ts
+++ packages/flow/src/modules/basket/payment/utils.ts
@@ -1,9 +1,20 @@
 // --- external
 
 // --- internal
-import { PaymentTypes } from "./services";
+import { PaymentTypes, GatewayTypes } from "./services";
+
 // --- utils
-import { get, map, set, reduce, defaultsDeep } from "lodash-es";
+import {
+  find,
+  get,
+  map,
+  set,
+  reduce,
+  defaultsDeep,
+  merge,
+  concat,
+  isEmpty
+} from "lodash-es";
 
 // --- types
 import type { IPaymentDetail, PaymentDetailsContext } from "./types.d";
@@ -13,7 +24,8 @@ import type { JsonSchema, UISchemaElement } from "@jsonforms/core";
 
 export const useSchema = ({
   payment_types,
-  gateways
+  gateways,
+  model
 }: PaymentDetailsContext) => {
   const schema = {
     type: "object",
@@ -55,11 +67,62 @@ export const useSchema = ({
     }
   };
 
-  return schema as JsonSchema;
+  // append our gateway specific schema
+  const gatewaySchema = getGatewaySchema(model?.gateway_id, gateways);
+  if (!isEmpty(gatewaySchema?.properties)) {
+    schema.properties = merge(schema.properties, gatewaySchema?.properties);
+    schema.required = concat(schema.required, gatewaySchema?.required);
+  }
+
+  return schema;
 };
 
-export const useUischema = ({ currency }: PaymentDetailsContext) => {
-  const schema = {
+const getGatewaySchema = (gateway_id, providers) => {
+  const gatewayProvider = find(providers, ["gateway_id", gateway_id]);
+  switch (gatewayProvider?.gateway?.type) {
+    case GatewayTypes.CARD:
+      return useCardSchema();
+    case GatewayTypes.BANK_TRANSFER:
+      return useBankTransferSchema();
+    case GatewayTypes.DIRECT_DEBIT:
+      return useDirectDebitCardSchema();
+    case GatewayTypes.SEPA:
+      return useSepaCardSchema();
+    case GatewayTypes.OFFLINE:
+      return useOfflineCardSchema();
+    case GatewayTypes.MOBILE:
+      return useMobileCardSchema();
+    case GatewayTypes.WALLET:
+      return useWalletCardSchema();
+
+    default:
+      return null;
+  }
+};
+
+const useCardSchema = () => ({
+  required: ["card_number", "expiry_date", "cvv"],
+  properties: {
+    card_number: { type: "string", title: "Card Number" },
+    expiry_date: { type: "string", title: "Expiry Date" },
+    cvv: { type: "string", title: "CVV" }
+  }
+});
+const useBankTransferSchema = () => {};
+const useDirectDebitCardSchema = () => {};
+const useSepaCardSchema = () => {};
+const useOfflineCardSchema = () => {};
+const useMobileCardSchema = () => {};
+const useWalletCardSchema = () => {};
+
+// ---
+
+export const useUischema = ({
+  currency,
+  model,
+  gateways
+}: PaymentDetailsContext) => {
+  const uischema = {
     type: "VerticalLayout",
     elements: [
       {
@@ -94,8 +157,82 @@ export const useUischema = ({ currency }: PaymentDetailsContext) => {
     ]
   };
 
-  return schema as UISchemaElement;
+  // append our gateway specific uischema
+  const gatewayUischema = getGatewayUischema(model?.gateway_id, gateways);
+  if (!isEmpty(gatewayUischema?.elements)) {
+    debugger;
+    uischema.elements = concat(uischema.elements, gatewayUischema?.elements);
+  }
+
+  return uischema as UISchemaElement;
 };
+
+const getGatewayUischema = (gateway_id, providers) => {
+  const gatewayProvider = find(providers, ["gateway_id", gateway_id]);
+
+  switch (gatewayProvider?.gateway?.type) {
+    case GatewayTypes.CARD:
+      return useCardUischema();
+    case GatewayTypes.BANK_TRANSFER:
+      return useBankTransferUischema();
+    case GatewayTypes.DIRECT_DEBIT:
+      return useDirectDebitCardUischema();
+    case GatewayTypes.SEPA:
+      return useSepaCardUischema();
+    case GatewayTypes.OFFLINE:
+      return useOfflineCardUischema();
+    case GatewayTypes.MOBILE:
+      return useMobileCardUischema();
+    case GatewayTypes.WALLET:
+      return useWalletCardUischema();
+
+    default:
+      return null;
+  }
+};
+
+const useCardUischema = () => ({
+  elements: [
+    {
+      type: "Group",
+      label: "Card Details",
+      options: {
+        styles: {
+          group: {
+            root: "group bg-base-300",
+            label: "group-label divider ",
+            item: "group-item"
+          }
+        }
+      },
+      elements: [
+        {
+          type: "Control",
+          scope: "#/properties/card_number",
+          options: {}
+        },
+        {
+          type: "Control",
+          scope: "#/properties/expiry_date",
+          options: {}
+        },
+        {
+          type: "Control",
+          scope: "#/properties/cvv",
+          options: {}
+        }
+      ]
+    }
+  ]
+});
+const useBankTransferUischema = () => [];
+const useDirectDebitCardUischema = () => [];
+const useSepaCardUischema = () => [];
+const useOfflineCardUischema = () => [];
+const useMobileCardUischema = () => [];
+const useWalletCardUischema = () => [];
+
+// ---
 
 export const useModelParser = (schema: JsonSchema, values: IPaymentDetail) => {
   const model = reduce(
