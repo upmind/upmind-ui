37bd3ad48ba67bf7f9eebbcaedc0aa520e97a127
Dominic Da Costa <dominic@thecollaboration.studio>
2024-02-20 16:08:00 +0200

    restored gateway to payment details form

--- packages/flow/src/modules/paymentDetails/details.machine.typegen.ts
+++ packages/flow/src/modules/paymentDetails/details.machine.typegen.ts
@@ -3,13 +3,8 @@
 export interface Typegen0 {
   "@@xstate/typegen": true;
   internalEvents: {
-    "done.invoke.paymentDetailsManager.checking.parsing:invocation[0]": {
-      type: "done.invoke.paymentDetailsManager.checking.parsing:invocation[0]";
-      data: unknown;
-      __tip: "See the XState TS docs to learn how to strongly type this.";
-    };
-    "done.invoke.paymentDetailsManager.checking.validating:invocation[0]": {
-      type: "done.invoke.paymentDetailsManager.checking.validating:invocation[0]";
+    "done.invoke.paymentDetailsManager.available.details.checking.parsing:invocation[0]": {
+      type: "done.invoke.paymentDetailsManager.available.details.checking.parsing:invocation[0]";
       data: unknown;
       __tip: "See the XState TS docs to learn how to strongly type this.";
     };
@@ -18,47 +13,29 @@ export interface Typegen0 {
       data: unknown;
       __tip: "See the XState TS docs to learn how to strongly type this.";
     };
-    "done.invoke.paymentDetailsManager.processing:invocation[0]": {
-      type: "done.invoke.paymentDetailsManager.processing:invocation[0]";
-      data: unknown;
-      __tip: "See the XState TS docs to learn how to strongly type this.";
-    };
-    "error.platform.paymentDetailsManager.checking.validating:invocation[0]": {
-      type: "error.platform.paymentDetailsManager.checking.validating:invocation[0]";
+    "error.platform.paymentDetailsManager.available.details.checking.validating:invocation[0]": {
+      type: "error.platform.paymentDetailsManager.available.details.checking.validating:invocation[0]";
       data: unknown;
     };
     "error.platform.paymentDetailsManager.loading:invocation[0]": {
       type: "error.platform.paymentDetailsManager.loading:invocation[0]";
       data: unknown;
     };
-    "error.platform.paymentDetailsManager.processing:invocation[0]": {
-      type: "error.platform.paymentDetailsManager.processing:invocation[0]";
-      data: unknown;
-    };
-    "error.platform.stripe:invocation[0]": {
-      type: "error.platform.stripe:invocation[0]";
-      data: unknown;
-    };
-    "xstate.after(wait)#processed": { type: "xstate.after(wait)#processed" };
     "xstate.init": { type: "xstate.init" };
   };
   invokeSrcNameMap: {
     load: "done.invoke.paymentDetailsManager.loading:invocation[0]";
-    parse: "done.invoke.paymentDetailsManager.checking.parsing:invocation[0]";
-    stripeMachine: "done.invoke.stripe:invocation[0]";
-    update: "done.invoke.paymentDetailsManager.processing:invocation[0]";
-    validate: "done.invoke.paymentDetailsManager.checking.validating:invocation[0]";
+    parse: "done.invoke.paymentDetailsManager.available.details.checking.parsing:invocation[0]";
+    validate: "done.invoke.paymentDetailsManager.available.details.checking.validating:invocation[0]";
   };
   missingImplementations: {
     actions: never;
     delays: never;
-    guards: "isStripe";
-    services: "load" | "parse" | "stripeMachine" | "update" | "validate";
+    guards: never;
+    services: "load" | "parse" | "validate";
   };
   eventsCausingActions: {
-    clearDirty: "done.invoke.paymentDetailsManager.processing:invocation[0]";
     clearError:
-      | "CHECKOUT"
       | "CLEAR"
       | "REFRESH"
       | "SET"
@@ -69,62 +46,51 @@ export interface Typegen0 {
     clearSchemas: "UNAUTHENTICATED";
     refreshContext: "REFRESH";
     setContext:
-      | "done.invoke.paymentDetailsManager.checking.parsing:invocation[0]"
+      | "done.invoke.paymentDetailsManager.available.details.checking.parsing:invocation[0]"
       | "done.invoke.paymentDetailsManager.loading:invocation[0]";
     setDirty: "CLEAR" | "SET";
     setError:
-      | "error.platform.paymentDetailsManager.checking.validating:invocation[0]"
-      | "error.platform.paymentDetailsManager.loading:invocation[0]"
-      | "error.platform.paymentDetailsManager.processing:invocation[0]"
-      | "error.platform.stripe:invocation[0]";
-    setFeedbackError:
-      | "error.platform.paymentDetailsManager.loading:invocation[0]"
-      | "error.platform.paymentDetailsManager.processing:invocation[0]"
-      | "error.platform.stripe:invocation[0]";
-    setFeedbackSuccess: "done.invoke.paymentDetailsManager.processing:invocation[0]";
-    setModel:
-      | "SET"
-      | "done.invoke.paymentDetailsManager.processing:invocation[0]";
+      | "error.platform.paymentDetailsManager.available.details.checking.validating:invocation[0]"
+      | "error.platform.paymentDetailsManager.loading:invocation[0]";
+    setFeedbackError: "error.platform.paymentDetailsManager.loading:invocation[0]";
+    setModel: "SET";
     setSchemas:
       | "REFRESH"
-      | "done.invoke.paymentDetailsManager.checking.parsing:invocation[0]"
+      | "done.invoke.paymentDetailsManager.available.details.checking.parsing:invocation[0]"
       | "done.invoke.paymentDetailsManager.loading:invocation[0]";
   };
-  eventsCausingDelays: {
-    wait: "done.invoke.paymentDetailsManager.processing:invocation[0]";
-  };
-  eventsCausingGuards: {
-    isStripe: "SELECT";
-    needsGateway: "done.invoke.paymentDetailsManager.checking.validating:invocation[0]";
-  };
+  eventsCausingDelays: {};
+  eventsCausingGuards: {};
   eventsCausingServices: {
     load: "CLEAR" | "REFRESH" | "SET" | "UNAUTHENTICATED" | "xstate.init";
     parse:
       | "CLEAR"
       | "SET"
       | "done.invoke.paymentDetailsManager.loading:invocation[0]";
-    stripeMachine: "SELECT";
-    update: "CHECKOUT";
-    validate: "done.invoke.paymentDetailsManager.checking.parsing:invocation[0]";
+    validate: "done.invoke.paymentDetailsManager.available.details.checking.parsing:invocation[0]";
   };
   matchesStates:
-    | "checking"
-    | "checking.parsing"
-    | "checking.validating"
+    | "available"
+    | "available.details"
+    | "available.details.checking"
+    | "available.details.checking.parsing"
+    | "available.details.checking.validating"
+    | "available.details.invalid"
+    | "available.details.valid"
     | "complete"
     | "error"
-    | "invalid"
-    | "invalid.details"
-    | "invalid.gateway"
-    | "invalid.gateway.idle"
-    | "invalid.gateway.stripe"
     | "loading"
-    | "processed"
-    | "processing"
     | "valid"
     | {
-        checking?: "parsing" | "validating";
-        invalid?: "details" | "gateway" | { gateway?: "idle" | "stripe" };
+        available?:
+          | "details"
+          | {
+              details?:
+                | "checking"
+                | "invalid"
+                | "valid"
+                | { checking?: "parsing" | "validating" };
+            };
       };
   tags: never;
 }
