6c5e44204f6c7a31acfaf5e7b9eadf7c2203d0a3
Dominic Da Costa <dominic@thecollaboration.studio>
2024-03-04 12:36:47 +0200

    refactored playground to be multiple playgrounds based on ui framework

---
+++ playgrounds/daisyui/src/modules/basket/components/CurrencyForm.vue
@@ -0,0 +1,66 @@
+<template>
+  <div
+    tabindex="0"
+    ref="form"
+    class="card card-bordered card-compact bg-base-100"
+    :class="[
+      meta.hasErrors ? 'border-error' : '',
+      meta.isComplete ? 'border-primary' : '',
+      !meta.isComplete ? 'border-warning' : ''
+    ]"
+  >
+    <div class="card-body">
+      <h3 class="text-inherit uppercase text-xl mt-2 mb-0 opacity-50">
+        Currency
+      </h3>
+
+      <upm-form-generator
+        tabindex="1"
+        :additional-errors="errors?.data"
+        :loading="meta.isLoading"
+        :model-value="model"
+        :processing="meta.isProcessing"
+        :schema="schema"
+        :uischema="uischema"
+        @reject="clear"
+        @resolve="update"
+        @update:modelValue="input"
+        class="mt-2 gap-4"
+      >
+        <template #actions="{ meta }">
+          <button
+            type="submit"
+            class="btn btn-outline btn-sm btn-primary border-none"
+            :disabled="!meta.isDirty || !meta.isValid || meta.isProcessing"
+            v-show="meta.isDirty"
+          >
+            Update currency
+          </button>
+        </template>
+      </upm-form-generator>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import UpmFormGenerator from "@/components/FormGenerator.vue";
+import { useBasketCurrency } from "@upmind/vue";
+
+export default defineComponent({
+  name: "UpmBasketCurrency",
+  components: { UpmFormGenerator },
+  inheritAttrs: true,
+  customOptions: {},
+  props: {
+    actor: {
+      type: Object, // xstate actor
+      required: true
+    }
+  },
+
+  setup(props) {
+    return useBasketCurrency(props.actor);
+  }
+});
+</script>
---
+++ playgrounds/daisyui/src/modules/product/components/Provisioning.vue
@@ -0,0 +1,67 @@
+<template>
+  <section class="provisioning" v-if="hasFields">
+    <h4 class="">Additional Information</h4>
+
+    <upm-form-generator
+      class="list-none p-4 border border-base-300 bg-base-200 bg-opacity-30 rounded-box"
+      :schema="fields"
+      :model-value="modelValue"
+      :additional-errors="additionalErrors"
+      @update:modelValue="doUpdate"
+      no-actions
+    />
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent, type PropType } from "vue";
+import UpmFormGenerator from "@/components/FormGenerator.vue";
+import { get, isEmpty } from "lodash-es";
+import type { ErrorObject } from "ajv";
+
+export default defineComponent({
+  name: "ProductConfigProvisioning",
+  components: { UpmFormGenerator },
+  inheritAttrs: true,
+  customOptions: {},
+  props: {
+    processing: {
+      type: Boolean,
+      default: false
+    },
+    fields: {
+      type: Object,
+      required: true
+    },
+    modelValue: {
+      type: Object,
+      required: true
+    },
+    additionalErrors: {
+      type: Array as PropType<
+        ErrorObject<string, Record<string, any>, unknown>[]
+      >,
+      default: () => []
+    }
+  },
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  setup(props, { emit }) {
+    function getValue(field) {
+      const value = get(props.modelValue, field, null);
+      return value;
+    }
+
+    const doUpdate = value => emit("update:modelValue", value);
+
+    return {
+      getValue,
+      doUpdate
+    };
+  },
+  computed: {
+    hasFields() {
+      return !isEmpty(this.fields?.properties);
+    }
+  }
+});
+</script>
