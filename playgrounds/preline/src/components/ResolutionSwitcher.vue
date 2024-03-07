9babbab2f88a03805d3cc3f387d361422d9e65bf
Dominic Da Costa <dominic@thecollaboration.studio>
2024-03-07 11:27:47 +0200

    device icons

--- playgrounds/preline/src/components/ResolutionSwitcher.vue
+++ playgrounds/preline/src/components/ResolutionSwitcher.vue
@@ -5,11 +5,11 @@
       type="button"
       class="inline-flex items-center gap-x-2.5 py-3 px-4 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
     >
-      <upm-icon name="palette" class="flex-shrink-0 size-4" />
+      <upm-icon name="devices" class="flex-shrink-0 size-6" />
       <span>{{ activeResolutionName }}</span>
     </button>
     <template #content>
-      <div :class="['rounded-xl', 'shadow-md']">
+      <div :class="['rounded-xl', 'shadow-md', 'my-4']">
         <ul class="flex flex-col sm:flex-row">
           <li
             :class="[
@@ -47,9 +47,9 @@
               @click.prevent="activeResolution = resolution"
             >
               <upm-icon
-                path="resolutions"
+                path="devices"
                 :name="resolution"
-                class="flex-shrink-0 size-12"
+                class="flex-shrink-0 size-8"
               />
 
               {{ startCase(resolution) }}
