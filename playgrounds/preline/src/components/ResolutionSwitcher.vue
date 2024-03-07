acc9dc05af5d6d964d7cac9a43c3897fe75444c1
Dominic Da Costa <dominic@thecollaboration.studio>
2024-03-07 12:42:02 +0200

    currentColor icons

--- playgrounds/preline/src/components/ResolutionSwitcher.vue
+++ playgrounds/preline/src/components/ResolutionSwitcher.vue
@@ -7,11 +7,11 @@
         v-for="resolution in resolutions"
         :key="resolution"
         type="button"
-        class="flex flex-col place-items-center gap-1 text-xs sm:text-sm text-neutral-800 hover:text-black font-medium rounded-lg py-2 px-3 dark:text-neutral-200 dark:hover:text-white dark:hs-tab-active:bg-neutral-800 dark:hs-tab-active:text-neutral-200"
+        class="flex flex-col place-items-center gap-1 text-xs sm:text-sm text-inherit hover:text-black font-medium rounded-lg py-2 px-3 dark:text-neutral-200 dark:hover:text-white dark:hs-tab-active:bg-neutral-800 dark:hs-tab-active:text-neutral-200"
         :class="{
           active: activeResolution === resolution,
           'bg-white': activeResolution === resolution,
-          'text-neutral-800': activeResolution === resolution,
+          'text-inherit': activeResolution === resolution,
           'shadow-sm': activeResolution === resolution,
         }"
         @click.prevent="activeResolution = resolution"
@@ -21,7 +21,7 @@
       </button>
       <!-- <button
         type="button"
-        class="hs-tab-active:bg-white hs-tab-active:text-neutral-800 hs-tab-active:shadow-sm text-xs sm:text-sm text-neutral-800 hover:text-black font-medium rounded-lg py-2 px-3 dark:text-neutral-200 dark:hover:text-white dark:hs-tab-active:bg-neutral-800 dark:hs-tab-active:text-neutral-200 active"
+        class="hs-tab-active:bg-white hs-tab-active:text-inherit hs-tab-active:shadow-sm text-xs sm:text-sm text-inherit hover:text-black font-medium rounded-lg py-2 px-3 dark:text-neutral-200 dark:hover:text-white dark:hs-tab-active:bg-neutral-800 dark:hs-tab-active:text-neutral-200 active"
         id="ctc-component-primary-bg-tab-html-item"
         data-hs-tab="#ctc-component-primary-bg-tab-html"
         aria-controls="ctc-component-primary-bg-tab-html"
@@ -49,7 +49,7 @@ export default defineComponent({
   },
   setup() {
     const activeResolution = inject("activeResolution");
-    const resolutions = ["mobile", "tablet", "desktop"];
+    const resolutions = ["desktop", "tablet", "mobile"];
 
     watch(
       () => activeResolution.value,
