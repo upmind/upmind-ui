ecee52dc47b0e343c152eab61bc57a53569de2e8
Dominic Da Costa <dominic@thecollaboration.studio>
2024-03-07 11:17:19 +0200

    added resolutions

--- playgrounds/preline/src/components/ResolutionSwitcher.vue
+++ playgrounds/preline/src/components/ResolutionSwitcher.vue
@@ -6,7 +6,7 @@
       class="inline-flex items-center gap-x-2.5 py-3 px-4 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
     >
       <upm-icon name="palette" class="flex-shrink-0 size-4" />
-      <span>{{ activeThemeName }}</span>
+      <span>{{ activeResolutionName }}</span>
     </button>
     <template #content>
       <div :class="['rounded-xl', 'shadow-md']">
@@ -34,25 +34,25 @@
               'sm:mt-0',
 
               'text-sm',
-              activeTheme == theme
+              activeResolution == resolution
                 ? ['text-primary', 'hover:bg-white']
                 : ['text-gray-800', 'hover:bg-gray-50'],
             ]"
-            v-for="theme in themes"
-            :key="theme"
+            v-for="resolution in resolutions"
+            :key="resolution"
           >
             <button
               type="button"
               class="flex flex-col items-center place-content-center h-full gap-2.5 py-3 px-8 text-sm font-medium text-inherit rounded-lg"
-              @click.prevent="activeTheme = theme"
+              @click.prevent="activeResolution = resolution"
             >
               <upm-icon
-                path="themes"
-                :name="theme"
+                path="resolutions"
+                :name="resolution"
                 class="flex-shrink-0 size-12"
               />
 
-              {{ startCase(theme) }}
+              {{ startCase(resolution) }}
             </button>
           </li>
         </ul>
@@ -70,27 +70,29 @@ import UpmIcon from "@/components/Icon.vue";
 import { startCase } from "lodash-es";
 
 export default defineComponent({
-  name: "ThemeSwitcher",
+  name: "ResolutionSwitcher",
   components: {
     Popper,
     UpmIcon,
   },
   setup() {
-    const activeTheme = inject("activeTheme");
-    const themes = import.meta.env.VITE_THEMES.split(",");
+    const activeResolution = inject("activeResolution");
+    const resolutions = ["mobile", "tablet", "desktop"];
 
     watch(
-      () => activeTheme.value,
-      (theme, current) => {
+      () => activeResolution.value,
+      (resolution, current) => {
         debugger;
-        // TODO: implement theme change
+        // TODO: implement resolution change
       }
     );
 
     return {
-      themes,
-      activeTheme,
-      activeThemeName: computed(() => startCase(activeTheme.value || "simple")),
+      resolutions,
+      activeResolution,
+      activeResolutionName: computed(() =>
+        startCase(activeResolution.value || "mobile")
+      ),
       startCase,
     };
   },
