8a79015928ac1e051abf3d1f9898b33b09bed0b8
Dominic Da Costa <dominic@thecollaboration.studio>
2024-03-07 12:13:00 +0200

    resolution to tabs

--- playgrounds/preline/src/components/ResolutionSwitcher.vue
+++ playgrounds/preline/src/components/ResolutionSwitcher.vue
@@ -1,64 +1,35 @@
 <template>
-  <!-- Popover -->
-  <popper placement="left" :interactive="false">
-    <button
-      type="button"
-      class="inline-flex items-center gap-x-2.5 py-3 px-4 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-    >
-      <upm-icon name="devices" class="flex-shrink-0 size-6" />
-      <span>{{ activeResolutionName }}</span>
-    </button>
-    <template #content>
-      <div :class="['rounded-xl', 'shadow-md', 'my-4']">
-        <ul class="flex flex-col sm:flex-row">
-          <li
-            :class="[
-              'border',
-              'bg-white',
-              'border-gray-200',
-              'dark:bg-slate-900',
-              'dark:border-gray-700',
-              'dark:text-white',
-              'first:mt-0',
-              'first:rounded-t-lg',
-              'font-medium',
-              'gap-x-2.5',
-              'inline-flex',
-              'items-center',
-              'last:rounded-b-lg',
-              'sm:-ms-px',
-              'sm:first:rounded-es-lg',
-              'sm:first:rounded-se-none',
-              'sm:last:rounded-es-none',
-              'sm:last:rounded-se-lg',
-              'sm:mt-0',
-
-              'text-sm',
-              activeResolution == resolution
-                ? ['text-primary', 'hover:bg-white']
-                : ['text-gray-800', 'hover:bg-gray-50'],
-            ]"
-            v-for="resolution in resolutions"
-            :key="resolution"
-          >
-            <button
-              type="button"
-              class="flex flex-col items-center place-content-center h-full gap-2.5 py-3 px-8 text-sm font-medium text-inherit rounded-lg"
-              @click.prevent="activeResolution = resolution"
-            >
-              <upm-icon
-                path="devices"
-                :name="resolution"
-                class="flex-shrink-0 size-8"
-              />
-
-              {{ startCase(resolution) }}
-            </button>
-          </li>
-        </ul>
-      </div>
-    </template>
-  </popper>
+  <div
+    class="flex bg-neutral-100 hover:bg-neutral-200 rounded-xl transition p-1 dark:bg-neutral-700 dark:hover:bg-neutral-700/[.7]"
+  >
+    <nav class="flex space-x-1" aria-label="Tabs" role="tablist">
+      <button
+        v-for="resolution in resolutions"
+        :key="resolution"
+        type="button"
+        class="flex flex-col place-items-center gap-1 hs-tab-active:bg-white hs-tab-active:text-neutral-800 hs-tab-active:shadow-sm text-xs sm:text-sm text-neutral-800 hover:text-black font-medium rounded-lg py-2 px-3 dark:text-neutral-200 dark:hover:text-white dark:hs-tab-active:bg-neutral-800 dark:hs-tab-active:text-neutral-200"
+        :class="{ active: activeResolution === resolution }"
+        :id="`trigger-resolution-${resolution}`"
+        :data-hs-tab="`#resolution-${resolution}`"
+        :aria-controls="`resolution-${resolution}`"
+        role="tab"
+        @click.prevent="activeResolution = resolution"
+      >
+        <upm-icon path="devices" :name="resolution" class="size-5" />
+        <span class="text-xs sr-only">{{ startCase(resolution) }}</span>
+      </button>
+      <!-- <button
+        type="button"
+        class="hs-tab-active:bg-white hs-tab-active:text-neutral-800 hs-tab-active:shadow-sm text-xs sm:text-sm text-neutral-800 hover:text-black font-medium rounded-lg py-2 px-3 dark:text-neutral-200 dark:hover:text-white dark:hs-tab-active:bg-neutral-800 dark:hs-tab-active:text-neutral-200 active"
+        id="ctc-component-primary-bg-tab-html-item"
+        data-hs-tab="#ctc-component-primary-bg-tab-html"
+        aria-controls="ctc-component-primary-bg-tab-html"
+        role="tab"
+      >
+        HTML
+      </button> -->
+    </nav>
+  </div>
 
   <!-- End Popover -->
 </template>
