b9145600f58b9b622e4669efd4af99a817866cc0
Dominic Da Costa <dominic@thecollaboration.studio>
2024-04-04 12:48:06 +0200

    take II

--- playgrounds/checkout/src/assets/themes/retro/upwind.config.js
+++ playgrounds/checkout/src/assets/themes/retro/upwind.config.js
@@ -1,10 +1,12 @@
 export default {
   button: {
-    size: {
-      sm: ["py-1", "px-2", "text-sm"],
-      default: ["py-1", "px-2", "text-sm"],
-      lg: ["py-2", "px-4", "text-sm"],
-    },
     label: ["font-light", "uppercase"],
+    attributes: {
+      size: {
+        sm: ["py-1", "px-2", "text-sm"],
+        default: ["py-1", "px-2", "text-sm"],
+        lg: ["py-2", "px-4", "text-sm"],
+      },
+    },
   },
 };
