dd32c8f4be96eba65f49fbffeba16c0a47e03459
Dominic Da Costa <dominic@thecollaboration.studio>
2024-04-15 13:11:17 +0200

    fix style overrides using cva

--- packages/upwind/src/utils/useStyles.ts
+++ packages/upwind/src/utils/useStyles.ts
@@ -1,55 +1,52 @@
 // --- global
-import { unref, inject, computed } from "vue";
+import { unref, inject, computed, toRaw } from "vue";
 import { twMerge } from "tailwind-merge";
+import { cx } from "class-variance-authority";
 import type { VariantProps } from "class-variance-authority";
 
 // ---utils
 import {
-  merge,
   get,
-  map,
-  mapValues,
+  isEmpty,
   isFunction,
-  omitBy,
   isNil,
+  map,
+  omitBy,
+  reduce,
+  remove,
+  set,
 } from "lodash-es";
 
 // -----------------------------------------------------------------------------
 
-function generateComponentConfig(
-  component: string,
-  globalConfig: Record<string, Object>,
-  ...configs: Array<Record<string, Object>>
-) {
-  configs = map(configs, unref); // safety check
-  globalConfig = unref(globalConfig) || {};
-
-  // Check if weve been provided with config overrides, then merge the default config
-  // TODO: maybe add some intelligent merging here, to ensure we dont add contradictory styles, eg 2 bg-[colors]
-  const componentConfig = get(globalConfig, component, {});
-  const mergedConfig = merge({}, ...configs, componentConfig);
-  return mergedConfig;
-}
-
-function applyComponentStyles(
-  component: string,
-  styles: Record<string, Function>,
+function applyVariants(
+  configs: Array<Record<string, Function>>,
   context: Object = {}
 ) {
   // ----------------------------------------------
-  context = omitBy(unref(context), isNil);
+  //  NB: This works by getting ALL the unique keys from ALL of the provided configs
+  //      then we loop over each key
+  //      and get each variant for that key from each config
+  //      we then run the variant function with the context object
+  //      and merge the results into a single object with the CX helper
+  //      finally we us the twMerge helper to clean up the final object
 
-  return mapValues(styles, (variant, key) => {
-    if (!isFunction(variant)) return variant;
+  const configKeys = configs.map(Object.keys).flat();
 
-    console.log("applyComponentStyles", component, {
-      key,
-      context,
-      variant: twMerge(variant(context)),
-    });
+  return reduce(
+    configKeys,
+    (styles, key) => {
+      const variants = map(configs, config => get(config, key, {}));
+      const results = map(variants, variant => {
+        if (!isFunction(variant)) return variant;
+        return variant(context);
+      });
 
-    return twMerge(variant(context));
-  });
+      set(styles, key, twMerge(cx(...results)));
+      return styles;
+    },
+    {}
+  );
 }
 
 export const useStyles = (
@@ -59,12 +56,19 @@ export const useStyles = (
 ) => {
   // Check if weve been provided with style overrides, then merge the default styles with the overrides
   const globalConfig = inject("upwind", {});
+
   const styles = computed(() => {
-    return applyComponentStyles(
-      component,
-      generateComponentConfig(component, globalConfig, ...configs),
-      context
-    );
+    // add any component specific overrides from our injected global config
+    const componentConfig = get(toRaw(unref(globalConfig)), component);
+    configs.push(componentConfig);
+
+    // safety checks
+    const cleanContext = omitBy(unref(context), isNil);
+    const cleanConfigs = configs.map(unref);
+    remove(cleanConfigs, isEmpty);
+
+    // return the merged configs using the provided helper
+    return applyVariants(cleanConfigs, cleanContext);
   });
 
   return styles;
--- playgrounds/labs/src/assets/themes/retro/upwind.config.js
+++ playgrounds/labs/src/assets/themes/retro/upwind.config.js
@@ -1,20 +1,40 @@
+import { cva } from "class-variance-authority";
+// -----------------------------------------------------------------------------
+
 export default {
   button: {
-    label: "font-light uppercase",
-    attributes: {
-      size: {
-        default: "px-2 py-1 text-sm ",
-        sm: "px-2 py-1 text-sm",
-        lg: "px-4 py-2 text-sm",
+    label: cva("font-light uppercase"),
+    root: cva("", {
+      variants: {
+        size: {
+          sm: "px-2 py-1 text-xs",
+          md: "px-2 py-1 text-sm",
+          lg: "px-2 py-1 text-base",
+        },
       },
-    },
+      defaultVariants: {
+        size: "md",
+      },
+    }),
+    content: cva("", {
+      variants: {
+        size: {
+          sm: "px-1",
+          medium: "px-3",
+          lg: "px-5",
+        },
+      },
+      defaultVariants: {
+        size: "md",
+      },
+    }),
   },
-  form: {
-    content: {
-      loading: "scale-0 transform invisible opacity-0",
-    },
-    actions: {
-      loading: "invisible opacity-0",
-    },
-  },
+  // form: {
+  //   content: {
+  //     loading: "scale-0 transform invisible opacity-0",
+  //   },
+  //   actions: {
+  //     loading: "invisible opacity-0",
+  //   },
+  // },
 };
