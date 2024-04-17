d6ef560a357d0dcf60ec4c87c0d390a3e82c86dc
Dominic Da Costa <dominic@thecollaboration.studio>
2024-04-17 09:26:21 +0200

    match checkbox figma

--- packages/upwind/src/components/form/renderers/controls/boolean/config.cva.js
+++ packages/upwind/src/components/form/renderers/controls/boolean/config.cva.js
@@ -4,7 +4,7 @@ import { cva } from "class-variance-authority";
 export default {
   checkbox: {
     root: cva(
-      "border-base-300 group-focus-within:border-primary group-focus-within:ring-primary group inline-flex size-[1.5em] items-center justify-center  rounded-md border outline-none ring-0 group-focus-within:ring-4 group-focus-within:ring-opacity-10",
+      "border-base-300 group-focus-within:border-primary group-focus-within:ring-primary group box-content inline-flex size-4 items-center justify-center rounded border outline-none ring-0 group-focus-within:ring-4 group-focus-within:ring-opacity-10",
       {
         variants: {
           size: {
@@ -16,11 +16,11 @@ export default {
             true: "bg-base-100 ",
           },
           isChecked: {
-            true: "group-focus-within:border-primary group-focus-within:ring-primary group-focus-within:ring-4 group-focus-within:ring-opacity-10",
+            true: "bg-base-content border-base-content group-focus-within:border-primary group-focus-within:ring-primary text-base group-focus-within:ring-4 group-focus-within:ring-opacity-10",
           },
-          isValid: {
-            true: "border-success-300 group-focus-within:border-success group-focus-within:ring-success",
-          },
+          // isValid: {
+          //   true: "border-success-300 group-focus-within:border-success group-focus-within:ring-success",
+          // },
           isInvalid: {
             true: "border-error-300 group-focus-within:!border-error group-focus-within:!ring-error group-focus-within:!ring-opacity-10",
           },
