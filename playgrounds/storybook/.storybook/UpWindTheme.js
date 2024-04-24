b9d26545178da439abfddb7c4be42c1c880caca8
Dominic Da Costa <dominic@thecollaboration.studio>
2024-04-24 12:36:42 +0200

    storybook theming & housekeeping

---
+++ playgrounds/storybook/.storybook/UpWindTheme.js
@@ -0,0 +1,42 @@
+import { create } from "@storybook/theming/create";
+
+export default create({
+  base: "light",
+
+  // --- Branding
+  brandTitle: "Upmind Labs",
+  brandUrl: "https://upmind.com/",
+  brandImage: "https://upmind.com/assets/interface/upmind-logo.svg",
+  brandTarget: "_self",
+
+  // --- Typography
+  fontBase: '"Inter", sans-serif',
+  fontCode: '"Inconsolata", monospace',
+
+  // --- Colors
+  colorPrimary: "#2b4779",
+  colorSecondary: "#018ffd",
+
+  // UI
+  appBg: "#F4F6FB",
+  appContentBg: "#F4F6FB",
+  appPreviewBg: "#ffffff",
+  appBorderColor: "#CBD8EC",
+  appBorderRadius: 4,
+
+  // Text colors
+  textColor: "#2B4779",
+  textInverseColor: "#ffffff",
+
+  // Toolbar default and active colors
+  barTextColor: "#ffffff",
+  barSelectedColor: "#CBD8EC",
+  barHoverColor: "#CBD8EC",
+  barBg: "#2b4779",
+
+  // Form colors
+  inputBg: "#ffffff",
+  inputBorder: "#CBD8EC",
+  inputTextColor: "#2B4779",
+  inputBorderRadius: 4,
+});
