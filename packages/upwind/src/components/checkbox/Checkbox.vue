e6f7af10c28f392301dd9021128f9d9af0c126e7
Dominic Da Costa <dominic@thecollaboration.studio>
2024-04-24 16:48:04 +0200

    wip checkbox refactor to use new input wrapper

--- packages/upwind/src/components/checkbox/Checkbox.vue
+++ packages/upwind/src/components/checkbox/Checkbox.vue
@@ -1,39 +1,64 @@
 <template>
-  <span :class="styles.checkbox.root">
-    <input
-      v-bind="$attrs"
-      type="checkbox"
-      :checked="modelValue"
-      :class="styles.checkbox.input"
-      @blur="$emit('blur', $event)"
-      @change="$emit('update:modelValue', !modelValue)"
-      @focus="$emit('focus', $event)"
-      :aria-invalid="meta.isInvalid"
-    />
-    <upw-icon
-      :class="styles.checkbox.icon"
-      :icon="computedIcon"
-      v-if="computedIcon"
-    />
-  </span>
+  <upw-input
+    :id="id"
+    :label="label"
+    :description="description"
+    :errors="errors"
+    :size="size"
+    :append-avatar="appendAvatar"
+    :append-icon="appendIcon"
+    :append-text="appendText"
+    :prepend-avatar="prependAvatar"
+    :prepend-icon="prependIcon"
+    :prepend-text="prependText"
+    :feedback-icon="feedbackIcon"
+    :dirty="meta.isDirty"
+    :disabled="meta.isDisabled"
+    :visible="meta.isVisible"
+    :required="meta.isRequired"
+    :focused="meta.isFocused"
+    layout="flat"
+    variant="inline"
+  >
+    <span :class="styles.checkbox.root">
+      <input
+        v-bind="safeAttrs"
+        type="checkbox"
+        :disabled="disabled"
+        :checked="modelValue"
+        :class="styles.checkbox.input"
+        @blur="onBlur"
+        @change="onChange"
+        @focus="onFocus"
+        :aria-invalid="meta.isInvalid"
+      />
+      <upw-icon
+        :class="styles.checkbox.icon"
+        :icon="computedIcon"
+        v-if="computedIcon"
+      />
+    </span>
+  </upw-input>
 </template>
 
 <script lang="ts">
 // --- external
-import { defineComponent, computed, toRefs } from "vue";
+import { defineComponent, computed, ref } from "vue";
 
 // --- local
 import config from "./config.cva";
 
 // --- components
+import UpwInput from "../input/Input.vue";
 import UpwIcon from "../icon/Icon.vue";
 
 // --- utils
 import { useStyles } from "../../utils";
-import { isNil } from "lodash-es";
+import { isNil, isEmpty, omit } from "lodash-es";
 
 // --- types
 import type { PropType } from "vue";
+import type { InputProps } from "../input/types";
 import type { CheckboxProps } from "./types";
 
 // ----------------------------------------------
@@ -43,10 +68,30 @@ export default defineComponent({
   inheritAttrs: false,
   emits: ["update:modelValue", "focus", "blur"],
   components: {
+    UpwInput,
     UpwIcon,
   },
 
   props: {
+    id: { type: String },
+    label: { type: String },
+    description: { type: String },
+    errors: { type: String },
+    // ---
+    size: { type: String as PropType<InputProps["size"]>, default: null },
+    // ---
+    appendAvatar: { type: [Object, String] as PropType<IconProps["icon"]> },
+    appendIcon: { type: [Object, String] as PropType<IconProps["icon"]> },
+    appendText: { type: String },
+    // ---
+    prependAvatar: { type: [Object, String] as PropType<IconProps["icon"]> },
+    prependIcon: { type: [Object, String] as PropType<IconProps["icon"]> },
+    prependText: { type: String },
+    // ---
+    feedbackIcon: {
+      type: [Object, String] as PropType<IconProps["icon"]>,
+      default: "information-circle",
+    },
     checkedIcon: {
       type: [String, Object] as PropType<CheckboxProps["checkedIcon"]>,
       default: "check",
@@ -60,42 +105,31 @@ export default defineComponent({
       default: "subtract",
     },
     // ---
-    modelValue: {
-      type: Boolean,
-      default: null,
-    },
+    modelValue: { type: Boolean },
     // ---
-    invalid: {
-      type: Boolean,
-      default: null,
-    },
-    loading: {
-      type: Boolean,
-      default: false,
-    },
-    disabled: {
-      type: Boolean,
-      default: false,
-    },
-    // ---
-    size: {
-      type: String as PropType<CheckboxProps["size"]>,
-      default: null,
-    },
+    required: { type: Boolean },
+    visible: { type: Boolean, default: true },
+    disabled: { type: Boolean },
+    forceFocus: { type: Boolean },
     // --- Provide a way to add custom styles for a specific instance of the component
-    upwindConfig: {
-      type: [Array, Object],
-      default: null,
-    },
+    upwindConfig: { type: [Array, Object], default: null },
   },
 
-  setup(props) {
+  setup(props, { emit }) {
+    const focused = ref(false);
+
     const meta = computed(() => ({
-      isLoading: props.loading,
+      size: props.size,
+      // ---
+      isFocused: props.forceFocus || focused.value,
+      isDisabled: props.disabled,
+      isVisible: props.visible,
+      isRequired: props.required,
+      isDirty: !isEmpty(props.modelValue),
       isChecked: !!props.modelValue,
       isIndeterminate: isNil(props.modelValue),
-      isValid: !props.invalid && !isNil(props.modelValue),
-      isInvalid: props.invalid,
+      isInvalid: !isEmpty(props.errors),
+      isValid: isEmpty(props.errors) && !isNil(props.modelValue),
     }));
 
     const styles = useStyles("checkbox", meta, config, props.upwindConfig);
@@ -103,10 +137,24 @@ export default defineComponent({
     return {
       meta,
       styles,
+      onFocus: event => {
+        focused.value = true;
+        emit("focus", event);
+      },
+      onBlur: event => {
+        focused.value = false;
+        emit("blur", event);
+      },
+      onChange: event => {
+        emit("update:modelValue", event.target.value);
+      },
     };
   },
-
   computed: {
+    safeAttrs() {
+      // TODO: maybe whitelist input attributes
+      return omit(this.$attrs, ["layout", "variant"]);
+    },
     computedIcon() {
       return this.meta.isIndeterminate
         ? this.indeterminateIcon
