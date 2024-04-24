e600282fb87ba1c6122adf0c9b50bc8b12d2dc03
Dominic Da Costa <dominic@thecollaboration.studio>
2024-04-24 16:48:04 +0200

    wip checkbox refactor to use new input wrapper

--- packages/upwind/src/index.ts
+++ packages/upwind/src/index.ts
@@ -1,8 +1,13 @@
 export { default as UpwIcon } from "./components/icon/Icon.vue";
 export { default as UpwButton } from "./components/button/Button.vue";
 export { default as UpwSpinner } from "./components/spinner/Spinner.vue";
-export { default as UpwForm } from "./components/form/Form.vue";
 export { default as UpwDropdown } from "./components/dropdown/Dropdown.vue";
 export { default as UpwListbox } from "./components/listbox/Listbox.vue";
+export { default as UpwForm } from "./components/form/Form.vue";
+export { default as UpwInput } from "./components/input/Input.vue";
+export { default as UpwCheckbox } from "./components/checkbox/Checkbox.vue";
+export { default as UpwRadio } from "./components/radio/Radio.vue";
+export { default as UpwTextbox } from "./components/textbox/Textbox.vue";
+
 // ===
 export { useStyles } from "./utils/useStyles";
