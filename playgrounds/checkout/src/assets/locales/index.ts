30a89dd0070420d8d66fbbcf31394114152a1db4
Dominic Da Costa <dominic@thecollaboration.studio>
2024-05-01 10:03:42 +0200

    localazy integration

--- playgrounds/checkout/src/assets/locales/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as de } from "./de.json";
-export { default as en } from "./en.json";
-export { default as es } from "./es.json";
-export { default as fr } from "./fr.json";
-export { default as it } from "./it.json";
-export { default as pt } from "./pt.json";
-export { default as ru } from "./ru.json";
