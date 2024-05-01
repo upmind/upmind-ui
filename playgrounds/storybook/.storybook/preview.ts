ba40cb2786139d39cd6f86de61497c35c708e7e7
Dominic Da Costa <dominic@thecollaboration.studio>
2024-05-01 14:14:34 +0200

    moved taks form to storybook. addedi18n to storybook.

--- playgrounds/storybook/.storybook/preview.ts
+++ playgrounds/storybook/.storybook/preview.ts
@@ -5,30 +5,89 @@ import "../stories/assets/main.css";
 import { setup } from "@storybook/vue3";
 import { withUpwindTheme } from "./withUpwindTheme.decorator";
 import { useArgs } from "@storybook/preview-api";
+import { createI18n } from "vue-i18n";
 
 // --- internal
 import upwind from "../plugins/upwind";
 import themes from "@/assets/themes";
 
 // --- utils
+import { reduce, last, merge } from "lodash-es";
 
 // --- types
 import type { Preview } from "@storybook/your-renderer";
 // -----------------------------------------------------------------------------
 
 setup(app => {
+  function getGlobalMessages() {
+    const messages = reduce(
+      import.meta.glob("@locales/**/*.json", { eager: true }),
+      (result, value, key) => {
+        const locale = last(key.split("/"))?.replace(".json", "");
+        if (!locale) return result;
+        merge(result, { [locale]: value?.default || {} });
+        return result;
+      },
+      {}
+    );
+
+    return messages;
+  }
+
+  const i18n = createI18n({
+    locale: "en",
+    fallbackLocale: "en",
+    messages: getGlobalMessages(),
+  });
+  app.use(i18n);
+
   app.use(upwind);
 });
 
 const preview: Preview = {
   parameters: {
-    backgrounds: { disable: true },
+    // backgrounds: {
+    //   disable: true,
+    //   grid: {
+    //     disable: true,
+    //   },
+    // },
+    // measure: { disable: true },
+    // outline: { disable: true },
     layout: "fullscreen",
+    toolbar: {
+      zoom: { hidden: true },
+      eject: { hidden: true },
+      copy: { hidden: true },
+      fullscreen: { hidden: true },
+    },
+  },
+  globalTypes: {
+    locale: {
+      description: "Internationalization locale",
+      defaultValue: "en",
+      toolbar: {
+        title: "Locale",
+        icon: "globe",
+        dynamicTitle: true,
+        items: [
+          { value: "en", right: "🇬🇧", title: "English" },
+          { value: "fr", right: "🇫🇷", title: "French" },
+          { value: "es", right: "🇪🇸", title: "Spanish" },
+          { value: "de", right: "🇩🇪", title: "German" },
+          { value: "it", right: "🇮🇹", title: "Italian" },
+          { value: "pt", right: "🇵🇹", title: "Portuguese" },
+          { value: "ru", right: "🇷🇺", title: "Russian" },
+        ],
+      },
+    },
   },
   decorators: [
     withUpwindTheme({
       themes,
       defaultTheme: "light",
+      locales: [],
+      defaultLocale: "en",
     }),
     (story, context) => {
       // This make sit possible to updateArgs within stories
--- playgrounds/storybook/stories/UpWind™/Form/Generator.stories.ts
+++ playgrounds/storybook/stories/UpWind™/Form/Generator.stories.ts
@@ -1,10 +1,17 @@
 // --- external
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import type { Meta, StoryObj } from "@storybook/vue3";
+
+// ---internal
+import * as messages from "./locales";
 
 // --- components
 import { UpwForm } from "@upmind/upwind";
+import { on } from "events";
 
+// --- utils
+import { isEmpty, omitBy } from "lodash";
+import { useSystemArgTypes } from "../../../utils";
 // -----------------------------------------------------------------------------
 
 const meta: Meta<typeof UpwForm> = {
@@ -24,28 +31,41 @@ const meta: Meta<typeof UpwForm> = {
   render: args => ({
     components: { UpwForm },
     setup() {
+      const isValid = ref(false);
       const model = ref({});
 
       function doReject() {}
 
       function doResolve(value) {}
+
       return {
         args,
         model,
+        isValid,
         doReject,
         doResolve,
+        formStatus: computed(() =>
+          !isEmpty(omitBy(model.value, isEmpty)) ? (isValid.value ? 1 : 2) : 0
+        ),
       };
     },
+    i18n: { messages },
     template: `
       <upw-form
+       :locale="$i18n.locale"
+       :translator="$t"
         v-bind="args"
         v-model="model"
-        @reject="doReject"
-        @resolve="doResolve"
+        @valid="isValid = $event"
       />
 
-      <h4>Model</h4>
-      <pre class="sticky top-0 rounded-lg bg-base-100 p-4">{{ model }}</pre>
+      <div class="w-full flex-1 self-stretch rounded-lg bg-base-100 p-4 mt-8">
+      <strong class="font-mono uppercase">{{
+        $tc("form.model", formStatus)
+      }}</strong>
+
+      <pre class="sticky top-0 text-wrap">{{ model }}</pre>
+    </div>
     `,
   }),
   argTypes: {},
@@ -206,6 +226,200 @@ export const Formatted: Story = {
             {
               type: "Control",
               scope: "#/properties/personalData/properties/vegetarian",
+            },
+          ],
+        },
+      ],
+    },
+  },
+};
+
+export const Task: Story = {
+  args: {
+    schema: {
+      required: ["name", "rating"],
+      properties: {
+        name: {
+          type: ["string"],
+          i18n: "form.name",
+        },
+
+        description: {
+          type: ["string", "null"],
+          maxLength: 140,
+          i18n: "form.description",
+        },
+
+        rating: {
+          type: "integer",
+          maximum: 5,
+          minimum: 1,
+          i18n: "form.rating",
+        },
+
+        impact: {
+          type: "number",
+          maximum: 10,
+          minimum: 0,
+          i18n: "form.impact",
+        },
+
+        dueDate: {
+          type: ["string", "null"],
+          format: "date",
+          formatMinimum: new Date(Date.now()).toLocaleString(),
+          i18n: "form.dueDate",
+        },
+
+        recurrence: {
+          type: ["string", "null"],
+          i18n: "form.recurrence",
+          oneOf: [
+            {
+              const: "daily",
+              i18n: "form.recurrence.options.daily",
+            },
+            {
+              const: "weekly",
+              i18n: "form.recurrence.options.weekly",
+            },
+            {
+              const: "monthly",
+              i18n: "form.recurrence.options.monthly",
+              title: "Monthly",
+            },
+          ],
+        },
+
+        recurrenceInterval: {
+          type: "integer",
+          minimum: 1,
+          maximum: 365,
+          i18n: "form.recurrenceInterval",
+        },
+
+        done: {
+          type: ["boolean", "null"],
+          enum: [true, false, null],
+          i18n: "form.done",
+        },
+      },
+    },
+
+    uischema: {
+      type: "VerticalLayout",
+      elements: [
+        {
+          type: "Label",
+          i18n: "form.label",
+        },
+
+        {
+          type: "Control",
+          scope: "#/properties/name",
+          options: {
+            focus: true,
+          },
+        },
+
+        {
+          type: "Control",
+          scope: "#/properties/description",
+          options: {
+            multi: true,
+            autosize: true,
+          },
+        },
+
+        {
+          type: "HorizontalLayout",
+          elements: [
+            {
+              type: "Control",
+              scope: "#/properties/rating",
+            },
+
+            {
+              type: "Control",
+              scope: "#/properties/impact",
+            },
+          ],
+        },
+
+        {
+          type: "HorizontalLayout",
+          elements: [
+            {
+              type: "Control",
+              scope: "#/properties/recurrence",
+              options: {
+                format: "radio",
+              },
+            },
+            {
+              type: "Control",
+              scope: "#/properties/recurrenceInterval",
+              options: {
+                min: 1,
+                max: 365,
+                suffix: "days",
+              },
+              rule: {
+                effect: "SHOW",
+                condition: {
+                  scope: "#/properties/recurrence",
+                  schema: { type: "string", const: "Daily" },
+                },
+              },
+            },
+            {
+              type: "Control",
+              scope: "#/properties/recurrenceInterval",
+              options: {
+                min: 1,
+                max: 52,
+                suffix: "weeks",
+              },
+              rule: {
+                effect: "SHOW",
+                condition: {
+                  scope: "#/properties/recurrence",
+                  schema: { type: "string", const: "Weekly" },
+                },
+              },
+            },
+            {
+              type: "Control",
+              scope: "#/properties/recurrenceInterval",
+              options: {
+                min: 1,
+                max: 12,
+                suffix: "months",
+              },
+              rule: {
+                effect: "SHOW",
+                condition: {
+                  scope: "#/properties/recurrence",
+                  schema: { type: "string", const: "Monthly" },
+                },
+              },
+            },
+          ],
+        },
+
+        {
+          type: "Group",
+
+          i18n: "form.completion",
+          elements: [
+            {
+              type: "Control",
+              scope: "#/properties/dueDate",
+            },
+
+            {
+              type: "Control",
+              scope: "#/properties/done",
             },
           ],
         },
