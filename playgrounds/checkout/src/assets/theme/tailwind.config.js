af72bf8c049c841c5f4ba979a41dbabf156b8965
Dominic Da Costa <dominic@thecollaboration.studio>
2024-05-06 17:00:15 +0200

    theme housekeeping + tweaks

--- playgrounds/checkout/src/assets/theme/tailwind.config.js
+++ playgrounds/checkout/src/assets/theme/tailwind.config.js
@@ -13,19 +13,6 @@ For a full list of options, see the official Tailwind CSS documentation: https:/
 */
 
 export default {
-  /*
-|-------------------------------------------------------------------------------
-| Colors                                    https://tailwindcss.com/docs/colors
-|-------------------------------------------------------------------------------
-|
-| Here you can specify the colors used in your project.
-| We generally dont specify colors here, only the theme colors. eg: primary, secondary, tertiary, accent, neutral
-| To get you started, we've provided our base palette of great looking colors that are perfect
-| for your Upmind store, but don't hesitate to change them for your project.
-| You own these colors, nothing will break if you change everything about them.
-|
-*/
-
   colors: {
     transparent: "transparent",
     black: "#000000",
@@ -203,59 +190,17 @@ export default {
     // ---
   },
 
-  /*
-  |-----------------------------------------------------------------------------
-  | Fonts                                    https://tailwindcss.com/docs/fonts
-  |-----------------------------------------------------------------------------
-  |
-  | Here is where you define your project's font stack, or font families.
-  | Keep in mind that Tailwind doesn't actually load any fonts for you.
-  | If you're using custom fonts you'll need to import them prior to
-  | defining them here.
-  |
-  | By default we provide a native font stack that works remarkably well on
-  | any device or OS you're using, since it just uses the default fonts
-  | provided by the platform.
-  |
-  | Class name: .font-{name}
-  |
-  */
-
   fontFamily: {
     sans: ["Inter", "sans-serif", ...defaultTheme.fontFamily.sans],
     serif: ["Inter", "sans-serif", ...defaultTheme.fontFamily.serif],
     mono: ["Inconsolata", "monospace", ...defaultTheme.fontFamily.mono],
   },
 
-  // fontFamily: {
-  //   sans: ["Courier New", "monospace"],
-  //   serif: ["Courier New", "monospace"],
-  //   mono: ["Courier New", "monospace"],
-  // },
-
-  /*
-  |-----------------------------------------------------------------------------
-  | Text sizes                         https://tailwindcss.com/docs/text-sizing
-  |-----------------------------------------------------------------------------
-  |
-  | Here is where you define your text sizes. Name these in whatever way
-  | makes the most sense to you. We use size names by default, but
-  | you're welcome to use a numeric scale or even something else
-  | entirely.
-  |
-  | By default Tailwind uses the "rem" unit type for most measurements.
-  | This allows you to set a root font size which all other sizes are
-  | then based on. That said, you are free to use whatever units you
-  | prefer, be it rems, ems, pixels or other.
-  |
-  | Class name: .text-{size}
-  |
-  */
-
-  textSizes: {
+  fontSize: {
     xs: ".75rem", // 12px
     sm: ".875rem", // 14px
     base: "1rem", // 16px
+    md: "1rem", // 16px
     lg: "1.125rem", // 18px
     xl: "1.25rem", // 20px
     "2xl": "1.5rem", // 24px
@@ -263,96 +208,16 @@ export default {
     "4xl": "2.25rem", // 36px
     "5xl": "3rem", // 48px
   },
-
-  /*
-  |-----------------------------------------------------------------------------
-  | Leading (line height)              https://tailwindcss.com/docs/line-height
-  |-----------------------------------------------------------------------------
-  |
-  | Here is where you define your line height values, or as we call
-  | them in Tailwind, leadings.
-  |
-  | Class name: .leading-{size}
-  |
-  */
-
-  leading: {
-    none: 1,
-    tight: 1.25,
-    normal: 1.5,
-    loose: 2,
-  },
-
-  /*
-  |-----------------------------------------------------------------------------
-  | Tracking (letter spacing)       https://tailwindcss.com/docs/letter-spacing
-  |-----------------------------------------------------------------------------
-  |
-  | Here is where you define your letter spacing values, or as we call
-  | them in Tailwind, tracking.
-  |
-  | Class name: .tracking-{size}
-  |
-  */
-
-  tracking: {
-    tight: "-0.05em",
-    normal: "0",
-    wide: "0.05em",
-  },
-
-  /*
-  |-----------------------------------------------------------------------------
-  | Border radius                    https://tailwindcss.com/docs/border-radius
-  |-----------------------------------------------------------------------------
-  |
-  | Here is where you define your border radius values. If a `default` radius
-  | is provided, it will be made available as the non-suffixed `.rounded`
-  | utility.
-  |
-  | If your scale includes a `0` value to reset already rounded corners, it's
-  | a good idea to put it first so other values are able to override it.
-  |
-  | Class name: .rounded{-side?}{-size?}
-  |
-  */
-
-  borderRadius: {
-    DEFAULT: ".25rem",
-    none: "0",
-    xs: ".0625rem", // 1px
-    sm: ".125rem", // 2px
-    default: ".25rem", // 4px
-    lg: ".5rem", // 8px
-    xl: "1rem", // 16px
-    full: "9999px", // 9999px,
-    pill: "10em", // 9999px,
-    button: ".25rem", // 4px,
-    box: ".5rem", // 8px,
-  },
-
-  /*
-  |-----------------------------------------------------------------------------
-  | Shadows                                https://tailwindcss.com/docs/shadows
-  |-----------------------------------------------------------------------------
-  |
-  | Here is where you define your shadow utilities. As you can see from
-  | the defaults we provide, it's possible to apply multiple shadows
-  | per utility using comma separation.
-  |
-  | If a `default` shadow is provided, it will be made available as the non-
-  | suffixed `.shadow` utility.
-  |
-  | Class name: .shadow-{size?}
-  |
-  */
-
-  shadows: {
-    default: "0 2px 4px 0 rgba(0,0,0,0.10)",
-    md: "0 4px 8px 0 rgba(0,0,0,0.12), 0 2px 4px 0 rgba(0,0,0,0.08)",
-    lg: "0 15px 30px 0 rgba(0,0,0,0.11), 0 5px 15px 0 rgba(0,0,0,0.08)",
-    inner: "inset 0 2px 4px 0 rgba(0,0,0,0.06)",
-    outline: "0 0 0 3px rgba(52,144,220,0.5)",
-    none: "none",
-  },
+  // ---------------------------------------------------------------------------
+  typography: ({ theme }) => ({
+    DEFAULT: {
+      css: {
+        h1: {
+          fontWeight: theme("fontWeight.light"),
+          fontSize: theme("fontSize.5xl"),
+          color: "red",
+        },
+      },
+    },
+  }),
 };
