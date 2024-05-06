0c53f1da212996bbc35fa46f78da2d8d1af64282
Dominic Da Costa <dominic@thecollaboration.studio>
2024-05-06 16:45:40 +0200

    housekeeping

--- packages/upwind/src/components/button/types.d.ts
+++ packages/upwind/src/components/button/types.d.ts
@@ -1,3 +1,7 @@
+import type { IconProps } from "../icon/types";
+
+// --------------------------------------------
+
 export interface ButtonProps {
   size?: "sm" | "md" | "lg";
   variant?: "flat" | "outlined" | "ghost" | "link";
