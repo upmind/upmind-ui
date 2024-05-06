ba627b87cbdcc490dcd5a04dc43d06ac0e4b87f1
Dominic Da Costa <dominic@thecollaboration.studio>
2024-05-06 16:59:10 +0200

    story for tabs

--- playgrounds/storybook/stories/UpWind™/Form/Generator.stories.ts
+++ playgrounds/storybook/stories/UpWind™/Form/Generator.stories.ts
@@ -60,12 +60,9 @@ const meta: Meta<typeof UpwForm> = {
       />
 
       <div class="w-full flex-1 self-stretch rounded-lg bg-base-100 p-4 mt-8">
-      <strong class="font-mono uppercase">{{
-        $tc("form.model", formStatus)
-      }}</strong>
-
-      <pre class="sticky top-0 text-wrap">{{ model }}</pre>
-    </div>
+        <strong class="font-mono uppercase">{{$tc("form.model", formStatus)}}</strong>
+        <pre class="sticky top-0 text-wrap">{{ model }}</pre>
+      </div>
     `,
   }),
   argTypes: {},
