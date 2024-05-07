d8da9bfe8d801c4d6ada0c852640a07bacd2841e
Dominic Da Costa <dominic@thecollaboration.studio>
2024-05-07 16:00:21 +0200

    better i18n with local messages and debug option

---
+++ playgrounds/checkout/src/views/client/Companies.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="client w-full">
+    <header class="flex w-full flex-wrap">
+      <h2 class="m-0 text-nowrap">
+        {{ $t("client.companies.title") }}
+      </h2>
+    </header>
+
+    <router-view />
+  </section>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { getLocalMessages } from "@/utils";
+
+export default defineComponent({
+  name: "ClientCompanies",
+  i18n: {
+    messages: getLocalMessages("client"),
+  },
+});
+</script>
