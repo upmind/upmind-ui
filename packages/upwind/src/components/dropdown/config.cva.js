4392a8257ea58b920bbae7dd4ab624674372e0ea
Dominic Da Costa <dominic@thecollaboration.studio>
2024-05-08 14:43:14 +0200

    fix disabled dropdown items

--- packages/upwind/src/components/dropdown/config.cva.js
+++ packages/upwind/src/components/dropdown/config.cva.js
@@ -23,21 +23,43 @@ export default {
       "border-base-200 mb-2 w-full border-b pb-2 first:pt-0 last:mb-0 last:border-b-0 last:pb-0"
     ),
   },
-  dropdownGroupItem: {
-    root: cva(
-      "text-base-500 flex w-full items-center gap-x-3.5 px-3 py-2 text-xs font-medium"
-    ),
-    label: cva("uppercase"),
-    icon: cva("size-[1.5em] flex-shrink-0"),
-  },
+
   dropdownItem: {
     root: cva(
-      "text-base-800 focus:bg-base-100 flex w-full cursor-pointer items-center justify-start gap-x-4 text-nowrap rounded-lg px-4 py-2 text-left text-sm no-underline focus:outline-none"
+      "text-base-800 flex w-full cursor-pointer items-center justify-start gap-x-4 text-nowrap rounded-lg px-4 py-2 text-left text-sm no-underline focus:outline-none",
+      {
+        variants: {
+          group: {
+            true: "text-base-500 cursor-default gap-x-3.5 px-3 text-xs font-medium",
+          },
+          disabled: {
+            true: "pointer-events-none opacity-50",
+          },
+        },
+      }
     ),
-    active: cva("bg-base-100 hover:bg-base-100"),
+    active: cva("bg-base-100 hover:bg-base-100", {
+      variants: {
+        group: {
+          true: "bg-base hover:bg-base",
+        },
+      },
+    }),
     selected: cva("bg-base-100 hover:bg-base-100 cursor-default select-none"),
-    label: cva("flex-1 truncate"),
-    icon: cva("size-[1.25em]"),
+    label: cva("flex-1 truncate", {
+      variants: {
+        group: {
+          true: "uppercase ",
+        },
+      },
+    }),
+    icon: cva("size-[1.25em]  flex-shrink-0", {
+      variants: {
+        group: {
+          true: "size-[1.5em]",
+        },
+      },
+    }),
   },
   dropdownTransitionEnter: {
     active: cva("m-0 transition duration-100 ease-out"),
