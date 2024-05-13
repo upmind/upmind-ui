9e280ce1e3c0aa6f456f3b7afb5ede31a4f17717
Dominic Da Costa <dominic@thecollaboration.studio>
2024-05-13 12:21:04 +0200

    combobox components

--- packages/upwind/src/index.ts
+++ packages/upwind/src/index.ts
@@ -1,7 +1,9 @@
 export { default as UpwAlert } from "./components/alert/Alert.vue";
+export { default as UpwBadge } from "./components/badge/Badge.vue";
 export { default as UpwButton } from "./components/button/Button.vue";
 export { default as UpwCheckbox } from "./components/checkbox/Checkbox.vue";
 export { default as UpwCheckboxList } from "./components/checkbox/CheckboxList.vue";
+export { default as UpwCombobox } from "./components/combobox/Combobox.vue";
 export { default as UpwDialog } from "./components/dialog/Dialog.vue";
 export { default as UpwDropdown } from "./components/dropdown/Dropdown.vue";
 export { default as UpwForm } from "./components/form/Form.vue";
