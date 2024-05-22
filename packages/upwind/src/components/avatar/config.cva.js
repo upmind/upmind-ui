85b72b0e80a126c29c4a9d255b3715ad0ebb79d7
Dominic Da Costa <dominic@thecollaboration.studio>
2024-05-22 11:24:34 +0200

    WIP basic breadcrumb/stepper

--- packages/upwind/src/components/avatar/config.cva.js
+++ packages/upwind/src/components/avatar/config.cva.js
@@ -4,7 +4,7 @@ import { cva } from "class-variance-authority";
 export default {
   avatar: {
     root: cva(
-      "bg-base-200 m-0 flex flex-shrink-0 items-center justify-center overflow-hidden rounded-full",
+      "bg-base-200 m-0 flex flex-shrink-0 items-center justify-center overflow-hidden rounded-full transition",
       {
         variants: {
           size: {
@@ -23,7 +23,7 @@ export default {
     ),
     icon: cva("h-full w-full object-cover"),
     image: cva("h-full w-full object-cover"),
-    caption: cva("m-0", {
+    caption: cva("m-0 text-current transition", {
       variants: {
         size: {
           xs: "text-xs",
--- playgrounds/checkout/src/views/checkout/Checkout.vue
+++ playgrounds/checkout/src/views/checkout/Checkout.vue
@@ -1,29 +1,42 @@
 <template>
   <article class="flex flex-col gap-4">
-    <section class="h-20 border border-dashed">
-      {{ $t("checkout.breadcrumb") }}
-    </section>
-    <section class="min-h-screen border border-dashed">
+    <upm-breadcrumb />
+    <section id="overview" class="min-h-screen border border-dashed">
       {{ $t("checkout.overview") }}
     </section>
-    <section class="min-h-screen border border-dashed">
+    <section id="account" class="min-h-screen border border-dashed">
       {{ $t("checkout.account") }}
     </section>
-    <section class="min-h-screen border border-dashed">
+    <section id="payment" class="min-h-screen border border-dashed">
       {{ $t("checkout.payment") }}
     </section>
-    <section class="min-h-screen border border-dashed">
+    <section id="confirmation" class="min-h-screen border border-dashed">
       {{ $t("checkout.confirmation") }}
     </section>
   </article>
 </template>
 
 <script>
+// --- external
 import { defineComponent } from "vue";
+
+// -- components
+import UpmBreadcrumb from "./components/Breadcrumb.vue";
+
+// -- utils
 import { getLocalMessages } from "@/utils";
 
+// -----------------------------------------------------------------------------
 export default defineComponent({
   name: "Checkout",
   i18n: { messages: getLocalMessages("checkout") },
+  components: { UpmBreadcrumb },
+  setup() {
+    return {};
+  },
+  mounted() {
+    // force the route to have a hash of the first step
+    if (!this.$route?.hash) this.$router.push({ hash: "#overview" });
+  },
 });
 </script>
