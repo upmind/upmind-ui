86a49fa70d422fba9c615fef617d02043da196c5
Dominic Da Costa <dominic@thecollaboration.studio>
2024-05-22 13:18:10 +0200

    preparation work for checkout page basic breadcrumb/stepper basket with count in header

--- playgrounds/checkout/src/views/checkout/components/Breadcrumb.vue
+++ playgrounds/checkout/src/views/checkout/components/Breadcrumb.vue
@@ -1,30 +1,39 @@
 <template>
   <nav
-    class="sticky top-0 -mx-4 -mt-8 flex flex-row items-center justify-start gap-8 border-b border-base-300 bg-base px-4 text-base-content sm:-mx-6 sm:px-6 lg:-mx-20 lg:px-20"
+    class="sticky top-0 z-10 -mx-4 -mt-8 flex flex-row items-center justify-start gap-8 border-b border-base-300 bg-base px-4 text-base-content sm:-mx-6 sm:px-6 lg:-mx-20 lg:px-20"
   >
     <router-link
       v-for="(step, index) in steps"
       :key="step.hash"
-      class="m-0 flex items-center gap-3 border-b-2 border-transparent p-0 py-8 font-light leading-none no-underline transition"
-      :class="[
-        {
-          'text-primary': step.hash == $route?.hash,
-          '!border-primary': step.hash == $route?.hash,
-        },
-      ]"
+      custom
+      v-slot="{ isActive, href, navigate }"
       :to="{ hash: step.hash }"
     >
-      <upw-avatar
-        :avatar="{ caption: `${index + 1}` }"
-        size="xs"
-        :class="
-          step.hash == $route?.hash
-            ? 'bg-primary text-primary-content transition'
-            : ''
-        "
-      ></upw-avatar>
+      <component
+        :is="step.disabled ? 'span' : 'a'"
+        @click="!step.disabled && navigate"
+        :href="href"
+        :disabled="step.disabled"
+        class="m-0 flex items-center gap-3 border-b-2 border-transparent p-0 py-8 font-light leading-none no-underline transition"
+        :class="[
+          {
+            'text-primary': step.hash == $route?.hash,
+            '!border-primary': step.hash == $route?.hash,
+          },
+        ]"
+      >
+        <upw-avatar
+          :avatar="{ caption: `${index + 1}` }"
+          size="xs"
+          :class="
+            step.hash == $route?.hash
+              ? 'bg-primary text-primary-content transition'
+              : ''
+          "
+        ></upw-avatar>
 
-      <span>{{ step.label }}</span>
+        <span>{{ step.label }}</span>
+      </component>
     </router-link>
   </nav>
 </template>
@@ -45,10 +54,10 @@ export default defineComponent({
       meta,
       steps: computed(() => {
         return [
-          { label: "Overview", hash: "#overview", active: true },
+          { label: "Overview", hash: "#overview" },
           { label: "Account", hash: "#account" },
           { label: "Payment", hash: "#payment" },
-          { label: "Confirmation", hash: "#confirmation" },
+          { label: "Confirmation", hash: "#confirmation", disabled: true },
         ];
       }),
       doCheckout: async () => {
