a0d588e4176fd0717151a8bfa0a2ddefcaa49879
Dominic Da Costa <dominic@thecollaboration.studio>
2024-05-23 10:14:42 +0200

    housekeeping

--- packages/client-vue/src/components/session/Auth.vue
+++ packages/client-vue/src/components/session/Auth.vue
@@ -49,7 +49,6 @@ import type { AuthProps } from "./types";
 export default defineComponent({
   name: "UpmAuth",
   components: { UpwForm, UpwTabs, UpwButton },
-  inheritAttrs: true,
   customOptions: {},
   emits: ["update:modelValue"],
   props: {
