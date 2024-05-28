882896fc1e4db15e8c4cea8741b068c1969a8e8a
Dominic Da Costa <dominic@thecollaboration.studio>
2024-05-28 09:39:19 +0200

    fix sequencing and added better states for processing order

--- packages/client-vue/src/components/basket/Details.vue
+++ packages/client-vue/src/components/basket/Details.vue
@@ -2,7 +2,7 @@
   <section
     :class="styles.basket.details.root"
     :disabled="!meta.hasProducts || !meta.hasAccount"
-    v-if="meta.isAvailable"
+    v-if="meta.isAvailable || meta.isProcessingOrder"
   >
     <header :class="styles.basket.details.header">
       <slot name="header" v-bind="{ meta }">
