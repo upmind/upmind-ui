75a3c1513b4899f10f172430efce45bbfa1ee354
Dominic Da Costa <dominic@thecollaboration.studio>
2024-07-15 11:24:44 +0200

    added sentry integration

--- playgrounds/checkout/src/main.ts
+++ playgrounds/checkout/src/main.ts
@@ -2,12 +2,19 @@ import "./assets/main.css";
 
 import { createApp } from "vue";
 import { createI18n } from "vue-i18n";
+import * as Sentry from "@sentry/vue";
 
 import upmind from "./plugins/upmind";
 
 import App from "./App.vue";
 import router from "./router";
 import { getGlobalMessages } from "./utils";
+
+// ---------------------
+
+const app = createApp(App);
+
+// ---------------------
 
 const i18n = createI18n({
   // legacy: false, // you must set `false` to use Composition API
@@ -21,7 +28,36 @@ const i18n = createI18n({
   silentFallbackWarn: true,
 });
 
-const app = createApp(App);
+// ---------------------
+
+Sentry.init({
+  app,
+  dsn: import.meta.env.VITE_APP_SENTRY_DSN,
+  integrations: [
+    Sentry.browserTracingIntegration({ router }),
+    Sentry.replayIntegration(),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for tracing.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+
+  // Set `tracePropagationTargets` to control for which URLs trace propagation should be enabled
+  tracePropagationTargets: [
+    "localhost",
+    /^http:\/\/*.local/,
+    /^https:\/\/upm-checkout-doteasy\.web\.app\.io/,
+    /^https:\/\/checkout\.sprighost\.com/,
+  ],
+
+  // Capture Replay for 10% of all sessions,
+  // plus for 100% of sessions with an error
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
+// ---------------------
 
 app.use(router);
 app.use(upmind);
