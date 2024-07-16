edff6e7b570aeb1af960b980911e4b1ee85a4432
Dominic Da Costa <dominic@thecollaboration.studio>
2024-07-16 17:19:28 +0200

    fixed cva + prettier

--- packages/client-vue/src/components/feedback/config.cva.js
+++ packages/client-vue/src/components/feedback/config.cva.js
@@ -6,24 +6,24 @@ export default {
     root: cva("w-full bg-transparent"),
     banners: cva("flex flex-col"),
     toasts: cva(
-      "flex flex-col justify-start items-start max-h-screen fixed z-[999] [&>.alert]:shadow-md w-full overflow-auto ",
+      "fixed z-[999] flex max-h-screen w-full flex-col items-start justify-start overflow-auto [&>.alert]:shadow-md ",
       {
         variants: {
           position: {
             "top-start":
               "bottom-auto end-auto start-0 top-0 translate-x-0 translate-y-0 items-start",
             "top-center":
-              "bottom-auto end-1/2 start-1/2 top-0 translate-y-0 -translate-x-1/2 rtl:translate-x-1/2 items-center",
+              "bottom-auto end-1/2 start-1/2 top-0 -translate-x-1/2 translate-y-0 items-center rtl:translate-x-1/2",
             "top-end":
-              "bottom-auto start-auto end-0 top-0 translate-x-0 translate-y-0 items-end",
+              "bottom-auto end-0 start-auto top-0 translate-x-0 translate-y-0 items-end",
             center:
-              "end-1/2 start-1/2 -translate-x-1/2 rtl:translate-x-1/2 items-center",
+              "end-1/2 start-1/2 -translate-x-1/2 items-center rtl:translate-x-1/2",
             "bottom-start":
               "bottom-0 end-auto start-0 translate-x-0 translate-y-0 items-start",
             "bottom-center":
-              "bottom-0 end-1/2 start-1/2 -translate-x-1/2 rtl:translate-x-1/2 translate-y-0 items-center",
+              "bottom-0 end-1/2 start-1/2 -translate-x-1/2 translate-y-0 items-center rtl:translate-x-1/2",
             "bottom-end":
-              "bottom-0 start-auto end-0 translate-x-0 translate-y-0 items-end",
+              "bottom-0 end-0 start-auto translate-x-0 translate-y-0 items-end",
           },
         },
         defaultVariants: {
@@ -40,7 +40,7 @@ export default {
   },
 
   bannerTransitionLeave: {
-    active: cva("transition duration-100 ease-in absolute"),
+    active: cva("absolute transition duration-100 ease-in"),
     from: cva("translate-y-0 transform opacity-100"),
     to: cva("-translate-y-1 transform opacity-0"),
   },
