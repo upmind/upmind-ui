f78d2303aba13b473c99b378b67348104ce692d9
Dominic Da Costa <dominic@thecollaboration.studio>
2024-07-17 10:53:39 +0200

    always show required on input, regardless of validity

--- packages/upwind/src/components/label/config.cva.js
+++ packages/upwind/src/components/label/config.cva.js
@@ -26,17 +26,7 @@ export default {
     required: cva(""),
     optional: cva(""),
     status: cva(
-      "text-base-500 ml-auto inline-flex items-center gap-2 text-xs leading-tight",
-      {
-        variants: {
-          isValid: {
-            true: "hidden",
-          },
-          isInvalid: {
-            // true: "text-error",
-          },
-        },
-      }
+      "text-base-500 ml-auto inline-flex items-center gap-2 text-xs leading-tight"
     ),
     icon: cva("size-4"),
   },
