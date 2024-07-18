3a2b39f280b1986db85a555975284bfcb541d901
Dominic Da Costa <dominic@thecollaboration.studio>
2024-07-18 12:54:35 +0200

    fix intermittent stripe load error

--- packages/flow-vue/src/modules/basket/useBasketPaymentDetails.ts
+++ packages/flow-vue/src/modules/basket/useBasketPaymentDetails.ts
@@ -60,6 +60,11 @@ export const useBasketPaymentDetails = (actor?: TActor<any>) => {
           "generating",
           "claiming",
         ]),
+      isAvailable: stateMatches(
+        payment_details.value?.state,
+        ["available"] &&
+          !stateMatches(payment_details.value?.state, ["available.loading"])
+      ),
       hasErrors: stateMatches(payment_details.value?.state, ["error"]),
       isProcessing: stateMatches(payment_details.value?.state, [
         "checking",
