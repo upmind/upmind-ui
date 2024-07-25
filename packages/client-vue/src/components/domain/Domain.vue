5dd785ac65a54dbd6e00f3876fe6d8833a58acc6
Dominic Da Costa <dominic@thecollaboration.studio>
2024-07-25 14:11:08 +0200

    domain search working

--- packages/client-vue/src/components/domain/Domain.vue
+++ packages/client-vue/src/components/domain/Domain.vue
@@ -2,15 +2,48 @@
   <div :class="styles.domain.root">
     <!-- loader -->
 
+    <pre>{{ meta }}</pre>
+
     <!-- type -->
     <upw-radio-list
       v-if="meta.showChoices"
       :items="choices"
-      :model-value="type"
+      :model-value="choice"
       @update:modelValue="choose"
     />
 
     <!-- register/transfer -->
+    <upw-textbox
+      v-if="meta.showRegister"
+      @update:modelValue="search"
+      prependIcon="MagnifyingGlassIcon"
+      placeholder="Find your pefect domain &hellip;"
+      :autofocus="meta.showRegister"
+      autocomplete="url"
+      :model-value="query"
+    >
+      <template #append>
+        <upw-button
+          label="Find"
+          :loading="meta.isSearching"
+          variant="ghost"
+          size="sm"
+        />
+      </template>
+    </upw-textbox>
+
+    <upw-textbox
+      v-if="meta.showTransfer"
+      @update:modelValue="search"
+      prependIcon="MagnifyingGlassIcon"
+      placeholder="Search for the domain to Transfer &hellip;"
+      :autofocus="meta.showTransfer"
+      autocomplete="url"
+    >
+      <template #append>
+        <upw-button label="Search" :loading="meta.isSearching" />
+      </template>
+    </upw-textbox>
 
     <!-- basket -->
 
@@ -24,17 +57,26 @@ import { defineComponent } from "vue";
 
 // --- internal
 import { useDomain } from "@upmind/flow-vue";
-import { useStyles, mergeStyles, UpwRadioList } from "@upmind/upwind";
+import {
+  useStyles,
+  mergeStyles,
+  UpwRadioList,
+  UpwTextbox,
+  UpwButton,
+} from "@upmind/upwind";
 import config from "./config.cva";
 
 // --- components
+
+// --- utils
+import { debounce } from "lodash-es";
 
 // --- types
 
 // -----------------------------------------------------------------------------
 export default defineComponent({
   name: "UpmDomain",
-  components: { UpwRadioList },
+  components: { UpwRadioList, UpwTextbox, UpwButton },
   props: {
     sync: { type: Boolean, default: true },
     type: {
@@ -57,6 +99,7 @@ export default defineComponent({
       // values,
       // selected,
       type,
+      query,
       // available,
       // errors,
       // primaryDomain,
@@ -64,7 +107,7 @@ export default defineComponent({
       meta,
       // ---
       choose,
-      // search,
+      search,
       // add,
       // remove,
       // toggle,
@@ -80,9 +123,12 @@ export default defineComponent({
       meta,
       choices,
       // ---
-      type,
+      choice: type,
+      query,
       // ---
       choose,
+      search: debounce(search, 500),
+
       // ---
       styles,
       mergeStyles,
