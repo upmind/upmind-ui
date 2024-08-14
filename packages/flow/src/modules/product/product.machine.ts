2a38136210b30ad10dfee9a5fd501ea6bd3b65fa
Dominic Da Costa <dominic@thecollaboration.studio>
2024-08-14 07:40:30 +0200

    restore currency check

--- packages/flow/src/modules/product/product.machine.ts
+++ packages/flow/src/modules/product/product.machine.ts
@@ -391,7 +391,7 @@ export default createMachine(
         ) => {
           return {
             // ---
-            currency_id, //useBrand().validateCurrency(currency_id),
+            currency_id: useBrand().validateCurrency(currency_id),
             promotions,
             // ---
             baseModel: !isEmpty(basket_product)
