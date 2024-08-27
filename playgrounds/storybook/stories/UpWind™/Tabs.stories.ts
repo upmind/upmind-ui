3125a8c62bde3e93051dae1e8273444c92b9a9da
Rhodri Jones <rhodri.jones@upmind.com>
2024-08-27 10:57:34 +0000

    Feature/shadcn-tabs

--- playgrounds/storybook/stories/UpWind™/Tabs.stories.ts
+++ playgrounds/storybook/stories/UpWind™/Tabs.stories.ts
@@ -2,12 +2,24 @@
 import type { Meta, StoryObj } from "@storybook/vue3";
 
 // --- components
-import { UpwTabs } from "@upmind/upwind";
+import {
+  UwTabs,
+  UwTabsTrigger,
+  UwTabsList,
+  UwTabsContent,
+} from "@upmind/upwind";
 
 // --- utils
 import { useSystemArgTypes } from "../../utils";
 import { keys } from "lodash-es";
 // -----------------------------------------------------------------------------
+
+// --- types
+enum variants {
+  flat = "Flat",
+  outlined = "Outlined",
+  tonal = "Tonal",
+}
 
 enum alignments {
   start = "start",
@@ -18,42 +30,110 @@ enum alignments {
   evenly = "evenly",
 }
 
-const meta: Meta<typeof UpwTabs> = {
-  parameters: {
-    controls: {
-      exclude: ["size", "modelValue"],
-    },
-  },
-  component: UpwTabs,
+enum widths {
+  full = "full",
+  auto = "auto",
+}
+
+const meta: Meta<typeof UwTabs> = {
+  component: UwTabs,
   argTypes: {
-    size: useSystemArgTypes.size,
-    align: {
+    variant: {
+      options: keys(variants),
+      control: {
+        type: "radio",
+        labels: variants,
+      },
+    },
+    alignment: {
       options: keys(alignments),
       control: {
         type: "radio",
         labels: alignments,
       },
-      if: { arg: "block", truthy: true },
     },
+    width: {
+      options: keys(widths),
+      control: {
+        type: "radio",
+        labels: widths,
+      },
+    },
+    color: useSystemArgTypes.color,
   },
   args: {
-    size: "md",
-    disabled: false,
-    block: false,
-    stretch: false,
-    modelValue: "item3",
-    align: "start",
-    tabs: [
-      { id: "item1", label: "Mollit", disabled: true },
-      { id: "item2", label: "Lorem ex Lorem" },
-      { id: "item3", label: "Veniam aliqua quis" },
-    ],
+    variant: "flat",
+    color: "base",
+    alignment: "evenly",
+    width: "full",
   },
 };
 
 export default meta;
-type Story = StoryObj<typeof UpwTabs>;
+type Story = StoryObj<typeof UwTabs>;
 
 // -----------------------------------------------------------------------------
 
-export const Base: Story = {};
+export const Base: Story = {
+  render: args => ({
+    components: { UwTabs, UwTabsTrigger, UwTabsList, UwTabsContent },
+    setup() {
+      const tabs = ["Tab 1", "Tab 2", "Tab 3", "Tab 4", "Tab 5"];
+      const colors = useSystemArgTypes.color;
+      return {
+        colors,
+        tabs,
+        args,
+      };
+    },
+    template: `
+      <div class="max-w-md">
+        <uw-tabs default-value="Tab 3" class="w-full">
+          <uw-tabs-list v-bind="args" class="w-full">
+            <uw-tabs-trigger v-bind="args" v-for="(tab, index) in tabs" :key="'tab' + index" :value="tab">
+              {{ tab }}
+            </uw-tabs-trigger>
+          </uw-tabs-list>
+          <uw-tabs-content v-for="(tab, index) in tabs" :key="'tab' + index" :value="tab" class="w-full">
+            <div class="bg-gray-100 rounded-lg h-44 flex items-center justify-center text-base-500">
+              {{ tab }} Content
+            </div>
+          </uw-tabs-content>
+        </uw-tabs>
+      </div>
+    `,
+  }),
+};
+
+export const Colors: Story = {
+  parameters: {
+    controls: { exclude: ["color"] },
+  },
+  render: args => ({
+    components: { UwTabs, UwTabsTrigger, UwTabsList, UwTabsContent },
+    setup() {
+      const tabs = ["Tab 1", "Tab 2", "Tab 3", "Tab 4"];
+      const colors = useSystemArgTypes.color;
+      return {
+        colors,
+        tabs,
+        args,
+      };
+    },
+    template: `
+      <div
+        v-for="color in colors.options"
+        :key="color"
+        class="my-12"
+      >
+        <uw-tabs :default-value="color + '2'">
+          <uw-tabs-list v-bind="args" :color="color">
+            <uw-tabs-trigger v-bind="args" :color="color" v-for="(tab, index) in 5" :key="color + index" :value="color + index" class="capitalize">
+              {{ color }} {{ index + 1 }}
+            </uw-tabs-trigger>
+          </uw-tabs-list>
+        </uw-tabs>
+      </div>
+    `,
+  }),
+};
