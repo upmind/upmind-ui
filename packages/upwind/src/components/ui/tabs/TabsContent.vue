6274ed7b9fb9020ab6fd75f3fd3f29c21284c212
Rhodri Jones <rhodri.jones@upmind.com>
2024-08-27 10:57:34 +0000

    Feature/shadcn-tabs

---
+++ packages/upwind/src/components/ui/tabs/TabsContent.vue
@@ -0,0 +1,30 @@
+<template>
+  <TabsContent :class="styles.tabs.content">
+    <slot />
+  </TabsContent>
+</template>
+
+<script lang="ts">
+// --- external
+import { TabsContent } from "radix-vue";
+
+// --- internal
+import config from "./tabs.config";
+
+// --- utils
+import { useStyles } from "../../../utils";
+
+export default {
+  components: {
+    TabsContent,
+  },
+
+  setup() {
+    const styles = useStyles("tabs", {}, config);
+
+    return {
+      styles,
+    };
+  },
+};
+</script>
