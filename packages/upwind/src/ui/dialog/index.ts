1a26fa3e64e65569327a6a7c80068e1a43457e77
RhodriJonesUpmind <rhodri.jones@upmind.com>
2024-08-30 10:42:43 +0100

    dialog close (has same issues as combobox, portal related I think)

--- packages/upwind/src/ui/dialog/index.ts
+++ packages/upwind/src/ui/dialog/index.ts
@@ -1,13 +1,6 @@
 // --- external
 import { defineCustomElement } from "vue";
 
-// --- components
-export { default as UwDialogConsolidated } from "./Dialog.ce.vue";
-
 // -- custom elements
 import DialogCE from "./Dialog.ce.vue";
 export const UwDialog = defineCustomElement(DialogCE);
-
-// -- custom elements
-import DialogCloseCE from "./DialogClose.ce.vue";
-export const UwDialogClose = defineCustomElement(DialogCloseCE);
