e49741f22d19e7ff1717e7fb81c6b4ec71bfb55b
RhodriJonesUpmind <rhodri.jones@upmind.com>
2024-09-02 13:08:33 +0100

    velia theme

--- playgrounds/storybook/stories/assets/themes/index.ts
+++ playgrounds/storybook/stories/assets/themes/index.ts
@@ -2,5 +2,6 @@
 import light from "./light";
 import retro from "./retro";
 import dark from "./dark";
+import velia from "./velia";
 // -----------------------------------------------------------------------------
-export default [light, dark, retro];
+export default [light, dark, retro, velia];
