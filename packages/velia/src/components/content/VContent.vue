3ee6bc53b45e6f66efbf0292ed12fbd25b0586a3
RhodriJonesUpmind <rhodri.jones@upmind.com>
2024-09-02 14:21:00 +0100

    v-hero-image padding

--- packages/velia/src/components/content/VContent.vue
+++ packages/velia/src/components/content/VContent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="-mt-16 ml-auto mr-auto max-w-6xl px-6 pb-8">
+  <div class="-mt-16 ml-auto mr-auto max-w-6xl px-6 pb-8 md:-mt-44">
     <slot />
   </div>
 </template>
--- packages/velia/src/components/hero/VHeroImage.vue
+++ packages/velia/src/components/hero/VHeroImage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :style="backgroundImage" class="pb-16">
+  <div :style="backgroundImage" class="pb-16 md:pb-64">
     <slot />
   </div>
 </template>
@@ -14,7 +14,7 @@ import coverImage from "../../assets/cover.jpeg";
 export default defineComponent({
   setup() {
     const backgroundImage = ref({
-      backgroundImage: `linear-gradient(rgba(20, 46, 77, 0.5), rgba(20, 46, 77, 1)), url(${coverImage})`,
+      backgroundImage: `linear-gradient(rgba(20, 46, 77, 0.75), rgba(20, 46, 77, 1)), url(${coverImage})`,
       backgroundSize: "cover",
       backgroundPosition: "center",
     });
