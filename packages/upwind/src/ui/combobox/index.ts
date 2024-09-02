1c2405b8e544e8e69633b28b5bcc5254062b442b
RhodriJonesUpmind <rhodri.jones@upmind.com>
2024-09-02 09:06:07 +0100

    combobox housekeeping

--- packages/upwind/src/ui/combobox/index.ts
+++ packages/upwind/src/ui/combobox/index.ts
@@ -4,7 +4,3 @@ import { defineCustomElement } from "vue";
 // --- custom elements
 import ComboboxCE from "./Combobox.ce.vue";
 export const UwCombobox = defineCustomElement(ComboboxCE);
-
-// --- custom elements
-import ComboboxItemCE from "./ComboboxItem.ce.vue";
-export const UwComboboxItem = defineCustomElement(ComboboxItemCE);
