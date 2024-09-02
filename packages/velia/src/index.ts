b713e2da2cd74533e5820cac9367634d1136f959
RhodriJonesUpmind <rhodri.jones@upmind.com>
2024-09-02 14:15:28 +0100

    seperate hero and hero content

--- packages/velia/src/index.ts
+++ packages/velia/src/index.ts
@@ -2,5 +2,6 @@ export { default as VHeader } from "./components/header/VHeader.vue";
 export { default as VFooter } from "./components/footer/VFooter.vue";
 export { default as VContent } from "./components/content/VContent.vue";
 export { default as VHero } from "./components/hero/VHero.vue";
+export { default as VHeroContent } from "./components/hero/VHeroContent.vue";
 
 export { default as VApp } from "./components/VApp.vue";
