20ff84620337a776151c0341d270f7eaaf50c211
RhodriJonesUpmind <rhodri.jones@upmind.com>
2024-09-02 12:07:28 +0100

    velia basic structure

---
+++ packages/velia/src/components/footer/VFooter.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="bg-primary flex items-center justify-between">
+    <img class="h-6" src="../../assets/logo-white.svg" alt="logo" />
+  </div>
+</template>
--- packages/velia/src/components/header/Header.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <div class="h-64 w-full bg-red-500 p-12 text-white">test</div>
-</template>
