67a6511a9eb6e60c2b5277064daf015470259598
Rui Posse <ruiposse@gmail.com>
2024-09-04 21:30:59 +0100

    chore(cicd): Upflow-vue first successful build

--- packages/flow-vue/vite.config.ts
+++ packages/flow-vue/vite.config.ts
@@ -3,7 +3,6 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import { configDefaults } from "vitest/config";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -16,8 +15,8 @@ export default defineConfig({
   build: {
     lib: {
       entry: fileURLToPath(new URL('./src/index.ts', import.meta.url)),
-      name: "upmind-vue",
-      fileName: "upmind-vue"
+      name: "@upmind/upflow-vue",
+      fileName: "upflow-vue"
     }
   },
   // Vitest config - https://vitest.dev/guide/#configuring-vitest
