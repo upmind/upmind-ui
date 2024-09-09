9a0eecec64d6f27759e02396cfae98db961d4f61
Rhodri Jones <rhodri.jones@upmind.com>
2024-09-09 11:55:13 +0100

    bring back upw avatar for now

--- packages/upwind/src/index.ts
+++ packages/upwind/src/index.ts
@@ -2,6 +2,7 @@
 
 // --- upw
 export { default as UpwAlert } from "./components/alert/Alert.vue";
+export { default as UpwAvatar } from "./components/avatar/Avatar.vue";
 export { default as UpwButton } from "./components/button/Button.vue";
 export { default as UpwCheckbox } from "./components/checkbox/Checkbox.vue";
 export { default as UpwCheckboxList } from "./components/checkbox/CheckboxList.vue";
