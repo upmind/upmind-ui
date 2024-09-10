031a896818ab8a603fbedc12d5e9f57801a23ca8
James Booth <james.booth@upmind.com>
2024-09-10 14:29:24 +0000

    Vitepress deploy tweaks

--- playgrounds/labs/src/plugins/upmind.ts
+++ playgrounds/labs/src/plugins/upmind.ts
@@ -1,5 +1,5 @@
 import type { App, Plugin } from "vue";
-import { useApi, useBrand, useSession, useSystem } from "@upmind/flow-vue";
+import { useApi, useBrand, useSession, useSystem } from "@upmind/upflow-vue";
 
 const upmindPlugin: Plugin = {
   install: (app: App): void => {
