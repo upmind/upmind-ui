81d5ca102e6ab63670547f57fa414e5298fdc506
Dominic Da Costa <dominic@thecollaboration.studio>
2024-09-11 13:02:25 +0200

    update button custom element to have access to internals and allow for form submit/reset

--- packages/upwind/src/ui/button/index.ts
+++ packages/upwind/src/ui/button/index.ts
@@ -2,5 +2,42 @@
 import { defineCustomElement } from "vue";
 
 // --- custom elements
-import ButtonCE from "./Button.ce.vue";
-export const UwButton = defineCustomElement(ButtonCE);
+import Button from "./Button.ce.vue";
+const ButtonCE = defineCustomElement(Button);
+
+// Buttons need to be form associated to be focusable
+// and to be able to submit forms
+export class UwButton extends ButtonCE {
+  static formAssociated = true;
+  _internals: ElementInternals;
+  constructor() {
+    super();
+    this._internals = this.attachInternals();
+  }
+  connectedCallback() {
+    // So this is a special case where we need to add an event listener
+    // to the button to handle form submission and reset
+    const form = this._internals.form;
+    if (!!form && this.getAttribute("type")) {
+      this.addEventListener("click", () => this.handleClick());
+    }
+    return super.connectedCallback();
+  }
+
+  private handleClick() {
+    const form = this._internals.form;
+    if (form) {
+      const type = this.getAttribute("type");
+      switch (type) {
+        case "submit":
+          form.requestSubmit();
+          break;
+        case "reset":
+          form.reset();
+          break;
+        default:
+          break;
+      }
+    }
+  }
+}
