8bf73a30c45e6691ac5d206e12c3800f124cbe6f
Rhodri Jones <rhodri.jones@upmind.com>
2024-09-26 10:18:24 +0100

    session component increase max width

--- packages/client-vue/src/components/session/config.cva.js
+++ packages/client-vue/src/components/session/config.cva.js
@@ -13,7 +13,7 @@ export default {
     footer: cva(
       "flex flex-col gap-2 text-sm font-light leading-tight tracking-tight"
     ),
-    content: cva("rounded-box w-full max-w-3xl items-start"),
+    content: cva("rounded-box w-full max-w-5xl items-start"),
 
     transition: {
       enter: {
