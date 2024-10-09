5fc7133c9c7091702eff9f80bbd6a095ae15faf5
Rhodri Jones <rhodri.jones@upmind.com>
2024-10-09 14:56:48 +0100

    slight shadow on button (we can override shadow classes in tailwind config when we don't want them, similar to border radius)

--- packages/upwind/src/ui/button/button.config.ts
+++ packages/upwind/src/ui/button/button.config.ts
@@ -3,7 +3,7 @@ import { cva } from "class-variance-authority";
 // -----------------------------------------------------------------------------
 
 export const buttonVariants = cva(
-  "relative inline-flex items-center justify-center whitespace-nowrap rounded-md border font-medium no-underline ring-offset-background transition focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:cursor-not-allowed disabled:!opacity-50 ",
+  "relative inline-flex items-center justify-center whitespace-nowrap rounded-md border font-medium no-underline shadow-sm ring-offset-background transition focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:cursor-not-allowed disabled:!opacity-50 ",
   {
     variants: {
       variant: {
