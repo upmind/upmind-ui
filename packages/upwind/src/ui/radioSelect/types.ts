d7795fe2b4cf7ae0f15bcfd351a762edbf1610b6
Rhodri Jones <rhodri.jones@upmind.com>
2024-10-10 10:01:38 +0100

    RadioSelect required flag

--- packages/upwind/src/ui/radioSelect/types.ts
+++ packages/upwind/src/ui/radioSelect/types.ts
@@ -28,6 +28,7 @@ export interface RadioSelectProps {
   avatar?: Partial<AvatarProps>;
   icon?: IconProps["icon"];
   placeholder?: string;
+  required?: boolean;
   // --- state
   items: RadioSelectItem[];
   modelValue?: string | RadioSelectItem;
