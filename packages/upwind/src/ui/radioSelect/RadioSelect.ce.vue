dd47a07a03b83e19281ac678936f0b80e14e4de9
Dominic Da Costa <dominic@thecollaboration.studio>
2024-10-15 17:26:16 +0200

    WIP : radio select as enhanced radio group

--- packages/upwind/src/ui/radioSelect/RadioSelect.ce.vue
+++ packages/upwind/src/ui/radioSelect/RadioSelect.ce.vue
@@ -1,5 +1,5 @@
 <template>
-  <Collapsible v-model:open="isOpen">
+  <Collapsible v-model:open="open">
     <CollapsibleTrigger class="group w-full">
       <Button
         :color="props.color"
@@ -9,16 +9,16 @@
       >
         <template #prepend>
           <Avatar
-            v-if="value?.avatar || props.avatar"
-            v-bind="value?.avatar ? value.avatar : props?.avatar"
+            v-if="selected?.avatar || props.avatar"
+            v-bind="selected?.avatar ? value.avatar : props?.avatar"
             size="3xs"
             shape="circle"
             fit="cover"
             aria-hidden="true"
           />
           <Icon
-            v-if="value?.icon || props.icon"
-            :icon="value?.icon ? value.icon : props?.icon"
+            v-if="selected?.icon || props.icon"
+            :icon="selected?.icon ? value.icon : props?.icon"
             shape="circle"
             size="3xs"
             fit="cover"
@@ -30,37 +30,33 @@
           class="flex w-full flex-col items-start justify-start text-left text-sm md:flex-row md:items-center md:justify-between md:space-x-4 md:space-y-0"
           :class="variants.radioSelect.content"
         >
-          <span
-            v-if="
-              value && value.label !== 'None' && value.label !== props.noneText
-            "
-            class="flex flex-col md:gap-y-0"
-          >
-            <span v-if="value?.label || props?.label" class="mb-1 leading-none">
-              {{ value?.label || props.label }}
+          <span v-if="selected" class="flex flex-col md:gap-y-0">
+            <span v-if="selected?.label || props?.label" class="mb-1">
+              {{ selected?.label || props.label }}
             </span>
 
             <span
-              v-if="value?.sublabel || props?.sublabel"
-              class="mb-1 leading-none opacity-50"
+              v-if="selected?.text || props?.text"
+              class="whitespace-normal leading-tight opacity-50"
             >
-              {{ value?.sublabel || props.sublabel }}
+              {{ selected?.text || props.text }}
             </span>
           </span>
+
           <span v-else class="text-sm opacity-50">
             {{ props.placeholder || "Select an option" }}
           </span>
 
           <span
-            v-if="value?.badge || props?.badge"
+            v-if="selected?.badge || props?.badge"
             class="flex md:items-center md:space-x-4"
           >
             <Badge
-              class="!m-0 !p-0 font-bold leading-none md:text-center"
+              class="!m-0 !p-0 font-bold md:text-center"
               :color="props.color"
               size="xs"
             >
-              {{ value?.badge || props.badge }}
+              {{ selected?.badge || props.badge }}
             </Badge>
           </span>
         </span>
@@ -68,72 +64,97 @@
         <template #append>
           <Icon
             class="opacity-50 transition-all duration-300 group-hover:opacity-100"
-            :class="{ 'rotate-180': isOpen }"
+            :class="{ 'rotate-180': !open }"
             icon="arrow-up"
             size="xs"
           />
         </template>
       </Button>
     </CollapsibleTrigger>
+
     <CollapsibleContent>
-      <Button
+      <RadioGroup
+        v-model="modelValue"
+        :default-value="defaultValue"
+        :class="variants.radioSelect.items"
+        @update:model-value="open = !open"
+      >
+        <div
+          :class="variants.radioSelect.item"
+          v-for="(item, index) in itemsList"
+          :key="`${props.name}-${index}`"
+        >
+          <RadioGroupItem
+            :id="`${props.name}-${index}`"
+            :value="item.value"
+            :name="props.name"
+            :class="variants.radioSelect.input"
+          />
+
+          <Label
+            :for="`${props.name}-${index}`"
+            :class="cn(variants.radioSelect.label, props.classLabel)"
+          >
+            <span
+              class="flex w-full flex-col items-start justify-start text-left text-sm md:flex-row md:justify-between md:space-x-4 md:space-y-0"
+              :class="variants.radioSelect.content"
+            >
+              <span class="flex w-full flex-col items-start">
+                <h5 v-if="item?.label" class="m-0 block">
+                  {{ item?.label }}
+                </h5>
+
+                <p
+                  v-if="item?.text"
+                  class="m-0 block whitespace-normal leading-tight opacity-50"
+                >
+                  {{ item?.text }}
+                </p>
+
+                <span class="flex items-center space-x-4 pr-2 md:hidden">
+                  <Badge
+                    class="text-center font-bold"
+                    :color="props.color"
+                    variant="tonal"
+                    size="xs"
+                  >
+                    {{ item?.badge }}
+                  </Badge>
+                </span>
+              </span>
+
+              <Badge
+                class="text-center font-bold"
+                :color="props.color"
+                variant="tonal"
+                size="xs"
+              >
+                {{ item?.badge }}
+              </Badge>
+            </span>
+          </Label>
+        </div>
+      </RadioGroup>
+
+      <!-- <Button
         v-for="(item, index) in itemsList"
-        :key="`radio-select-item-${index}`"
+        :key="`${props.name}-${index}`"
         variant="control"
         :color="props.color"
         :class="cn(variants.radioSelect.trigger, props.class)"
         class="border-t-0 !border-opacity-75"
         @click="doSelect(item)"
-      >
-        <span
-          class="flex w-full flex-col items-start justify-start space-y-1 text-left text-sm md:flex-row md:items-center md:justify-between md:space-x-4 md:space-y-0"
-          :class="variants.radioSelect.content"
-        >
-          <span class="flex flex-row items-center space-x-1">
-            <span class="-ml-1">
-              <UpwRadio :model-value="item.value === value?.value" />
-            </span>
-            <span class="flex flex-col">
-              <span v-if="item?.label" class="mb-1 leading-none">
-                {{ item?.label }}
-              </span>
+      > -->
 
-              <span v-if="item?.sublabel" class="mb-1 leading-none opacity-50">
-                {{ item?.sublabel }}
-              </span>
-
-              <span class="flex items-center space-x-4 pr-2 md:hidden">
-                <Badge
-                  class="text-center font-bold leading-none"
-                  :color="props.color"
-                  variant="tonal"
-                  size="xs"
-                >
-                  {{ item?.badge }}
-                </Badge>
-              </span>
-            </span>
-          </span>
-
-          <span class="hidden items-center space-x-4 pr-2 md:flex">
-            <Badge
-              class="text-center font-bold leading-none"
-              :color="props.color"
-              variant="tonal"
-              size="xs"
-            >
-              {{ item?.badge }}
-            </Badge>
-          </span>
-        </span>
-      </Button>
+      <!-- </Button> -->
     </CollapsibleContent>
   </Collapsible>
 </template>
 
 <script setup lang="ts">
 // ---external
-import { watch, ref, computed } from "vue";
+import { ref, computed } from "vue";
+import { useVModel } from "@vueuse/core";
 
 // --- internal
 import { cn, useStyles } from "../../utils";
@@ -147,23 +168,22 @@ import {
 } from "../collapsible";
 import { Button } from "../button";
 import { Icon } from "../icon";
-import UpwRadio from "../../components/radio/Radio.vue";
+import { RadioGroup, RadioGroupItem } from "../radio-group";
 
 // --- utils
-import { find, isString } from "lodash-es";
+import { find } from "lodash-es";
 
 // --- types
-import type { RadioSelectProps, RadioSelectItem } from "./types";
+import type { RadioSelectProps } from "./types";
 import type { ComputedRef } from "vue";
 
 // -----------------------------------------------------------------------------
 const props = withDefaults(defineProps<RadioSelectProps>(), {
   // --- props
-  label: "",
-  modelValue: "",
   loading: false,
   placeholder: "Select an option",
   required: false,
+  noneText: "None",
   // -- variants
   color: "base",
   variant: "control",
@@ -171,10 +191,13 @@ const props = withDefaults(defineProps<RadioSelectProps>(), {
   class: "",
 });
 
-const emit = defineEmits(["update:modelValue"]);
+const emits = defineEmits(["update:modelValue"]);
+const modelValue = useVModel(props, "modelValue", emits, {
+  passive: true,
+  defaultValue: props.defaultValue,
+});
 
-const isOpen = ref(false);
-const value: any = ref();
+const open = ref(false);
 
 const meta = computed(() => ({
   color: props.color,
@@ -191,25 +214,11 @@ const variants = useStyles(
 }>;
 
 const itemsList = computed(() => {
-  if (!props.required && value.value) {
-    return [...props.items, { value: null, label: props.noneText || "None" }];
+  if (!props.required && modelValue.value) {
+    return [...props.items, { value: null, label: props.noneText }];
   }
   return props.items;
 });
 
-// --- methods
-const doSelect = (item: String | RadioSelectItem) => {
-  const selected = isString(item) ? find(props.items, { value: item }) : item;
-  const hasChanged = selected?.value !== value.value;
-
-  if (hasChanged) {
-    value.value = selected;
-    emit("update:modelValue", item);
-  }
-  isOpen.value = false;
-};
-
-// --- side effect
-doSelect(props.modelValue);
-watch(() => props.modelValue, doSelect, { immediate: true });
+const selected = computed(() => find(props.items, { value: modelValue.value }));
 </script>
