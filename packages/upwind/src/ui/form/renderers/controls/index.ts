8f27c60a20e6923bce6296de490876ea1eea1acb
Rhodri Jones <rhodri.jones@upmind.com>
2024-10-15 14:11:24 +0100

    register url and email renderers

--- packages/upwind/src/ui/form/renderers/controls/index.ts
+++ packages/upwind/src/ui/form/renderers/controls/index.ts
@@ -16,6 +16,8 @@ import PasswordRenderer, {
 } from "./PasswordRenderer.vue";
 import NumberRenderer, { tester as NumberTest } from "./NumberRenderer.vue";
 import EnumRenderer, { tester as EnumTest } from "./EnumRenderer.vue";
+import UrlRenderer, { tester as UrlTest } from "./UrlRenderer.vue";
+import EmailRenderer, { tester as EmailTest } from "./EmailRenderer.vue";
 
 // -----------------------------------------------------------------------------
 
@@ -28,4 +30,6 @@ export const controlRenderers = [
   registerEntry(NumberRenderer, NumberTest),
   registerEntry(EnumRenderer, EnumTest),
   registerEntry(PasswordRenderer, PasswordTest),
+  registerEntry(UrlRenderer, UrlTest),
+  registerEntry(EmailRenderer, EmailTest),
 ];
