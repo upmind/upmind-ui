bb9ca2fa67c6598b27c2918bdb209d45540d751c
Rhodri Jones <rhodri.jones@upmind.com>
2024-10-18 14:35:10 +0100

    checkbox and radio group item polishes

--- packages/upwind/src/ui/checkbox-cards/checkboxCards.config.ts
+++ packages/upwind/src/ui/checkbox-cards/checkboxCards.config.ts
@@ -3,11 +3,13 @@ import { ringClasses, invalidRingClasses } from "../input/input.config";
 
 export default {
   checkboxCards: {
-    root: cva(`${ringClasses} ${invalidRingClasses} w-full gap-0`),
+    root: cva(
+      `${ringClasses} ${invalidRingClasses} w-full gap-0 border border-b-0 border-control`
+    ),
     item: cva(
-      "flex cursor-pointer items-start space-x-2 border border-control shadow-sm"
+      "flex !cursor-pointer items-start space-x-2 border-b border-control shadow-sm hover:bg-base-100"
     ),
     label: cva("m-0 h-full w-full rounded-md py-3 pr-6 text-xs "),
-    input: cva("my-4 ml-3 mr-1 leading-normal"),
+    input: cva("my-3 ml-3 mr-1 leading-normal"),
   },
 };
