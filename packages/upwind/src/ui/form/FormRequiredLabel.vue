16d0a90873ed8b45ec5f4338f6e239c34a569c76
Rhodri Jones <rhodri.jones@upmind.com>
2024-10-18 11:32:50 +0100

    form required label

---
+++ packages/upwind/src/ui/form/FormRequiredLabel.vue
@@ -0,0 +1,26 @@
+<template>
+  <Label
+    v-if="props.required"
+    :class="cn('!w-auto w-full text-right', props.class)"
+    :for="props.formItemId"
+  >
+    <slot />
+  </Label>
+</template>
+
+<script lang="ts" setup>
+import type { HTMLAttributes } from "vue";
+import { cn } from "../../utils";
+
+import type { LabelProps } from "radix-vue";
+import { Label } from "../label";
+
+const props = defineProps<
+  LabelProps & {
+    formItemId: string;
+    invalid?: boolean;
+    class?: HTMLAttributes["class"];
+    required?: boolean;
+  }
+>();
+</script>
