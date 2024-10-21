ce4490d33482b4be7807123695fd70692841fc9f
Rhodri Jones <rhodri.jones@upmind.com>
2024-10-21 09:14:08 +0100

    form layout renderers script setup

--- packages/upwind/src/ui/form/renderers/layouts/GroupRenderer.vue
+++ packages/upwind/src/ui/form/renderers/layouts/GroupRenderer.vue
@@ -20,9 +20,9 @@
   </fieldset>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 // --- external
-import { computed, defineComponent } from "vue";
+import { computed } from "vue";
 import { and, isLayout, uiTypeIs } from "@jsonforms/core";
 import {
   DispatchRenderer,
@@ -39,44 +39,31 @@ import { useStyles } from "../../../../utils";
 // --- types
 import type { PropType } from "vue";
 import type { Layout } from "@jsonforms/core";
-import type { RendererProps } from "@jsonforms/vue";
 import type { InputProps } from "../controls/types";
 
 // -------------------------------------------------------------------
 
-export default defineComponent({
-  name: "GroupRenderer",
-  components: {
-    DispatchRenderer,
+const props = defineProps({
+  ...rendererProps<Layout>(),
+  // ---  Additional Attributes
+  size: {
+    type: String as PropType<InputProps["size"]>,
+    default: null,
   },
-  props: {
-    ...rendererProps<Layout>(),
-    // ---  Additional Attributes
-    size: {
-      type: String as PropType<InputProps["size"]>,
-      default: null,
-    },
-    // --- Provide a way to add custom styles for a specific instance of the component
-    upwindConfig: { type: [Object, Array], default: () => ({}) },
-  },
-  setup(props: RendererProps<Layout>) {
-    const meta = computed(() => ({
-      isVisible: renderer.layout.value.visible,
-      isDisabled: !renderer.layout.value.enabled,
-    }));
-
-    const styles = useStyles(["group"], meta, config, props.upwindConfig);
-    const renderer = useUpwindLayoutRenderer(useJsonFormsLayout(props));
-    // we dont process styles as  we are using an upwind control, so rather pass the configs and allow the control to handle it
-    return {
-      ...renderer,
-      meta,
-      styles,
-      config, // pass the config to the  component
-    };
-  },
+  // --- Provide a way to add custom styles for a specific instance of the component
+  upwindConfig: { type: [Object, Array], default: () => ({}) },
 });
 
+const meta = computed(() => ({
+  isVisible: layout.value.visible,
+  isDisabled: !layout.value.enabled,
+}));
+
+const styles = useStyles(["group"], meta, config, props.upwindConfig);
+const { layout } = useUpwindLayoutRenderer(useJsonFormsLayout(props));
+</script>
+
+<script lang="ts">
 export const tester = {
   rank: 2,
   controlType: and(isLayout, uiTypeIs("Group")),
