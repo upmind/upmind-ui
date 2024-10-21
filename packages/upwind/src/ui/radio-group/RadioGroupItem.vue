217897f5fd8a0c06b94d39f7f93cd771e9caf26e
Rhodri Jones <rhodri.jones@upmind.com>
2024-10-21 16:56:11 +0100

    radio select hover and cursor-pointer

--- packages/upwind/src/ui/radio-group/RadioGroupItem.vue
+++ packages/upwind/src/ui/radio-group/RadioGroupItem.vue
@@ -4,7 +4,8 @@
     :class="
       cn(
         'aspect-square h-4 w-4 border border-control text-primary ring-offset-background focus:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
-        radioGroupItemVariants({ required })
+        radioGroupItemVariants({ required }),
+        props.class
       )
     "
   >
