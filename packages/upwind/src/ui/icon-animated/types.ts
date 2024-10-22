213466591b0333ac7cb4d09d97cfa69ef4d99236
Rhodri Jones <rhodri.jones@upmind.com>
2024-10-22 20:52:58 +0100

    animated icon config

--- packages/upwind/src/ui/icon-animated/types.ts
+++ packages/upwind/src/ui/icon-animated/types.ts
@@ -1,6 +1,19 @@
+import { type HTMLAttributes } from "vue";
+import type { VariantProps } from "class-variance-authority";
+
+// --- internal
+import type { iconVariants } from "./iconAnimated.config";
+type IconVariantProps = VariantProps<typeof iconVariants>;
+
+// --- types
 export interface AnimatedIconProps {
   icon: string;
   trigger?: string;
   primaryColor?: string;
   secondaryColor?: string;
+  // ---
+  size?: IconVariantProps["size"];
+  // ---
+  upwindConfig?: { icon: Partial<AnimatedIconProps> };
+  class?: HTMLAttributes["class"];
 }
