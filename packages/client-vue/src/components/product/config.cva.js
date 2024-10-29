b56027e014337fb918f9591c769cae0bc1b932c5
Rhodri Jones <rhodri.jones@upmind.com>
2024-10-29 21:39:18 +0000

    not found cva consistency tweaks

--- packages/client-vue/src/components/product/config.cva.js
+++ packages/client-vue/src/components/product/config.cva.js
@@ -305,12 +305,12 @@ export default {
 
     notFound: {
       root: cva(
-        "relative flex w-full flex-col flex-wrap items-center justify-center gap-6 py-16"
+        "relative flex w-full flex-col flex-wrap items-center justify-center gap-8 py-16"
       ),
-      title: cva("m-0 text-center text-3xl font-light text-inherit"),
-      text: cva(
-        "text-base-500 m-0 text-center text-sm leading-5 tracking-tight"
-      ),
+      title: cva("m-0 text-center text-3xl text-inherit"),
+      text: cva("m-0 text-center text-lg leading-5 tracking-tight opacity-50"),
+      avatar: cva("bg-primary text-primary-foreground size-20 p-2"),
+      actions: cva("flex w-full justify-center"),
     },
   },
 };
