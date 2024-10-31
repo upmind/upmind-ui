78c9c93257e420accd5418055cc7221f48731cc3
Rhodri Jones <rhodri.jones@upmind.com>
2024-10-31 18:37:39 +0000

    state max-w-md

--- packages/client-vue/src/components/order/config.cva.js
+++ packages/client-vue/src/components/order/config.cva.js
@@ -9,7 +9,9 @@ export default {
         "relative flex w-full flex-col flex-wrap items-center justify-center gap-1 px-4 py-16 md:px-8"
       ),
       title: cva("m-0 mt-3 text-center text-3xl text-inherit"),
-      text: cva("m-0 mb-8 text-center text-lg leading-normal opacity-50"),
+      text: cva(
+        "m-0 mb-8 max-w-md text-center text-lg leading-normal opacity-50"
+      ),
       avatar: cva("bg-primary text-primary-foreground size-20 p-2"),
       actions: cva("flex w-full justify-center"),
     },
