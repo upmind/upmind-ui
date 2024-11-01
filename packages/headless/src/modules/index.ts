dc47a2becc4c0582f420d4df5b87b1c309f6d4b4
Dominic Da Costa <dominic@thecollaboration.studio>
2024-11-01 14:05:19 +0200

    expose productModel types

--- packages/headless/src/modules/index.ts
+++ packages/headless/src/modules/index.ts
@@ -3,8 +3,8 @@ export * from "./system";
 export * from "./brand";
 export * from "./session";
 export * from "./basket";
+export * from "./product";
 export * from "./domain";
 export * from "./feedback";
 export * from "./client";
-
 // export * from "./locale";
