20c777ddd98803b0d91fe45d125f6663ea7f6844
Rhodri Jones <rhodri.jones@upmind.com>
2024-11-05 16:58:25 +0000

    disable pointer events none for now

--- packages/upwind/src/ui/dialog/Dialog.ce.vue
+++ packages/upwind/src/ui/dialog/Dialog.ce.vue
@@ -138,23 +138,23 @@ const onOpen = (open: boolean, force: boolean = false) => {
 };
 
 onMounted(() => {
-  if (props.to) {
-    nextTick(() => {
-      const element = document.querySelector(props.to as string);
-      if (element) {
-        document.body.style.setProperty("pointer-events", "auto");
-        (element as HTMLElement).style.setProperty("pointer-events", "none");
-      }
-    });
-  }
+  // if (props.to) {
+  //   nextTick(() => {
+  //     const element = document.querySelector(props.to as string);
+  //     if (element) {
+  //       document.body.style.setProperty("pointer-events", "auto");
+  //       (element as HTMLElement).style.setProperty("pointer-events", "none");
+  //     }
+  //   });
+  // }
 });
 
 onUnmounted(() => {
-  if (props.to) {
-    const element = document.querySelector(props.to as string);
-    document.body.style.setProperty("pointer-events", "auto");
-    (element as HTMLElement)?.style.setProperty("pointer-events", "auto");
-  }
+  // if (props.to) {
+  //   const element = document.querySelector(props.to as string);
+  //   document.body.style.setProperty("pointer-events", "auto");
+  //   (element as HTMLElement)?.style.setProperty("pointer-events", "auto");
+  // }
 });
 
 const forceClose = () => {
