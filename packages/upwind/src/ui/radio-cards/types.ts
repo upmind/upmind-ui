519e3e46e56f52e8598a4df5289e19ee2a7cd3fd
Rhodri Jones <rhodri.jones@upmind.com>
2024-11-12 21:44:44 +0000

    radio ring variant

--- packages/upwind/src/ui/radio-cards/types.ts
+++ packages/upwind/src/ui/radio-cards/types.ts
@@ -25,6 +25,7 @@ export interface RadioCardsProps extends RadioGroupRootProps {
   color?: ButtonProps["color"];
   variant?: ButtonProps["variant"];
   layout?: RadioCardsVariantProps["layout"];
+  ring?: boolean;
   // ---
   upwindConfig?: { tooltip: Partial<RadioCardsProps> };
   class?: HTMLAttributes["class"];
