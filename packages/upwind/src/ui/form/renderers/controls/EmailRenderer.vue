a48abc42512c5993c7cc94100031bb7ebb68a10c
Rhodri Jones <rhodri.jones@upmind.com>
2024-11-13 13:21:31 +0000

    email renderer remove min max

--- packages/upwind/src/ui/form/renderers/controls/EmailRenderer.vue
+++ packages/upwind/src/ui/form/renderers/controls/EmailRenderer.vue
@@ -2,12 +2,8 @@
   <FormField v-bind="formFieldProps">
     <Input
       type="email"
-      :max="safeMax"
-      :min="safeMin"
       :placeholder="appliedOptions?.placeholder"
       :autocomplete="appliedOptions?.autocomplete"
-      :maxlength="appliedOptions?.maxLength"
-      :minlength="appliedOptions?.minLength"
       :disabled="appliedOptions?.disabled"
       :model-value="control.data"
       @update:modelValue="onInput"
@@ -17,21 +13,17 @@
 
 <script lang="ts" setup>
 // --- external
-import { computed } from "vue";
 import { useJsonFormsControl } from "@jsonforms/vue";
 
 // --- components
 import FormField from "../../FormField.vue";
 import { Input } from "../../../input";
-import { isNil } from "lodash";
-
 // --- utils
 import { useUpwindRenderer } from "../utils";
 
 // --- types
 import type { ControlElement } from "@jsonforms/core";
 import type { RendererProps } from "@jsonforms/vue";
-import type { ComputedRef } from "vue";
 // ----------------------------------------------
 
 const props = defineProps<RendererProps<ControlElement>>();
@@ -39,26 +31,6 @@ const props = defineProps<RendererProps<ControlElement>>();
 const { control, formFieldProps, onInput, appliedOptions } = useUpwindRenderer(
   useJsonFormsControl(props)
 );
-
-const safeMin: ComputedRef<number | undefined> = computed(() => {
-  const applied = appliedOptions.value?.minLength;
-  if (!isNil(applied)) return applied;
-
-  const minimum = control.value?.schema?.minLength;
-  if (!isNil(minimum)) return minimum;
-
-  return undefined;
-});
-
-const safeMax: ComputedRef<number | undefined> = computed(() => {
-  const applied = appliedOptions.value?.maxLength;
-  if (!isNil(applied)) return applied;
-
-  const maximum = control.value?.schema?.maxLength;
-  if (!isNil(maximum)) return maximum;
-
-  return undefined;
-});
 </script>
 
 <script lang="ts">
