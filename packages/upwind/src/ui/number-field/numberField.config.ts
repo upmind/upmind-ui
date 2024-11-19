1f4bd11dfb57594bafe6e2c61ad6522a9008eb76
Rhodri Jones <rhodri.jones@upmind.com>
2024-11-19 13:13:04 +0000

    number field sizing improvements

--- packages/upwind/src/ui/number-field/numberField.config.ts
+++ packages/upwind/src/ui/number-field/numberField.config.ts
@@ -12,6 +12,11 @@ export const numberFieldRootVariants = cva("group inline-block", {
       full: "w-full",
       app: "w-app",
     },
+    size: {
+      sm: "text-sm",
+      md: "text-md",
+      lg: "text-lg",
+    },
   },
   defaultVariants: {
     width: "full",
@@ -19,30 +24,24 @@ export const numberFieldRootVariants = cva("group inline-block", {
 });
 
 export const numberFieldVariants = cva(
-  "bg-control-background group-aria-invalid:!ring-invalid group-aria-invalid:!ring-2 group-aria-invalid:!ring-offset-2 focus-visible:ring-ring inline-flex w-full rounded-md text-center font-medium text-control-foreground ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+  "group-aria-invalid:!ring-invalid group-aria-invalid:!ring-2 group-aria-invalid:!ring-offset-2 focus-visible:ring-ring inline-flex w-full rounded-md bg-transparent text-center font-medium text-control-foreground ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
   {
     variants: {
       variant: {
-        flat: "border border-control",
-        minimal: "!border-none",
-      },
-      size: {
-        sm: "h-8 px-3 py-2 text-sm",
-        md: "h-10 px-3 py-2 text-md",
-        lg: "h-12 px-3 py-2 text-lg",
+        flat: "h-10 border border-control",
+        minimal: "h-4 !border-none",
       },
     },
     defaultVariants: {
-      size: "md",
       variant: "flat",
     },
   }
 );
 
-export const numberFieldInputVariants = cva("", {
+export const numberFieldInputVariants = cva("bg-control-background", {
   variants: {
     variant: {
-      flat: "",
+      flat: "h-10 border border-control",
       minimal: "rounded-lg border border-control p-0.5",
     },
   },
