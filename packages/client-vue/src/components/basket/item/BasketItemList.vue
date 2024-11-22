6913104fde616d2895771ebf98bf5cb3f548f1a0
Rhodri Jones <rhodri.jones@upmind.com>
2024-11-22 16:02:42 +0000

    basket item wip

--- packages/client-vue/src/components/basket/item/BasketItemList.vue
+++ packages/client-vue/src/components/basket/item/BasketItemList.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="!meta.isLoading" class="flex flex-col space-y-4">
-    <template v-for="product in products" :key="product.id">
+    <template v-for="product in data" :key="product.id">
       <UpmBasketItem
         v-bind="product"
         :open="!!open[product.id]"
@@ -12,8 +12,9 @@
 
 <script lang="ts" setup>
 // --- external
-import { ref, watch } from "vue";
+import { ref, watch, computed } from "vue";
 import { vAutoAnimate } from "@formkit/auto-animate";
+import { useRoute } from "vue-router";
 
 // --- internal
 import { useBasket } from "@upmind-automation/client-vue";
@@ -31,6 +32,92 @@ const emits = defineEmits(["update:open"]);
 const { meta, products } = useBasket();
 
 const open = ref<Record<string, boolean>>(forceOpen(props.open));
+
+const route = useRoute();
+const useMockData = computed(() => "mock" in route.query);
+
+const data = computed(() => {
+  if (useMockData.value) {
+    return [
+      {
+        id: "d6325079-8065-d1e0-67df-8174e234e98d",
+        quantity: 1,
+        productId: "3de78642-de53-9714-542c-21208469530d",
+        term: 0,
+        options: {
+          "20403869-6e54-721d-4ea5-18d9305e7d23": {
+            "78985742-6489-7012-0e4c-21e325d0ed36": {
+              productId: "78985742-6489-7012-0e4c-21e325d0ed36",
+              quantity: 1,
+              cycle: 0,
+            },
+          },
+        },
+        attributes: {},
+        provisionFields: [],
+        product: {
+          name: "Upmind License",
+          serviceIdentifier: "hosting.com",
+          category: "Software",
+          description: "Development - Dev Blocks",
+          excerpt: "__vue_devtool_undefined__",
+          id: "3de78642-de53-9714-542c-21208469530d",
+          imgUrl: "__vue_devtool_undefined__",
+          quantifiable: false,
+          step: 1,
+          min: 0,
+          max: "__vue_devtool_infinity__",
+        },
+        summary: {
+          pricing: [
+            {
+              name: "Monthly",
+              category: "Billing Cycle",
+              serviceIdentifier: null,
+              cycle: 1,
+              quantity: 1,
+              meta: { oneoff: false, discounted: true, free: false },
+              regularAmount: 5000,
+              regularPrice: "£50.00",
+              currentAmount: 4500,
+              currentPrice: "£45.00",
+              key: "term",
+            },
+          ],
+          details: [
+            {
+              name: "Monthly",
+              category: "Billing Cycle",
+              serviceIdentifier: null,
+              cycle: 1,
+              quantity: 1,
+              meta: { oneoff: false, discounted: true, free: false },
+              regularAmount: 5000,
+              regularPrice: "£50.00",
+              currentAmount: 4500,
+              currentPrice: "£45.00",
+              key: "term",
+            },
+            {
+              name: "Easy-to-implement",
+              category: "Bundle",
+              serviceIdentifier: null,
+              cycle: 0,
+              quantity: 1,
+              meta: { oneoff: false, discounted: false, free: false },
+              regularAmount: 1500,
+              regularPrice: "$1,500.00",
+              currentAmount: 1500,
+              currentPrice: "$1,500.00",
+              key: "option",
+            },
+          ],
+        },
+      },
+    ];
+  }
+  return products.value;
+});
 
 function forceOpen(value: boolean = false): Record<string, boolean> {
   return reduce(
