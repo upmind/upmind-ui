72120af80736545fbbe21de8d7690051e97052d5
Dominic Da Costa <dominic@thecollaboration.studio>
2024-11-22 11:04:21 +0200

    take II

--- packages/headless/src/modules/product/product.machine.ts
+++ packages/headless/src/modules/product/product.machine.ts
@@ -329,7 +329,7 @@ export default createMachine(
           valid: {},
           configured: {
             entry: ["cancelCalculation", "setSummaryWithBasketProduct"],
-            final: true,
+            type: "final",
           },
           error: {},
         },
