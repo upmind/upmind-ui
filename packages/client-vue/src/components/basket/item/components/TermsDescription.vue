6f86d1aad14a40158cabd42f608390c4f755e1a1
Rhodri Jones <rhodri.jones@upmind.com>
2024-11-25 14:31:43 +0000

    terms description logic

--- packages/client-vue/src/components/basket/item/components/TermsDescription.vue
+++ packages/client-vue/src/components/basket/item/components/TermsDescription.vue
@@ -1,7 +1,18 @@
 <template>
-  <span class="text-sm italic leading-5 opacity-50">{{
-    t(`product.terms.${pricing.cycle}`, [pricing.currentPrice])
-  }}</span>
+  <span class="text-sm italic leading-5 opacity-50">
+    <span>{{
+      t(
+        `product.terms.${mobile ? "minified" : "descriptive"}.${pricing.cycle}`,
+        [
+          pricing.cycle && pricing?.cycle >= 12
+            ? pricing.regularPrice
+            : pricing.currentPrice,
+        ]
+      )
+    }}</span
+    ><span v-if="true">, {{ t("product.terms.taxes") }}.</span>
+    <span v-else>.</span>
+  </span>
 </template>
 
 <script lang="ts" setup>
@@ -15,5 +26,6 @@ const { t } = useI18n();
 
 defineProps<{
   pricing: BasketProductSummaryPrice;
+  mobile?: boolean;
 }>();
 </script>
