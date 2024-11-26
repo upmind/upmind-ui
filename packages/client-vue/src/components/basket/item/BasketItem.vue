27ae5d64017d84ac38132ab28423b57402075c19
Rhodri Jones <rhodri.jones@upmind.com>
2024-11-26 17:06:01 +0000

    processing and loading state

--- packages/client-vue/src/components/basket/item/BasketItem.vue
+++ packages/client-vue/src/components/basket/item/BasketItem.vue
@@ -1,44 +1,46 @@
 <template>
-  <UpmCard
-    class="relative flex flex-col gap-y-4 !p-8 !py-7"
-    :class="[
-      !meta.isProcessing && (meta.hasErrors || error)
-        ? 'ring-error !ring-error-1 ring-1'
-        : 'ring-offset-background focus-within:ring-ring focus-within:outline-none focus-within:ring-1 focus-within:ring-offset-1 group-focus-within:ring-0 group-focus-within:ring-offset-0',
-    ]"
-  >
-    <UpmBasketItemSummary
-      v-for="(pricing, index) in summary.pricing"
-      :key="`${props.id}-${index}`"
-      :id="id"
-      :product="product"
-      :pricing="pricing"
-      :quantity="quantity"
-      :error="error"
-      :is-first="index === 0"
-      :is-last="index === summary.pricing.length - 1"
-    />
-
-    <!-- These margins help us position correctly (there is additional space when the details are closed) -->
-    <div v-auto-animate class="-my-2">
-      <UpmBasketConfigurationDetails
-        v-if="open"
+  <Loading :active="meta.isLoading || meta.isProcessing" class="text-secondary">
+    <UpmCard
+      class="relative flex flex-col gap-y-4 !p-8 !py-7"
+      :class="[
+        !meta.isProcessing && (meta.hasErrors || error)
+          ? 'ring-error !ring-error-1 ring-1'
+          : 'ring-offset-background focus-within:ring-ring focus-within:outline-none focus-within:ring-1 focus-within:ring-offset-1 group-focus-within:ring-0 group-focus-within:ring-offset-0',
+      ]"
+    >
+      <UpmBasketItemSummary
+        v-for="(pricing, index) in summary.pricing"
+        :key="`${props.id}-${index}`"
         :id="id"
-        :details="summary.details"
         :product="product"
-        class="-mb-2 mt-2"
+        :pricing="pricing"
+        :quantity="quantity"
+        :error="error"
+        :is-first="index === 0"
+        :is-last="index === summary.pricing.length - 1"
       />
-    </div>
 
-    <UpmBasketItemActions
-      v-model:isOpen="open"
-      :id="props.id"
-      :details="summary.details"
-      :is-disabled="meta.isProcessing || meta.isLoading"
-      :remove="remove"
-      details-button-label="Show details"
-    />
-  </UpmCard>
+      <!-- These margins help us position correctly (there is additional space when the details are closed) -->
+      <div v-auto-animate class="-my-2">
+        <UpmBasketConfigurationDetails
+          v-if="open"
+          :id="id"
+          :details="summary.details"
+          :product="product"
+          class="-mb-2 mt-2"
+        />
+      </div>
+
+      <UpmBasketItemActions
+        v-model:isOpen="open"
+        :id="props.id"
+        :details="summary.details"
+        :is-disabled="meta.isProcessing || meta.isLoading"
+        :remove="remove"
+        details-button-label="Show details"
+      />
+    </UpmCard>
+  </Loading>
 </template>
 
 <script lang="ts" setup>
@@ -53,6 +55,7 @@ import { useBasketProduct } from "@upmind-automation/client-vue";
 
 // --- components
 import { UpmCard } from "@upmind-automation/client-vue";
+import { Loading } from "@upmind-automation/upwind";
 import UpmBasketConfigurationDetails from "./BasketItemConfigurationDetails.vue";
 import UpmBasketItemSummary from "./BasketItemSummary.vue";
 import UpmBasketItemActions from "./BasketItemActions.vue";
