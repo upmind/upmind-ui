bc488a9e587549c4831068297c6da935b650f0ca
Rhodri Jones <rhodri.jones@upmind.com>
2024-11-28 11:01:23 +0000

    group details and render multiple lines by group

--- packages/client-vue/src/components/basket/product/BasketProductConfigurationDetails.vue
+++ packages/client-vue/src/components/basket/product/BasketProductConfigurationDetails.vue
@@ -2,27 +2,33 @@
   <div
     class="flex flex-col divide-y divide-solid rounded-lg border bg-gray-50/50 p-4 px-5 text-sm font-medium leading-[14px]"
   >
-    <template v-for="(item, index) in details" :key="'details-' + index">
-      <DetailsItem
-        :id="id"
-        v-bind="item"
-        :free="item.meta?.free"
-        :invalid="item.meta?.invalid"
-        :overrides="item.meta?.overrides"
-      />
-    </template>
+    <DetailsGroup
+      v-for="(group, index) in groupedDetails"
+      :key="'details-group-' + index"
+      :id="id"
+      :category="first(group)?.category"
+      :items="group"
+    />
   </div>
 </template>
 
 <script lang="ts" setup>
+// --- external
+import { groupBy, first } from "lodash-es";
+import { computed } from "vue";
+
 // --- components
-import DetailsItem from "./components/DetailsItem.vue";
+import DetailsGroup from "./components/DetailsGroup.vue";
 
 // --- types
 import type { BasketProductSummaryDetail } from "@upmind-automation/client-vue";
 
-defineProps<{
+const props = defineProps<{
   id: string;
   details: BasketProductSummaryDetail[];
 }>();
+
+const groupedDetails = computed(() => {
+  return groupBy(props.details, "category");
+});
 </script>
--- packages/client-vue/src/components/basket/product/components/DetailsItem.vue
+++ packages/client-vue/src/components/basket/product/components/DetailsItem.vue
@@ -1,56 +1,39 @@
 <template>
-  <div class="flex flex-col gap-y-2 py-3 first:pt-0 last:pb-0">
-    <div class="truncate opacity-35">
-      {{ category }}
-    </div>
+  <div class="flex justify-between">
+    <!-- Padding and minues margin avoids text from being cut off from overflow and keeps position -->
+    <span class="-my-1 truncate py-1">
+      {{ name }}
 
-    <div class="flex justify-between">
-      <div>
-        <span v-if="!invalid" class="truncate">
-          {{ name }}
+      <template v-if="quantity && quantity > 1">
+        ({{ t("product.configurationQuantity") }}{{ quantity }})
+      </template>
+    </span>
 
-          <template v-if="quantity && quantity > 1">
-            ({{ t("product.configurationQuantity") }}{{ quantity }})
-          </template>
-        </span>
-        <template v-else>
-          <router-link :to="editLink">
-            <Button
-              variant="link"
-              size="sm"
-              :label="t('product.configureNow')"
-              color="error"
-              class="inline-block h-5 underline"
-            />
-          </router-link>
-        </template>
-      </div>
-      <div v-if="currentPrice" class="flex items-center gap-x-1">
-        <template v-if="!free">
-          <template v-if="pricingKey !== 'term'">
-            <span v-if="showPlusIcon">
-              <Icon icon="plus" size="4xs" class="-mt-[2px] mr-1" />
-            </span>
-            <span v-else-if="overrides">
-              <Tooltip
-                :label="t('product.overridden')"
-                color="primary"
-                class="max-w-64 text-center"
-              >
-                <Icon size="3xs" icon="random" class="mr-1" />
-              </Tooltip>
-            </span>
-          </template>
-          <span>{{ currentPrice }}</span>
-          <span v-if="showTermLabel">
-            {{ t(`product.terms.term.${cycle}`) }}
+    <div v-if="currentPrice" class="flex items-center gap-x-1">
+      <template v-if="!free">
+        <template v-if="pricingKey !== 'term'">
+          <span v-if="showPlusIcon">
+            <Icon icon="plus" size="4xs" class="-mt-[2px] mr-1" />
+          </span>
+          <span v-else-if="overrides">
+            <Tooltip
+              :label="t('product.overridden')"
+              color="primary"
+              class="max-w-64 text-center"
+            >
+              <Icon size="3xs" icon="random" class="mr-1" />
+            </Tooltip>
           </span>
         </template>
+        <span>{{ currentPrice }}</span>
+        <span v-if="showTermLabel">
+          {{ t(`product.terms.term.${cycle}`) }}
+        </span>
+      </template>
 
-        <template v-else>
-          <span>{{ t("product.free") }}</span>
-        </template>
-      </div>
+      <template v-else>
+        <span>{{ t("product.free") }}</span>
+      </template>
     </div>
   </div>
 </template>
@@ -59,32 +42,23 @@
 // --- external
 import { computed } from "vue";
 import { useI18n } from "vue-i18n";
-import { Button, Icon, Tooltip } from "@upmind-automation/upwind";
+import { Icon, Tooltip } from "@upmind-automation/upwind";
 
 // --- types
 interface Props {
   id: string;
-  category: string;
   name: string;
   quantity?: number;
   currentPrice?: string;
   currentAmount?: number;
   overrides?: boolean;
-  invalid?: boolean;
   cycle?: number;
   pricingKey?: string;
   free?: boolean;
 }
 
-// --- props & composables
 const props = defineProps<Props>();
 const { t } = useI18n();
-
-// --- computed
-const editLink = computed(() => ({
-  name: "productEdit",
-  params: { bpid: props.id },
-}));
 
 const showPlusIcon = computed(
   () => !props.overrides && props.currentAmount && props.currentAmount > 0
