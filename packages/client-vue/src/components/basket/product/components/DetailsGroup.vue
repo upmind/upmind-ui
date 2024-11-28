5768d0a25ca23bc4c35aef40b22ecf1fdbfe3430
Rhodri Jones <rhodri.jones@upmind.com>
2024-11-28 11:01:23 +0000

    group details and render multiple lines by group

---
+++ packages/client-vue/src/components/basket/product/components/DetailsGroup.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="flex flex-col gap-y-2 py-3 first:pt-0 last:pb-0">
+    <div class="truncate opacity-35">
+      {{ category }}
+    </div>
+
+    <template v-if="!invalid">
+      <template v-for="(item, index) in items" :key="'details-item-' + index">
+        <DetailsItem
+          :id="id"
+          v-bind="item"
+          :free="item.meta?.free"
+          :overrides="item.meta?.overrides"
+        />
+      </template>
+    </template>
+
+    <template v-else>
+      <router-link :to="editLink">
+        <Button
+          variant="link"
+          size="sm"
+          :label="t('product.configureNow')"
+          color="error"
+          class="inline-block h-5 underline"
+        />
+      </router-link>
+    </template>
+  </div>
+</template>
+
+<script lang="ts" setup>
+// --- external
+import { computed } from "vue";
+import { useI18n } from "vue-i18n";
+import { Button } from "@upmind-automation/upwind";
+import { some } from "lodash-es";
+
+// --- components
+import DetailsItem from "./DetailsItem.vue";
+
+// --- types
+import type { BasketProductSummaryDetail } from "@upmind-automation/client-vue";
+
+const props = defineProps<{
+  id: string;
+  category?: string;
+  items: BasketProductSummaryDetail[];
+}>();
+
+const { t } = useI18n();
+
+const editLink = computed(() => ({
+  name: "productEdit",
+  params: { bpid: props.id },
+}));
+
+const invalid = computed(() => some(props.items, "meta.invalid"));
+</script>
