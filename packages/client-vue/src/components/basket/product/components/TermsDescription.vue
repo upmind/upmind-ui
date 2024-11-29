0a072a6ea113c143035e7c81efcb888223aa0ae5
Rhodri Jones <rhodri.jones@upmind.com>
2024-11-29 13:30:39 +0000

    terms description remove spacing before .

--- packages/client-vue/src/components/basket/product/components/TermsDescription.vue
+++ packages/client-vue/src/components/basket/product/components/TermsDescription.vue
@@ -1,7 +1,7 @@
 <template>
   <p v-if="!free" class="my-0 text-sm italic leading-5 opacity-35">
-    {{ t(`product.terms.billing.${cycle}`, [currentPrice]) }}
-    <template v-if="discounted && !oneOff"
+    {{ t(`product.terms.billing.${cycle}`, [currentPrice])
+    }}<template v-if="discounted && !oneOff"
       >. {{ t("product.terms.renews", [regularPrice]) }}
     </template>
     <template v-if="taxes">{{ t("product.terms.taxes") }}</template>
