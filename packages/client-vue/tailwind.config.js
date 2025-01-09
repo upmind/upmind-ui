b48c63ef9a527e6b1450cc19e509d23d319f70b9
Dominic Da Costa <dominic@thecollaboration.studio>
2025-01-09 11:36:48 +0200

    added logic for refreshing basket, handle errors retrieving products,

--- packages/client-vue/tailwind.config.js
+++ packages/client-vue/tailwind.config.js
@@ -2,4 +2,29 @@
 module.exports = {
   content: ["./src/**/*.{vue,js,ts}"],
   plugins: ["prettier-plugin-tailwindcss"],
+
+  extend: {
+    keyframes: {
+      fadeIn: {
+        from: { opacity: 0 },
+        to: { opacity: 1 },
+      },
+      "collapsible-down": {
+        from: { height: 0, opacity: 0 },
+        to: { height: "var(--radix-collapsible-content-height)", opacity: 1 },
+      },
+      "collapsible-up": {
+        from: {
+          height: "var(--radix-collapsible-content-height)",
+          opacity: 1,
+        },
+        to: { height: 0, opacity: 0 },
+      },
+    },
+    animation: {
+      fade: "fadeIn .5s ease-in-out",
+      "collapsible-down": "collapsible-down 0.3s ease-in-out",
+      "collapsible-up": "collapsible-up 0.3s ease-in-out",
+    },
+  },
 };
