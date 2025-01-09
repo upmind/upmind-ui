64d93e65c614c86b9aebdebb72308cb493dc88cb
Rhodri Jones <rhodri.jones@upmind.com>
2025-01-09 14:58:56 +0000

    cva refactor init

---
+++ packages/client-vue/src/components/basket/product/config.cva.ts
@@ -0,0 +1,22 @@
+import { cva } from "class-variance-authority";
+// -----------------------------------------------------------------------------
+
+export default {
+  product: {
+    root: {
+      card: cva(
+        "relative flex flex-col gap-y-4 !p-6 !py-7 text-inherit md:!p-9 md:!py-10",
+        {
+          variants: {
+            hasErrors: {
+              true: "ring-error !ring-error-1 ring-1",
+              false:
+                "ring-offset-background focus-within:ring-ring focus-within:outline-none focus-within:ring-1 focus-within:ring-offset-1 group-focus-within:ring-0 group-focus-within:ring-offset-0",
+            },
+          },
+        }
+      ),
+      container: cva("divide-y divide-dashed py-2"),
+    },
+  },
+};
