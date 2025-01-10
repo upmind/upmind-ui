c8929b9c10a02ff09ce623dd7ffb194822164fec
Rhodri Jones <rhodri.jones@upmind.com>
2025-01-10 14:26:54 +0000

    radio cards default to col-span-12 on mobile

--- packages/upwind/src/ui/radio-cards/radioCards.config.ts
+++ packages/upwind/src/ui/radio-cards/radioCards.config.ts
@@ -4,7 +4,7 @@ import { ringClasses, invalidRingClasses } from "../input/input.config";
 export const rootVariants = cva(`grid w-full grid-cols-12 gap-2`);
 
 export const itemVariants = cva(
-  `${ringClasses} ${invalidRingClasses} hover:bg-control-active-hover flex items-start space-x-1 border border-control bg-control text-control-foreground shadow-sm transition-all duration-200`,
+  `${ringClasses} ${invalidRingClasses} hover:bg-control-active-hover col-span-12 flex items-start space-x-1 border border-control bg-control text-control-foreground shadow-sm transition-all duration-200`,
   {
     variants: {
       layout: {
@@ -12,18 +12,18 @@ export const itemVariants = cva(
         grid: "h-full w-full rounded-md",
       },
       width: {
-        1: "col-span-1",
-        2: "col-span-2",
-        3: "col-span-3",
-        4: "col-span-4",
-        5: "col-span-5",
-        6: "col-span-6",
-        7: "col-span-7",
-        8: "col-span-8",
-        9: "col-span-9",
-        10: "col-span-10",
-        11: "col-span-11",
-        12: "col-span-12",
+        1: "md:col-span-1",
+        2: "md:col-span-2",
+        3: "md:col-span-3",
+        4: "md:col-span-4",
+        5: "md:col-span-5",
+        6: "md:col-span-6",
+        7: "md:col-span-7",
+        8: "md:col-span-8",
+        9: "md:col-span-9",
+        10: "md:col-span-10",
+        11: "md:col-span-11",
+        12: "md:col-span-12",
       },
     },
     compoundVariants: [
