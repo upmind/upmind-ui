f81b7ca5e7592c29109c1bc4bd180f210d27843f
Rhodri Jones <rhodri.jones@upmind.com>
2025-01-21 12:01:43 +0000

    ExPricing and CurrentPrice components

---
+++ packages/client-vue/src/components/product/pricing/Pricing.vue
@@ -0,0 +1,19 @@
+<template>
+  <ExPrice v-bind="props" />
+  <CurrentPrice v-bind="props" />
+</template>
+
+<script setup lang="ts">
+import ExPrice from "./ExPrice.vue";
+import CurrentPrice from "./CurrentPrice.vue";
+
+const props = defineProps<{
+  regularPrice?: number | string;
+  currentPrice?: number | string;
+  meta?: {
+    discounted?: boolean;
+    free?: boolean;
+  };
+  cycle?: number | string;
+}>();
+</script>
