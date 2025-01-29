2f01d5daa6c97c6ff3ea8cd52c18a1ef19fdb265
Rhodri Jones <rhodri.jones@upmind.com>
2025-01-29 16:59:07 +0000

    fix combobox key navigation (though it does require the search input - radix bug)

--- src/ui/combobox/combobox.config.ts
+++ src/ui/combobox/combobox.config.ts
@@ -25,28 +25,20 @@ export const triggerVariants = cva(
 );
 
 export const itemVariants = cva(
-  "flex w-full !cursor-pointer items-center bg-base px-3 py-2 text-sm text-control-foreground",
+  "data-[highlighted]:bg-control-active-focus flex w-full !cursor-pointer items-center bg-base px-3 py-2 text-sm text-control-foreground",
   {
     variants: {
       color: {
-        base: "data-[highlighted]:bg-control-active-muted data-[highlighted]:text-control-foreground",
-        primary:
-          "data-[highlighted]:bg-primary-muted data-[highlighted]:text-primary",
-        secondary:
-          "data-[highlighted]:bg-secondary-muted data-[highlighted]:text-secondary",
-        accent:
-          "data-[highlighted]:bg-accent-muted data-[highlighted]:text-accent",
-        promotion:
-          "data-[highlighted]:bg-promotion-muted data-[highlighted]:text-promotion",
-        destructive:
-          "data-[highlighted]:bg-destructive-muted data-[highlighted]:text-destructive",
-        success:
-          "data-[highlighted]:bg-success-muted data-[highlighted]:text-success",
-        info: "data-[highlighted]:bg-info-muted data-[highlighted]:text-info",
-        error:
-          "data-[highlighted]:bg-error-muted data-[highlighted]:text-error",
-        warning:
-          "data-[highlighted]:bg-warning-muted data-[highlighted]:text-warning",
+        base: "data-[highlighted]:text-control-foreground",
+        primary: "data-[highlighted]:text-primary",
+        secondary: "data-[highlighted]:text-secondary",
+        accent: "data-[highlighted]:text-accent",
+        promotion: "data-[highlighted]:text-promotion",
+        destructive: "data-[highlighted]:text-destructive",
+        success: "data-[highlighted]:text-success",
+        info: "data-[highlighted]:text-info",
+        error: "data-[highlighted]:text-error",
+        warning: "data-[highlighted]:text-warning",
       },
     },
     defaultVariants: {
@@ -56,7 +48,7 @@ export const itemVariants = cva(
 );
 
 export const contentVariants = cva(
-  "z-50 mt-1 rounded-md border bg-popover p-0 text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+  `${ringClasses} z-50 my-1.5 rounded-md border bg-popover p-0 text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2`,
   {
     variants: {
       width: {
