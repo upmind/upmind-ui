8c196c8c4a154de89f703a76445da42e839279c3
Dominic Da Costa <dominic@thecollaboration.studio>
2025-01-29 09:34:04 +0200

    make formi18n a composable handle objects for i18n

--- packages/client-vue/src/components/form/index.ts
+++ packages/client-vue/src/components/form/index.ts
@@ -1,3 +1,33 @@
 // --- vue elements
 export { default as UpmForm } from "./Form.vue";
 export * from "./renderers";
+
+// ---
+import { computed } from "vue";
+import { useI18n } from "vue-i18n";
+import { isFunction } from "lodash-es";
+import type { JsonFormsI18nState } from "@jsonforms/core";
+
+// ---
+export const useFormI18n = () => {
+  const { tm, locale } = useI18n();
+
+  const i18n = computed<JsonFormsI18nState>((): JsonFormsI18nState => {
+    // Create a translator using vue-i18n's t function and the current locale
+
+    const createTranslator =
+      (_locale: string) => (key: string, defaultMessage: string, data: any) => {
+        const value = isFunction(tm) ? tm(key) : null;
+        debugger;
+        return !value || value == key ? defaultMessage : value;
+      };
+
+    const safeLocale: string = locale.value;
+    return {
+      locale: safeLocale,
+      translate: createTranslator(safeLocale),
+    } as JsonFormsI18nState;
+  });
+
+  return i18n;
+};
