b7e0df8f7a95ff22ffdd308c1f7b8ebe89c51f41
Dominic Da Costa <dominic@thecollaboration.studio>
2025-01-29 09:34:04 +0200

    make formi18n a composable handle objects for i18n

--- packages/client-vue/src/components/form/Form.vue
+++ packages/client-vue/src/components/form/Form.vue
@@ -11,16 +11,15 @@
 
 <script lang="ts" setup>
 // --- external
-import { computed } from "vue";
-import { useI18n } from "vue-i18n";
 import { useForwardPropsEmits } from "radix-vue";
+
 // --- components
 import { Form } from "@upmind-automation/upmind-ui";
 
 // --- local
+import { useFormI18n } from ".";
 
 // --- utils
-import { isFunction } from "lodash-es";
 
 // --- types
 import type {
@@ -28,7 +27,6 @@ import type {
   FormFooterProps,
   FormActionsProps,
 } from "@upmind-automation/upmind-ui";
-import type { JsonFormsI18nState } from "@jsonforms/core";
 // ----------------------------------------------
 
 const props = defineProps<FormProps>();
@@ -50,27 +48,8 @@ const slots = defineSlots<{
 
 const forwarded = useForwardPropsEmits(props, emits);
 
+const i18n = useFormI18n();
 // --- state
 
 // --- computed
-
-// --- i18n
-const { t, tm, locale } = useI18n();
-
-const i18n = computed<JsonFormsI18nState>((): JsonFormsI18nState => {
-  // Create a translator using vue-i18n's t function and the current locale
-
-  const createTranslator =
-    (_locale: string) => (key: string, defaultMessage: string, data: any) => {
-      const value = isFunction(t) ? t(key, data) : null;
-      debugger;
-      return !value || value == key ? defaultMessage : value;
-    };
-
-  const safeLocale: string = locale.value;
-  return {
-    locale: safeLocale,
-    translate: createTranslator(safeLocale),
-  } as JsonFormsI18nState;
-});
 </script>
