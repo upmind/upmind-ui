2c591fafb0ac4a5eab0aaa7e70edae2b59c56bf6
Dominic Da Costa <dominic@thecollaboration.studio>
2025-01-30 14:01:03 +0200

    correct i18n translations

--- packages/client-vue/src/components/form/index.ts
+++ packages/client-vue/src/components/form/index.ts
@@ -5,21 +5,25 @@ export * from "./renderers";
 // ---
 import { computed } from "vue";
 import { useI18n } from "vue-i18n";
-import { isFunction } from "lodash-es";
+import { isEmpty, isFunction } from "lodash-es";
 import type { JsonFormsI18nState } from "@jsonforms/core";
 
 // ---
 export const useFormI18n = () => {
-  const { tm, locale } = useI18n();
+  const { tm, t, locale } = useI18n();
 
   const i18n = computed<JsonFormsI18nState>((): JsonFormsI18nState => {
     // Create a translator using vue-i18n's t function and the current locale
 
     const createTranslator =
       (_locale: string) => (key: string, defaultMessage: string, data: any) => {
-        const value = isFunction(tm) ? tm(key) : null;
-        debugger;
-        return !value || value == key ? defaultMessage : value;
+        // well try get the translation as an object for when the i18n key is nested
+        let value = isFunction(tm) ? tm(key) : undefined;
+        // then we fall back to a regular translation
+        if (isEmpty(value) && isFunction(t)) value = t(key, data);
+        // finally we fall back to the default message if no translation is found or the key is the same as the value
+        value = !value || value == key ? defaultMessage : value;
+        return value;
       };
 
     const safeLocale: string = locale.value;
