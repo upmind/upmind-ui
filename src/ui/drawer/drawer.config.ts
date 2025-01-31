0a478ab01fd4b7b162d1a4d6ad4c5a12045fea48
Rhodri Jones <rhodri.jones@upmind.com>
2025-01-31 15:55:23 +0000

    drawer overflow visible (for rings)

--- src/ui/drawer/drawer.config.ts
+++ src/ui/drawer/drawer.config.ts
@@ -48,7 +48,7 @@ export const innerVariant = cva("mx-auto max-h-[75vh] w-full ", {
   },
   defaultVariants: {
     width: "app",
-    overflow: "auto",
+    overflow: "visible",
     fit: "contain",
   },
 });
