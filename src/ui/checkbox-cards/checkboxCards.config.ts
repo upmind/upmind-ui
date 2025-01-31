bbe71acd67ba77c02cb1a5dec4f182a22e72bff5
Rhodri Jones <rhodri.jones@upmind.com>
2025-01-31 15:20:55 +0000

    checkbox ring and additional border radius

--- src/ui/checkbox-cards/checkboxCards.config.ts
+++ src/ui/checkbox-cards/checkboxCards.config.ts
@@ -4,7 +4,7 @@ import { ringClasses, invalidRingClasses } from "../../assets/styles";
 export const rootVariants = cva(`w-full space-y-2`);
 
 export const itemVariants = cva(
-  `hover:bg-control-active-muted flex items-start space-x-2 rounded-md border border-control bg-control text-control-foreground shadow-sm transition-all duration-300 ${ringClasses} ${invalidRingClasses}`
+  `hover:bg-control-active-muted group flex items-start space-x-2 rounded-md border border-control bg-control text-control-foreground shadow-sm transition-all duration-300 ${ringClasses} ${invalidRingClasses}`
 );
 
 export default {
@@ -15,7 +15,7 @@ export default {
       "m-0 h-full w-full cursor-pointer rounded-md py-3 pr-6 text-md font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
     ),
     input: cva(
-      "my-3 ml-3 mr-1 mt-[0.9rem] border-control leading-normal text-control-active"
+      `${ringClasses} my-3 ml-3 mr-1 mt-[0.9rem] rounded-md border-control leading-normal text-control-active`
     ),
   },
 };
