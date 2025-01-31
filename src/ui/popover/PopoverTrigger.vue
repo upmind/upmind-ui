701761f23e0799728b5981368c1d874f5c182cea
Rhodri Jones <rhodri.jones@upmind.com>
2025-01-31 14:38:29 +0000

    popover trigger ring styles

--- src/ui/popover/PopoverTrigger.vue
+++ src/ui/popover/PopoverTrigger.vue
@@ -1,11 +1,13 @@
 <script lang="ts" setup>
 import { PopoverTrigger, type PopoverTriggerProps } from "radix-vue";
 
+// TODO: Refactor into CE and CVA
+import { ringClasses } from "../../assets/styles";
 const props = defineProps<PopoverTriggerProps>();
 </script>
 
 <template>
-  <PopoverTrigger v-bind="props">
+  <PopoverTrigger v-bind="props" :class="ringClasses">
     <slot />
   </PopoverTrigger>
 </template>
