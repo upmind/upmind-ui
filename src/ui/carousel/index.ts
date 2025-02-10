57997a585897cccc9d838c66a601e35e7bd65f54
Dominic Da Costa <dominic@thecollaboration.studio>
2025-02-10 13:10:41 +0200

    fix for standalone/clean build

--- src/ui/carousel/index.ts
+++ src/ui/carousel/index.ts
@@ -5,4 +5,13 @@ export { default as CarouselNext } from "./CarouselNext.vue";
 export { default as CarouselPrevious } from "./CarouselPrevious.vue";
 export type { UnwrapRefCarouselApi as CarouselApi } from "./interface";
 
+export type {
+  EmblaPluginType,
+  EmblaCarouselType,
+  EmblaEventListType,
+  EmblaEventType,
+  EmblaOptionsType,
+  EmblaPluginsType,
+} from "embla-carousel"; // NB: needed for d.ts build
+
 export { useCarousel } from "./useCarousel";
