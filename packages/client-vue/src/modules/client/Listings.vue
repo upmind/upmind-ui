bdae19580dff95749ee933d01cc7da400c727e4a
Rhodri Jones <rhodri.jones@upmind.com>
2025-02-18 09:12:33 +0000

    import fixes

--- packages/client-vue/src/modules/client/Listings.vue
+++ packages/client-vue/src/modules/client/Listings.vue
@@ -117,11 +117,11 @@ import { useStyles } from "@upmind-automation/upmind-ui";
 import config from "./config.cva";
 
 // --- components
-import Section from "../../../components/content/ContentSection.vue";
-import Auth from "../../../components/session/AuthTabs.vue";
-import Empty from "./Empty.vue";
-import Card from "./Card.vue";
-import Item from "./Item.vue";
+import Section from "../../components/content/ContentSection.vue";
+import Auth from "../session/components/AuthTabs.vue";
+import Empty from "./components/Empty.vue";
+import Card from "./components/Card.vue";
+import Item from "./components/Item.vue";
 import { Input, SkeletonList } from "@upmind-automation/upmind-ui";
 
 // --- custom elements
--- packages/client-vue/src/modules/client/components/Card.vue
+++ packages/client-vue/src/modules/client/components/Card.vue
@@ -36,7 +36,7 @@ import { useI18n } from "vue-i18n";
 
 // --- internal
 import { useStyles } from "@upmind-automation/upmind-ui";
-import config from "./config.cva";
+import config from "../config.cva";
 
 // --- components
 import { DropdownMenu } from "@upmind-automation/upmind-ui";
