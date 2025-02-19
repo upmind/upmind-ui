379e670336a858c206206e4da067037c484bb5d5
Rhodri Jones <rhodri.jones@upmind.com>
2025-02-19 16:38:04 +0000

    smart title data-content

--- packages/client-vue/src/components/content/SmartTitle.vue
+++ packages/client-vue/src/components/content/SmartTitle.vue
@@ -10,7 +10,7 @@
     <template v-for="keyword in keywords" :key="keyword">
       <component
         :is="getComponent(keyword)"
-        :class="t(`${props.i18nKey}.${keyword}`)"
+        :data-content="t(`${props.i18nKey}.${keyword}`)"
       >
         {{ t(`${props.i18nKey}.${keyword}`) }}
       </component>
