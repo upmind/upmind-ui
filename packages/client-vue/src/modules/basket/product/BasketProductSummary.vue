851cdd1a8742d2a13a554ad1d95b600cb60e0648
Rhodri Jones <rhodri.jones@upmind.com>
2025-02-19 15:47:58 +0000

    promotions xs

--- packages/client-vue/src/modules/basket/product/BasketProductSummary.vue
+++ packages/client-vue/src/modules/basket/product/BasketProductSummary.vue
@@ -30,7 +30,6 @@
                 :discounted="pricing.meta?.discounted"
                 :disabled="error"
                 class="-mt-3 hidden md:block"
-                size="xs"
               />
             </div>
             <div class="flex items-end justify-between">
