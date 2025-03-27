a68dde2331bf5c5d198f4ffc2a99bfb5200cbe89
Dominic Da Costa <dominic@thecollaboration.studio>
2025-03-27 11:26:46 +0200

    nested routing for client views

--- playgrounds/labs/src/views/client/Addresses.vue
+++ playgrounds/labs/src/views/client/Addresses.vue
@@ -6,8 +6,7 @@
       size="sm"
       variant="tonal"
       label="Addresses"
-      @click.prevent="router.push({ name: 'client.addresses' })"
-      disabled
+      @click.prevent="router.push({ name: 'client.addresses.listings' })"
     >
     </Button>
     <Button
@@ -30,105 +29,44 @@
     </Button>
   </UpmContentSection>
 
-  <UpmContentSection class="mx-auto max-w-app" title="Addresses">
-    <UpmCard class="flex gap-2 pb-3 md:pb-3">
-      <Button
-        @click="fetchAddresses"
-        size="sm"
-        variant="tonal"
-        label="Load addresses"
-        :disabled="isLoadingAddresses || addresses.length > 0"
-      >
-        Load addresses
-      </Button>
-      <Button
-        @click="invalidateAddresses"
-        size="sm"
-        variant="tonal"
-        label="Invalidate addresses"
-      >
-        Invalidate addresses
-      </Button>
-      <Button
-        @click="clearAddresses"
-        size="sm"
-        variant="tonal"
-        label="Clear addresses"
-      >
-        Clear addresses
-      </Button>
-    </UpmCard>
+  <RouterView v-slot="routerViewProps" :key="$route.fullPath">
+    <template v-if="routerViewProps.Component">
+      <Content>
+        <Transition mode="out-in">
+          <KeepAlive>
+            <Suspense>
+              <!-- main content -->
+              <component :is="routerViewProps.Component" />
 
-    <UpmCard class="pb-3 md:pb-3">
-      <div v-if="isLoadingAddresses">Loading...</div>
-      <div v-else>
-        <div v-for="address in addresses" :key="address.id">
-          <div>{{ address.address1 }}</div>
-          <div>{{ address.address2 }}</div>
-          <div>{{ address.city }}</div>
-          <div>{{ address.state }}</div>
-          <Button
-            @click="doEdit(address.id)"
-            class="mt-2"
-            size="sm"
-            variant="tonal"
-            label="Edit"
-          >
-            Edit
-          </Button>
-        </div>
-      </div>
-    </UpmCard>
-  </UpmContentSection>
+              <!-- fallback / loading state -->
+              <template #fallback>
+                <Loading>
+                  <template #background>
+                    <slot name="loading-background"></slot>
+                  </template>
+                </Loading>
+              </template>
+            </Suspense>
+          </KeepAlive>
+        </Transition>
+      </Content>
+    </template>
+  </RouterView>
 </template>
 
 <script lang="ts" setup>
 // --- external
-import { ref } from "vue";
 import { useRouter } from "vue-router";
 
 // --- internal
 
 // --- components
-import {
-  UpmCard,
-  useQuery,
-  UpmContentSection,
-} from "@upmind-automation/client-vue";
+import { UpmContentSection } from "@upmind-automation/client-vue";
 import { Button } from "@upmind-automation/upmind-ui";
-import { Address, useClientAddresses } from "@upmind-automation/headless";
-
-const { getAll } = useClientAddresses();
-const { queryClient } = useQuery();
 
 // --- types
 
 // -----------------------------------------------------------------------------
 
 const router = useRouter();
-
-const addresses = ref<Address[]>([]);
-const isLoadingAddresses = ref<boolean>(false);
-
-function clearAddresses() {
-  addresses.value = [];
-}
-
-function fetchAddresses() {
-  addresses.value = [];
-  isLoadingAddresses.value = true;
-  getAll()
-    .then(res => (addresses.value = res))
-    .finally(() => (isLoadingAddresses.value = false));
-}
-
-function invalidateAddresses() {
-  return queryClient.invalidateQueries({
-    queryKey: ["client", "addresses"],
-  });
-}
-
-function doEdit(id: string) {
-  router.push({ params: { id: id }, name: "client.addresses.edit" });
-}
 </script>
