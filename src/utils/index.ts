615223454f2627b5180c68fb7c4926d85d552325
Dominic Da Costa <dominic@thecollaboration.studio>
2025-04-02 16:36:18 +0200

    better plugin registration

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,6 +2,5 @@ export * from "./useStyles";
 export * from "./useThemes";
 export * from "./isDeepEmpty";
 export * from "./useValidation";
-export * from "./useLottie";
 export * from "./usePointerEvents";
 export { useForwardPropsEmits } from "radix-vue";
