bd4bdd8eecb10a5ddaf4310bb8007102d30945f2
Dominic Da Costa <dominic@thecollaboration.studio>
2025-04-02 13:49:35 +0200

    slit useUpmind into headless & client

--- packages/client-vue/src/index.ts
+++ packages/client-vue/src/index.ts
@@ -1,4 +1,5 @@
 // --- expose our package exports
+import Upmind from "@upmind-automation/headless-vue";
 
 export * from "@upmind-automation/headless-vue";
 export { default as Upm } from "./Upmind.vue";
@@ -8,21 +9,7 @@ export * from "./modules";
 // export type { ActorRef } from "xstate";
 
 // --- alternative implementation ----------------------------------------------
-import {
-  VueQueryPlugin,
-  type VueQueryPluginOptions,
-} from "@tanstack/vue-query";
 
-import {
-  useUpmind,
-  useQuery,
-  useBrand,
-  useSession,
-  useSystem,
-  useBasket,
-  useDataLayer,
-  useTracking,
-} from "@upmind-automation/headless-vue";
 import { useRouting } from "./modules/routing";
 import { useI18n } from "./modules/system/i18n";
 
@@ -63,7 +50,7 @@ interface UpmindProps {
   };
 }
 
-class Upmind {
+class UpmindClient {
   private status: UpmindStatus = UpmindStatus.notInitialised;
   // ---
   app: UpmindProps["app"] | undefined;
@@ -74,49 +61,26 @@ class Upmind {
 
   // ---
   constructor() {
-    // console.debug("Upmind started");
+    console.debug("Upmind Client has started");
   }
 
-  init({ app, pop, router, i18n, analytics }: UpmindProps) {
+  init({ app, pop, router, i18n, analytics }: UpmindProps): Promise<void> {
     // NB: Only initialise once
-    if (this.status != UpmindStatus.notInitialised) return;
+    if (this.status != UpmindStatus.notInitialised) return Promise.resolve();
 
     this.status = UpmindStatus.initialising;
 
-    this.app = app;
-    this.pop = pop;
-    this.router = router;
-    this.i18n = i18n;
-    this.analytics = analytics;
+    return Upmind.init({ app, pop, analytics }).then(() => {
+      this.app = Upmind.app;
+      this.pop = Upmind.pop;
+      this.analytics = Upmind.analytics;
+      this.router = router;
+      this.i18n = i18n;
 
-    useUpmind(pop).then(() => {
-      Promise.all([
-        this.initHeadless(),
-        this.initRouter(),
-        this.initI18n(),
-        this.initAnalytics(),
-      ]).then(() => {
+      return Promise.all([this.initRouter(), this.initI18n()]).then(() => {
         this.status = UpmindStatus.initialised;
       });
     });
-  }
-
-  private async initHeadless() {
-    // lets initialize our api query client and register it with the vue app
-    const { queryClient } = useQuery();
-    const vueQueryPluginOptions: VueQueryPluginOptions = {
-      // @ts-ignore : minor mismatch of type between vue and core query client
-      queryClient,
-      enableDevtoolsV6Plugin: true,
-    };
-
-    this.app?.use(VueQueryPlugin, vueQueryPluginOptions);
-
-    // init our core modules
-    useSystem();
-    useBrand();
-    useSession();
-    // useBasket(); // we dont need this on startup
   }
 
   private async initRouter() {
@@ -138,52 +102,6 @@ class Upmind {
     setLocale(locale);
   }
 
-  private async initAnalytics() {
-    //  --- Initialise our Upmind tracking cookie and store the utm params
-    const { init: initTracking } = useTracking();
-    initTracking();
-
-    // --- Initialise our dataLayer (bail if analytics is not enabled)
-    if (!this.analytics?.enabled) return;
-    this.analytics.gtm ??= {}; // ensure we have a gtm object
-    const { init, id, dataLayer } = useDataLayer(
-      this.analytics?.gtm?.dataLayer
-    );
-    this.analytics.gtm.dataLayer = id; // ensure the data layer name matches
-    init();
-
-    // --- Implement Google Tag Manager
-    // IF we have a container id given to us OR in the brand settings
-    // Add GTM the script, allowing for a custom data layer name, (default is dataLayer)
-    this.analytics.gtm.containerId ??= await useBrand()
-      .getAnayltics()
-      .then(analytics => analytics?.gtm?.container_id);
-
-    if (this.analytics.gtm.containerId) {
-      dataLayer({ gtm_start: new Date().getTime(), event: "gtm.js" }).push();
-
-      const firstScript = first(document.getElementsByTagName("script"));
-      const script = document.createElement("script");
-      script.async = true;
-      script.setAttribute(
-        "src",
-        `https://www.googletagmanager.com/gtm.js?id=${this.analytics.gtm.containerId}${
-          this.analytics.gtm?.dataLayer &&
-          this.analytics.gtm.dataLayer != "dataLayer"
-            ? `&l=${this.analytics.gtm?.dataLayer}`
-            : ""
-        }`
-      );
-      // insert before the first script tag or append to head
-      firstScript?.parentNode
-        ? firstScript.parentNode.insertBefore(script, firstScript)
-        : document.head.appendChild(script);
-    }
-
-    // --- Finally push our initial payload to the data layer
-    dataLayer().withPage().withUser().push();
-  }
-
   // ---
   isReady(): Promise<void> {
     return new Promise(resolve =>
@@ -195,5 +113,5 @@ class Upmind {
 }
 
 // --- create a singleton provider of Upmind
-const upmind = new Upmind();
-export default upmind;
+const upmindClient = new UpmindClient();
+export default upmindClient;
