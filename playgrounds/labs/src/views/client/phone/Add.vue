cc73e762e7b65c18b005d76c4bc4694e6bcb279b
Rafael Macedo <rafaelmacedo4@gmail.com>
2025-04-07 15:43:37 +0100

    feat: refactor phone management components to use UpmForm and improve data handling

--- playgrounds/labs/src/views/client/phone/Add.vue
+++ playgrounds/labs/src/views/client/phone/Add.vue
@@ -5,22 +5,16 @@
     title="New Phone"
   >
     <UpmCard class="flex w-full flex-wrap gap-2 pb-3 md:pb-3">
-      <pre>{{ model }}</pre>
-      <div class="actions flex w-full basis-full gap-2">
-        <Button
-          @click="doInput"
-          variant="tonal"
-          :loading="inputting"
-          :disabled="inputting || updating"
-          >Input Data</Button
-        >
-        <Button
-          @click="doUpdate"
-          :loading="updating"
-          :disabled="updating || inputting || !isValid"
-          >Update</Button
-        >
-      </div>
+      <pre> {{ meta }}</pre>
+
+      <UpmForm
+        :model-value="model"
+        :schema="schema"
+        :uischema="uischema"
+        @update:modelValue="doInput"
+        @resolve="doUpdate"
+        @reject="doCancel"
+      />
     </UpmCard>
 
     <template #footer> </template>
@@ -28,59 +22,41 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
-import { faker } from "@faker-js/faker";
-import { Button } from "@upmind-automation/upmind-ui";
+import { debounce } from "lodash-es";
+import { useRouter } from "vue-router";
 import { UpmContentSection, UpmCard } from "@upmind-automation/client-vue";
-import { type PhoneModel, useClientPhone } from "@upmind-automation/headless";
 
-const { update, input, getModel, service } = useClientPhone();
+import {
+  UpmForm,
+  useClientPhone,
+  useClientPhones,
+  type PhoneModel,
+} from "@upmind-automation/client-vue";
 
-const model = ref<PhoneModel>(getModel() ?? {});
-const isValid = ref<boolean>(false);
-const updating = ref<boolean>(false);
-const inputting = ref<boolean>(false);
+const { isReady } = useClientPhones();
+await isReady().catch(() => router.push({ name: "client.phones" }));
 
-service.subscribe((state: any) => {
-  isValid.value = state.matches("available.valid");
-});
+const router = useRouter();
+const { update, input, model, meta, schema, uischema, stop } = useClientPhone();
 
 // --- METHODS
 
-function doInput() {
-  inputting.value = true;
-
-  const phoneNumber = faker.phone.number();
-
-  const data: PhoneModel = {
-    type: 1,
-    phone: phoneNumber,
-    country: "US",
-    countryCallingCode: "+1",
-    nationalNumber: phoneNumber,
-  };
-
-  input(data)
-    .then(data => {
-      model.value = data;
-    })
-    .catch(err => {
-      console.error("error inputting", { model, err });
-    })
-    .finally(() => {
-      inputting.value = false;
-    });
-}
+const doInput = debounce((data: PhoneModel) => {
+  input(data);
+}, 500);
 
 function doUpdate() {
-  updating.value = true;
-  update()
-    .then(() => {
-      model.value = getModel();
-    })
-    .catch(err => {
-      console.error("error adding", { model, err });
-    })
-    .finally(() => (updating.value = false));
+  update().then(() => {
+    router.push({
+      name: "client.phones",
+    });
+  });
+}
+
+function doCancel() {
+  stop();
+  router.push({
+    name: "client.phones",
+  });
 }
 </script>
--- playgrounds/labs/src/views/client/phone/Listings.vue
+++ playgrounds/labs/src/views/client/phone/Listings.vue
@@ -2,66 +2,78 @@
   <UpmContentSection class="mx-auto max-w-app" title="Phones">
     <div class="flex gap-2 pb-6">
       <Button
-        @click="fetchPhones"
+        @click="getAll"
         size="sm"
         variant="tonal"
-        label="Load phones"
-        :disabled="processing || phones.length > 0"
-      />
+        :disabled="meta.isLoading || !meta.isAvailable"
+      >
+        Load
+      </Button>
       <Button
-        @click="invalidatePhones"
+        @click="invalidate"
         size="sm"
         variant="tonal"
-        label="Invalidate phones"
-        :disabled="processing"
-      />
-      <Button
-        @click="clearPhones"
-        size="sm"
-        variant="tonal"
-        label="Clear phones"
-        :disabled="processing"
-      />
+        :disabled="meta.isLoading || !meta.isAvailable"
+      >
+        Invalidate
+      </Button>
 
-      <Button @click="doAdd" :loading="processing">New Phone</Button>
+      <Button
+        @click="doAdd"
+        :loading="meta.isLoading"
+        :disabled="!meta.isAvailable"
+      >
+        New Phone
+      </Button>
     </div>
 
-    <div v-if="processing">Loading...</div>
+    <Alert
+      v-if="!meta.isAvailable"
+      color="error"
+      title="Please log in to view phones"
+    />
 
-    <section class="pb-3 md:pb-3" v-for="phone in phones" :key="phone.id">
+    <Alert v-else-if="meta.isLoading" color="info" title="Loading..." />
+
+    <Alert
+      v-else-if="meta.isError"
+      color="error"
+      :title="error.title"
+      :message="error.message"
+    />
+
+    <Alert
+      v-else-if="meta.isEmpty"
+      color="info"
+      title="No phones found"
+      message="Please add an phone to get started."
+    />
+
+    <section v-else class="pb-3 md:pb-3" v-for="phone in data" :key="phone.id">
       <UpmCard>
         <h3 class="mt-0">{{ phone.title }}</h3>
         <p>{{ phone.description }}</p>
 
         <div class="flex gap-2">
-          <Button
-            @click="doEdit(phone.id)"
-            class="mt-2"
-            size="sm"
-            variant="tonal"
-            label="Edit"
-          >
-            Edit
+          <Button @click="doEdit(phone.id)" size="sm" variant="tonal">
+            <Icon icon="edit" class="size-4" />
           </Button>
           <Button
-            @click="doDelete(phone.id)"
-            class="mt-2"
+            @click="remove(phone.id)"
             size="sm"
             variant="tonal"
             label="Delete"
             :disabled="!phone.meta.canDelete"
           >
-            Delete
+            <Icon icon="remove" class="size-4" />
           </Button>
           <Button
-            @click="setPhoneAsDefault(phone.id)"
-            class="mt-2"
+            @click="setDefault(phone.id)"
             size="sm"
             variant="tonal"
             label="Set Default"
-            :disabled="!!phone.meta.isDefault"
-            >Set Default</Button
-          >
+            :disabled="phone.meta.isDefault"
+          />
         </div>
       </UpmCard>
     </section>
@@ -71,48 +83,22 @@
 <script lang="ts" setup>
 // --- external
 import { useRouter } from "vue-router";
-import { onMounted, ref } from "vue";
 
 // --- internal
-import { Phone, useClientPhones } from "@upmind-automation/headless";
+import { useClientPhones } from "@upmind-automation/headless-vue";
 
 // --- components
-import {
-  UpmCard,
-  useQuery,
-  UpmContentSection,
-} from "@upmind-automation/client-vue";
-import { Button } from "@upmind-automation/upmind-ui";
+import { Button, Alert, Icon } from "@upmind-automation/upmind-ui";
+import { UpmCard, UpmContentSection } from "@upmind-automation/client-vue";
 
-const { getAll, remove, setDefault } = useClientPhones();
-const { queryClient } = useQuery();
+const { isReady, getAll, data, meta, error, invalidate, remove, setDefault } =
+  useClientPhones();
 
 // --- types
 
 // -----------------------------------------------------------------------------
 
 const router = useRouter();
-
-const phones = ref<Phone[]>([]);
-const processing = ref<boolean>(false);
-
-function clearPhones() {
-  phones.value = [];
-}
-
-function fetchPhones() {
-  phones.value = [];
-  processing.value = true;
-  getAll()
-    .then(res => (phones.value = res))
-    .finally(() => (processing.value = false));
-}
-
-function invalidatePhones() {
-  return queryClient.invalidateQueries({
-    queryKey: ["client", "phones"],
-  });
-}
 
 function doEdit(id: string) {
   router.push({ params: { id: id }, name: "client.phones.edit" });
@@ -122,15 +108,5 @@ function doAdd() {
   router.push({ name: "client.phones.add" });
 }
 
-function doDelete(id: string) {
-  remove(id).then(() => fetchPhones());
-}
-
-function setPhoneAsDefault(id: string) {
-  setDefault(id).then(() => fetchPhones());
-}
-
-onMounted(() => {
-  fetchPhones();
-});
+await isReady().then(() => getAll());
 </script>
