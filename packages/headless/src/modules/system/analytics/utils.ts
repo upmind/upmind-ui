1016187306915918d2d418b16ed9636686d94b98
Dominic Da Costa <dominic@thecollaboration.studio>
2025-04-09 17:04:54 +0200

    housekeeping

--- packages/headless/src/modules/system/analytics/utils.ts
+++ packages/headless/src/modules/system/analytics/utils.ts
@@ -14,9 +14,7 @@ export function parseEcommerceItem(
     index,
     item_brand: product.product?.brand?.name, // For reporting purposes we intentionally pass untranslated brand name
     item_category: product.product.category.name, // For reporting purposes we intentionally pass untranslated category name
-    // @ts-ignore: TODO see why this is warni g when it is in fact valid
     item_category2: product.product.category?.top_category?.name, // For reporting purposes we intentionally pass untranslated category name
-    // @ts-ignore: TODO see why this is warni g when it is in fact valid
     item_category3: product.product.category?.top_category?.top_category?.name, // For reporting purposes we intentionally pass untranslated category name
     price: product.configuration_total_amount_converted, //TODO: check the correct value is used
     // net_price: product.configuration_net_amount_converted, //TODO: check the correct value is used
