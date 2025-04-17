0dffdcdc3dc87ff90d6b712d5b17eec0bbcfa587
Rhodri Jones <rhodri.jones@upmind.com>
2025-04-17 12:54:22 +0100

    address renderer init

---
+++ packages/client-vue/src/components/form/renderers/AddressRenderer.vue
@@ -0,0 +1,32 @@
+<template>
+  <FormField v-bind="formFieldProps"> Address Renderer </FormField>
+</template>
+
+<script lang="ts" setup>
+// --- external
+import { useJsonFormsControl } from "@jsonforms/vue";
+
+// --- components
+import { FormField } from "@upmind-automation/upmind-ui";
+
+// --- utils
+import { useUpmindUIRenderer } from "@upmind-automation/upmind-ui";
+
+// --- types
+import type { ControlElement } from "@jsonforms/core";
+import type { RendererProps } from "@jsonforms/vue";
+
+// ----------------------------------------------
+
+const props = defineProps<RendererProps<ControlElement>>();
+
+const { formFieldProps } = useUpmindUIRenderer(useJsonFormsControl(props));
+</script>
+
+<script lang="ts">
+import { isStringControl, formatIs, and } from "@jsonforms/core";
+export const tester = {
+  rank: 3,
+  controlType: and(isStringControl, formatIs("address")),
+};
+</script>
---
+++ playgrounds/labs/src/views/form/Domain.vue
@@ -0,0 +1,15 @@
+<template>
+  <UpmContentSection
+    class="mx-auto max-w-app"
+    title="Domain Renderer"
+    subtitle="Search and manage basket domains"
+  >
+    <UpmCard class="py-32 text-center text-xl font-bold">
+      Work in progress
+    </UpmCard>
+  </UpmContentSection>
+</template>
+
+<script setup lang="ts">
+import { UpmContentSection, UpmCard } from "@upmind-automation/client-vue";
+</script>
