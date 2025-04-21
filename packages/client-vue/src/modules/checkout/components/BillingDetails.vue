3171d9a277cba35ddceef1140e9f1e6c1757b923
Rhodri Jones <rhodri.jones@upmind.com>
2025-04-21 17:59:20 +0100

    billing details add wip

--- packages/client-vue/src/modules/checkout/components/BillingDetails.vue
+++ packages/client-vue/src/modules/checkout/components/BillingDetails.vue
@@ -1,7 +1,24 @@
 <template>
-  <Addresses />
+  <Addresses v-if="!isEmpty(data)" />
+
+  <Add v-else />
 </template>
 
 <script setup lang="ts">
+// --- internal
+import { useClientAddresses } from "@upmind-automation/headless-vue";
+
+// --- components
 import Addresses from "../../client/Addresses.vue";
+import Add from "../../client/components/address/Add.vue";
+
+// --- utils
+import { isEmpty } from "lodash-es";
+
+const { isReady, getAll, data, meta, error, invalidate, remove, setDefault } =
+  useClientAddresses();
+
+await isReady().then(() => {
+  getAll();
+});
 </script>
---
+++ packages/client-vue/src/modules/client/components/address/Add.vue
@@ -0,0 +1,31 @@
+<template>
+  <UpmForm
+    :model-value="model"
+    :schema="schema"
+    :uischema="uischema"
+    :additional-renderers="formRenderers"
+    @update:modelValue="data => input(data as AddressModel)"
+    @resolve="doUpdate"
+    @reject="doCancel"
+  />
+</template>
+
+<script setup lang="ts">
+// --- internal
+import {
+  useClientAddresses,
+  useClientAddress,
+  type AddressModel,
+} from "@upmind-automation/headless-vue";
+
+import { UpmForm, formRenderers } from "../../../../components/form";
+
+const { isReady } = useClientAddresses();
+
+const { update, input, model, meta, schema, uischema, stop } =
+  useClientAddress();
+
+function doUpdate() {}
+
+function doCancel() {}
+</script>
