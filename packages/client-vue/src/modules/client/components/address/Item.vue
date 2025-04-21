c214d8d879367a9328bfadfe3775fb1f38a7c21b
Rhodri Jones <rhodri.jones@upmind.com>
2025-04-21 15:30:41 +0100

    addresses redesign wip

---
+++ packages/client-vue/src/modules/client/components/address/Item.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex flex-col gap-y-1">
+    <h3 class="m-0 text-sm font-semibold leading-none">{{ address.title }}</h3>
+    <p class="text-emphasis-medium m-0 text-sm leading-none">
+      {{ address.description }}
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+// --- components
+import { Link, Icon } from "@upmind-automation/upmind-ui";
+
+// --- types
+import type { Address } from "@upmind-automation/headless-vue";
+
+defineProps<{
+  address: Address;
+}>();
+</script>
