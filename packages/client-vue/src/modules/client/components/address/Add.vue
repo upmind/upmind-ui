958f5ddf6bf28dcf8fb4a11c6214dd572e05a373
Rhodri Jones <rhodri.jones@upmind.com>
2025-04-22 15:16:50 +0100

    address auto fill details and submit with 1 click

--- packages/client-vue/src/modules/client/components/address/Add.vue
+++ packages/client-vue/src/modules/client/components/address/Add.vue
@@ -4,28 +4,37 @@
     :schema="schema"
     :uischema="uischema"
     :additional-renderers="formRenderers"
-    @update:modelValue="data => input(data as AddressModel)"
-    @resolve="doUpdate"
-    @reject="doCancel"
-  />
+    @update:modelValue="(data: any) => doUpdate(data)"
+  >
+    <template #actions="{ meta, doResolve, doReject }">
+      <Link
+        label="Enter address manually"
+        size="sm"
+        variant="muted"
+        class="-mt-4 leading-none"
+      />
+    </template>
+  </UpmForm>
 </template>
 
 <script setup lang="ts">
+// --- external
+import { ref, onUnmounted } from "vue";
+
 // --- internal
-import {
-  useClientAddresses,
-  useClientAddress,
-  type AddressModel,
-} from "@upmind-automation/headless-vue";
+import { useClientAddress } from "@upmind-automation/headless-vue";
 
 import { UpmForm, formRenderers } from "../../../../components/form";
+import { Link } from "@upmind-automation/upmind-ui";
 
-const { isReady } = useClientAddresses();
+// --- types
+import type { AddressModel } from "@upmind-automation/headless-vue";
 
-const { update, input, model, meta, schema, uischema, stop } =
+const { isReady, update, set, model, meta, schema, uischema, stop, errors } =
   useClientAddress();
 
-function doUpdate() {}
-
-function doCancel() {}
+const doUpdate = async (data: AddressModel) => {
+  await set(data);
+  await update();
+};
 </script>
