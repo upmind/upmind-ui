6f937d0ea95c5c1230dd295c37a4818004ab64f0
Rhodri Jones <rhodri.jones@upmind.com>
2025-04-22 15:16:50 +0100

    address auto fill details and submit with 1 click

--- packages/client-vue/src/components/form/renderers/AddressRenderer.vue
+++ packages/client-vue/src/components/form/renderers/AddressRenderer.vue
@@ -3,6 +3,7 @@
     <Search
       :results="searchResults"
       @update:search="searchAddresses"
+      @select="selectAddress"
       :placeholder="appliedOptions?.placeholder"
     />
   </FormField>
@@ -25,20 +26,20 @@ import { useUpmindUIRenderer } from "@upmind-automation/upmind-ui";
 // --- types
 import type { ControlElement } from "@jsonforms/core";
 import type { RendererProps } from "@jsonforms/vue";
+import type { Address } from "@upmind-automation/headless-vue";
 
-import { debounce, isEmpty } from "lodash-es";
+import { debounce, isEmpty, find } from "lodash-es";
 import { usePlaces } from "@upmind-automation/headless-vue";
 import { ref, onMounted, computed } from "vue";
-
 // ----------------------------------------------
 
 const props = defineProps<RendererProps<ControlElement>>();
 
 const places = usePlaces();
-const { appliedOptions, formFieldProps } = useUpmindUIRenderer(
+
+const { appliedOptions, formFieldProps, updateControl } = useUpmindUIRenderer(
   useJsonFormsControl(props)
 );
-
 const parsedResults = ref<any[]>([]);
 
 onMounted(async () => {
@@ -61,11 +62,25 @@ const searchResults = computed(() => {
   return parsedResults.value.map(
     (result: any) =>
       ({
-        id: result.description,
+        id: result.id,
         label: result.description,
       }) as SearchItem
   );
 });
+
+const selectAddress = (selectedItem: SearchItem) => {
+  const address = find(
+    parsedResults.value,
+    address => address.id === selectedItem.id
+  ).address as Address;
+
+  updateControl("id", address.id ?? "");
+  updateControl("address1", address.address1 ?? "");
+  updateControl("address2", address.address2 ?? "");
+  updateControl("city", address.city ?? "");
+  updateControl("postcode", address.postcode ?? "");
+  updateControl("countryId", address.countryId ?? "");
+};
 </script>
 
 <script lang="ts">
