360cc60d7bd0584af96b6f5110e1e4663a68784d
Rhodri Jones <rhodri.jones@upmind.com>
2025-04-23 11:35:10 +0100

    place renderer (wip)

--- packages/client-vue/src/components/form/renderers/index.ts
+++ packages/client-vue/src/components/form/renderers/index.ts
@@ -5,11 +5,12 @@ import { registerEntry } from "@upmind-automation/upmind-ui";
 import DomainRenderer, { tester as domainTest } from "./DomainRenderer.vue";
 import SLDRenderer, { tester as sldTest } from "./SLDRenderer.vue";
 import AddressRenderer, { tester as addressTest } from "./AddressRenderer.vue";
-
+import PlaceRenderer, { tester as placeTest } from "./PlaceRenderer.vue";
 // -----------------------------------------------------------------------------
 
 export const formRenderers = [
   registerEntry(DomainRenderer, domainTest),
   registerEntry(SLDRenderer, sldTest),
   registerEntry(AddressRenderer, addressTest),
+  registerEntry(PlaceRenderer, placeTest),
 ];
