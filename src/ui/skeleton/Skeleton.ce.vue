5084ba00a31b6f519b373fd8e8bc819977e1f488
RhodriJonesUpmind <rhodri.jones@upmind.com>
2025-05-07 15:01:48 +0100

    skeleton cva and housekeeping (#24)

--- src/ui/skeleton/Skeleton.ce.vue
+++ src/ui/skeleton/Skeleton.ce.vue
@@ -1,16 +1,37 @@
-<script setup lang="ts">
-import { cn } from "../../utils";
-import type { SkeletonProps } from "./types";
-
-const props = defineProps<SkeletonProps>();
-</script>
-
 <template>
-  <div
-    :class="
-      cn('bg-base-muted muted w-auto animate-pulse rounded-lg', props.class)
-    "
-  >
-    <div class="w-auto select-none opacity-0"><slot>Skeleton</slot></div>
+  <div :class="cn(styles.skeleton.root, props.class)">
+    <div :class="styles.skeleton.content"><slot>Skeleton</slot></div>
   </div>
 </template>
+
+<script setup lang="ts">
+// --- external
+import { computed } from "vue";
+
+// --- internal
+import config from "./skeleton.config";
+
+// --- utils
+import { cn, useStyles } from "../../utils";
+
+// --- types
+import type { SkeletonProps } from "./types";
+import type { ComputedRef } from "vue";
+const props = defineProps<SkeletonProps>();
+
+const meta = computed(() => ({
+  // ---
+}));
+
+const styles = useStyles(
+  "skeleton",
+  meta,
+  config,
+  props.uiConfig ?? {}
+) as ComputedRef<{
+  skeleton: {
+    root: string;
+    content: string;
+  };
+}>;
+</script>
