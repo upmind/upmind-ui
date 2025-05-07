b56d2d2e5da130e9c8c3d092c36bf58557ec69a0
Rhodri Jones <rhodri.jones@upmind.com>
2025-05-07 12:45:25 +0100

    tweak item class

--- packages/client-vue/src/modules/domain/domain.config.ts
+++ packages/client-vue/src/modules/domain/domain.config.ts
@@ -98,7 +98,7 @@ export default {
         radio: cva("relative flex w-11 justify-center pl-1.5"),
       },
       item: cva(
-        "hover:border-control-strong !text-base-foreground rounded-lg border transition-all duration-200"
+        "hover:border-control-strong rounded-lg border transition-all duration-200"
       ),
       items: cva("h-12"),
       card: cva("bg-base shadow-sm"),
