58e3112ae44ad164a757aaf66ba68a7535ca9f0e
Dominic Da Costa <dominic@thecollaboration.studio>
2025-05-08 11:45:30 +0200

    updated init recaptcha token passed up to upmind

--- packages/headless-vue/src/index.ts
+++ packages/headless-vue/src/index.ts
@@ -1,4 +1,6 @@
+import useUpmind from "./useUpmind";
+export { useQuery, type IApiPop } from "@upmind-automation/headless";
 export * from "./modules";
 export * as utils from "./utils";
-
-export { useQuery, useUpmind, type IApiPop } from "@upmind-automation/headless";
+export * from "./useUpmind";
+export default useUpmind;
--- packages/headless/src/modules/system/recaptcha/recaptcha.machine.ts
+++ packages/headless/src/modules/system/recaptcha/recaptcha.machine.ts
@@ -4,23 +4,31 @@ import { createMachine, assign } from "xstate";
 
 // --- internal
 import services from "./services";
+import { RecaptchaContext } from "./types";
+import { isString } from "xstate/lib/utils";
+import { isEmpty, set } from "lodash-es";
 
 // --utils
-
+import { useTime } from "../../../utils";
 // -----------------------------------------------------------------------------
 export default createMachine(
   {
     //tsTypes: {} as import("./recaptcha.machine.typegen").Typegen0,
     id: "recaptchaTokenManager",
     predictableActionArguments: true,
-    initial: "loading",
-    context: {
-      grecaptcha: undefined,
-      token: undefined,
-      created: undefined,
-      error: undefined,
-    },
+    initial: "subscribing",
+    context: {} as RecaptchaContext,
     states: {
+      subscribing: {
+        on: {
+          SET_SITE_KEY: {
+            target: "loading",
+            actions: ["setSiteKey"],
+            cond: "isValidSiteKey",
+          },
+        },
+      },
+
       loading: {
         invoke: {
           src: "load",
@@ -37,7 +45,53 @@ export default createMachine(
 
       unavailable: {},
 
-      available: {},
+      available: {
+        initial: "idle",
+        states: {
+          idle: {
+            on: {
+              GENERATE_TOKEN: {
+                target: "processing",
+              },
+            },
+          },
+          processing: {
+            invoke: {
+              src: "generateToken",
+              onDone: {
+                target: "processed",
+                actions: ["setToken"],
+              },
+              onError: {
+                target: "error",
+                actions: ["setError"],
+              },
+            },
+          },
+          processed: {
+            after: {
+              expired: {
+                target: "idle",
+                actions: ["clearToken"],
+              },
+            },
+          },
+          error: {
+            after: {
+              error: {
+                target: "idle",
+                actions: ["clearToken", "clearError"],
+              },
+            },
+          },
+        },
+        on: {
+          CLEAR: {
+            target: "available.idle",
+            actions: ["clearToken", "clearError"],
+          },
+        },
+      },
 
       complete: {
         type: "final",
@@ -46,17 +100,37 @@ export default createMachine(
   },
   {
     actions: {
+      setSiteKey: assign({
+        siteKey: (_context, { siteKey }: AnyEventObject) => siteKey,
+      }),
       setGrecaptcha: assign({
         grecaptcha: (_context, { data }: AnyEventObject) => data,
+      }),
+
+      setToken: assign({
+        token: (_context, { data }: AnyEventObject) => data,
+      }),
+      clearToken: assign({
+        token: _context => undefined,
       }),
 
       setError: assign({
         error: (_context, { data }: AnyEventObject) => data,
       }),
+      clearError: assign({
+        error: _context => undefined,
+      }),
     },
 
-    services,
-    guards: {},
-    delays: {},
+    services: services as any,
+    guards: {
+      isValidSiteKey: (_context, event) => {
+        return isString(event.siteKey) && !isEmpty(event.siteKey);
+      },
+    },
+    delays: {
+      expired: () => useTime().MINUTE * 2,
+      error: () => useTime().ERROR,
+    },
   }
 );
