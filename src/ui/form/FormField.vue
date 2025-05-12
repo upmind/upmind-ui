e38022373ef361069ccc1fbca6298e1c0ac99215
RhodriJonesUpmind <rhodri.jones@upmind.com>
2025-05-12 07:55:16 +0100

    feature/fix-field-animation (#26)
    * fix jumpy form transition
    
    * fix autofocus
    
    * pass input autofocus
    
    ---------
    
    Co-authored-by: Dominic da Costa <dominic@thecollaboration.studio>

--- src/ui/form/FormField.vue
+++ src/ui/form/FormField.vue
@@ -8,11 +8,8 @@
   >
     <slot name="field">
       <!-- label -->
-      <div
-        class="flex w-full flex-row flex-nowrap items-center justify-between"
-        v-if="meta.hasLabel"
-      >
-        <FormLabel :formItemId="id">
+      <div class="w-full flex-col space-y-1">
+        <FormLabel v-if="meta.hasLabel" :formItemId="id">
           <slot name="label" :label="label">
             <span class="inline-flex items-center gap-x-1">
               <slot name="icon" />
@@ -37,33 +34,31 @@
             </span>
           </slot>
         </FormLabel>
+
+        <!-- input -->
+        <FormControl
+          :key="props.id"
+          :invalid="meta.isInvalid"
+          :disabled="props.disabled"
+          :required="props.required"
+          :formItemId="props.id"
+          :auto-focus="props.autoFocus"
+          :formDescriptionId="`form-item-description-${props.id}`"
+          :formMessageId="`form-item-message-${props.id}`"
+        >
+          <slot></slot>
+        </FormControl>
       </div>
 
-      <!-- text -->
-
-      <!-- tags -->
-
-      <!-- input -->
-      <FormControl
-        :key="props.id"
-        :invalid="meta.isInvalid"
-        :disabled="props.disabled"
-        :required="props.required"
-        :formItemId="props.id"
-        :auto-focus="props.autoFocus"
-        :formDescriptionId="`form-item-description-${props.id}`"
-        :formMessageId="`form-item-message-${props.id}`"
-      >
-        <slot></slot>
-      </FormControl>
-
       <!-- validation messages -->
-      <FormMessage
-        v-if="meta.isInvalid && !props.noErrors"
-        :formMessageId="`form-item-message-${props.id}`"
-        :name="name"
-        :errors="errors"
-      />
+      <slot name="messages">
+        <FormMessage
+          v-if="meta.isInvalid && !props.noErrors"
+          :formMessageId="`form-item-message-${props.id}`"
+          :name="name"
+          :errors="errors"
+        />
+      </slot>
 
       <!-- description -->
       <FormDescription
