f33707c48fd2f6ccc3cdb03c5bb3444318c59aa6
Rhodri Jones <rhodri.jones@upmind.com>
2025-05-20 18:02:56 +0100

    oneof renderer correctly handle oneof ui schema and housekeeping

--- src/ui/form/renderers/controls/OneOfRenderer.vue
+++ src/ui/form/renderers/controls/OneOfRenderer.vue
@@ -2,8 +2,8 @@
   <FormField v-bind="formFieldProps">
     <Tabs
       :tabs="oneOfItems"
-      :value="selectedIndex.toString()"
-      :defaultValue="selectedIndex.toString()"
+      :value="String(selectedIndex)"
+      :defaultValue="String(selectedIndex)"
       @update:modelValue="toggleTab"
     />
 
@@ -21,7 +21,7 @@
 
 <script lang="ts" setup>
 // --- external
-import { computed, onMounted, ref, nextTick } from "vue";
+import { computed, ref } from "vue";
 import { useJsonFormsOneOfControl, DispatchRenderer } from "@jsonforms/vue";
 import { createCombinatorRenderInfos } from "@jsonforms/core";
 
@@ -31,7 +31,6 @@ import { Tabs } from "../../../tabs";
 
 // --- utils
 import { useUpmindUIRenderer } from "../utils";
-import { isEmpty } from "lodash-es";
 
 // --- types
 import type {
@@ -50,13 +49,13 @@ const { control, formFieldProps } = useUpmindUIRenderer(
 );
 
 const selectedIndex = ref(control.value.indexOfFittingSchema ?? 0);
-const selectIndex = ref(selectedIndex.value);
-const newSelectedIndex = ref(0);
 
 const indexedOneOfRenderInfos = computed(
   (): (CombinatorSubSchemaRenderInfo & {
     index: number;
   })[] => {
+    const oneOfUiSchemas = control.value.uischema.options?.oneOfUiSchema;
+
     const result = createCombinatorRenderInfos(
       control.value.schema.oneOf!,
       control.value.rootSchema,
@@ -68,7 +67,16 @@ const indexedOneOfRenderInfos = computed(
 
     return result
       .filter(info => info.uischema)
-      .map((info, index) => ({ ...info, index: index }));
+      .map((info, index) => {
+        if (oneOfUiSchemas && oneOfUiSchemas[index]) {
+          return {
+            ...info,
+            uischema: oneOfUiSchemas[index],
+            index: index,
+          };
+        }
+        return { ...info, index: index };
+      });
   }
 );
 
@@ -76,26 +84,14 @@ const oneOfItems = computed((): TabItem[] => {
   return (
     control.value.schema?.oneOf?.map((item: any, index: number) => ({
       value: String(index),
-      label: String(item.title) as string,
+      label: String(item.title),
     })) || []
   );
 });
 
 const toggleTab = (value: any) => {
   const numericValue = parseInt(value, 10);
-  console.log("numericValue", numericValue);
-  selectIndex.value = numericValue;
-
-  if (control.value.enabled && !isEmpty(control.value.data)) {
-    nextTick(() => {
-      newSelectedIndex.value = selectIndex.value;
-      selectIndex.value = selectedIndex.value;
-    });
-  } else {
-    nextTick(() => {
-      selectedIndex.value = selectIndex.value;
-    });
-  }
+  selectedIndex.value = numericValue;
 };
 </script>
 
