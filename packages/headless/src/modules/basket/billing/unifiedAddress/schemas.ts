3aa136827116ee47c05474f82f42790d7753f913
Rhodri Jones <rhodri.jones@upmind.com>
2025-06-05 17:33:30 +0100

    clear model after input + provide label for actions in model list

--- packages/headless/src/modules/basket/billing/unifiedAddress/schemas.ts
+++ packages/headless/src/modules/basket/billing/unifiedAddress/schemas.ts
@@ -419,7 +419,7 @@ export const useUischema = ({
         type: "ModelList",
         scope: "#/properties/addressId",
         options: {
-          label: "",
+          label: "Address",
           oneOf: map(addresses || [], item => ({
             id: item.id,
             title: item.title,
@@ -467,7 +467,7 @@ export const useUischema = ({
         type: "ModelList",
         scope: "#/properties/companyId",
         options: {
-          label: "",
+          label: "Company",
           oneOf: map(companies || [], item => ({
             id: item.id,
             title: item.title,
