5697c6ec5d468b6b5cde5caec5dcfaada62e2460
Rhodri Jones <rhodri.jones@upmind.com>
2025-06-06 12:20:07 +0100

    dialog layout renderer

--- src/ui/form/renderers/layouts/index.ts
+++ src/ui/form/renderers/layouts/index.ts
@@ -4,14 +4,19 @@ import { registerEntry } from "../utils";
 
 export { default as LayoutRenderer } from "./LayoutRenderer.vue";
 export { default as GroupRenderer } from "./GroupRenderer.vue";
+export { default as DialogRenderer } from "./DialogRenderer.vue";
 
 import LayoutRendererEntry, {
   tester as layoutTest,
 } from "./LayoutRenderer.vue";
 
 import GroupRendererEntry, { tester as groupTest } from "./GroupRenderer.vue";
+import DialogRendererEntry, {
+  tester as dialogTest,
+} from "./DialogRenderer.vue";
 
 export const layoutRenderers = [
   registerEntry(LayoutRendererEntry, layoutTest),
   registerEntry(GroupRendererEntry, groupTest),
+  registerEntry(DialogRendererEntry, dialogTest),
 ];
