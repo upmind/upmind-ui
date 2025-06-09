068da261303662aeebb815361f0c0fcc3ec5814c
Rhodri Jones <rhodri.jones@upmind.com>
2025-06-09 16:41:44 +0100

    file pond image renderer

--- packages/client-vue/src/components/form/renderers/ImageRenderer.vue
+++ packages/client-vue/src/components/form/renderers/ImageRenderer.vue
@@ -1,144 +1,92 @@
 <template>
-  <FormField v-bind="controlWrapper">
-    <input
-      ref="input"
-      :id="control.id + '-input'"
-      :class="[
-        styles.control.file,
-        controlWrapper.errors ? styles.control.error.input : null,
-        { hidden: meta.hasFile || meta.isProcessing },
-      ]"
-      :disabled="!control.enabled"
-      :autocomplete="appliedOptions.autocomplete"
-      :placeholder="appliedOptions.placeholder"
-      type="file"
-      accept="image/*"
-      @change="onChange"
-      @focus="isFocused = true"
-      @blur="isFocused = false"
+  <FormField v-bind="formFieldProps">
+    <FilePond
+      v-bind="appliedOptions"
+      :allow-multiple="false"
+      :accepted-file-types="fileTypes"
+      :max-files="1"
+      :max-file-size="'5MB'"
+      :label-idle="labelText"
+      :credits="false"
+      stylePanelLayout="integrated"
+      :class="styles.form.file"
+      @addfile="onAddFile"
+      @removefile="remove"
+      @change="remove"
     />
-    <aside
-      class="card sm:card-side rounded-btn bg-base-100 border"
-      v-if="meta.hasFile || meta.isProcessing"
-    >
-      <figure
-        class="relative m-0 aspect-square bg-neutral-100 sm:w-1/2 md:w-36"
-      >
-        <img
-          v-if="src"
-          :src="src"
-          alt="uploaded image thumbnail"
-          class="aspect-square h-full w-full rounded-l-lg object-cover"
-        />
-        <span
-          v-if="meta.isProcessing"
-          class="loading loading-dots absolute"
-        ></span>
-      </figure>
-      <div class="card-body p-4">
-        <h4 class="card-title m-0 text-base" v-if="name">{{ name }}</h4>
-        <span v-if="meta.isProcessing">Uploading image...</span>
-        <use-time-ago v-else-if="created" v-slot="{ timeAgo }" :time="created">
-          Uploaded {{ timeAgo }}
-        </use-time-ago>
-        <span v-else-if="!meta.isProcessing">Image uploaded</span>
-        <div class="card-actions mt-auto justify-end">
-          <button class="btn btn-ghost btn-sm" @click.prevent="onOpen">
-            Change Image
-          </button>
-          <button
-            class="btn btn-circle btn-ghost btn-sm"
-            @click.prevent="onRemove"
-            v-if="meta.isComplete"
-          >
-            <Icon icon="bin" class="h-6 w-6" />
-            <span class="sr-only">Remove image</span>
-          </button>
-        </div>
-      </div>
-    </aside>
   </FormField>
 </template>
 
 <script setup lang="ts">
-import { ref, onBeforeUnmount, computed } from "vue";
-import type {
-  ControlElement,
-  JsonFormsRendererRegistryEntry,
-} from "@jsonforms/core";
+import { onBeforeUnmount, computed } from "vue";
+import type { ControlElement } from "@jsonforms/core";
 import type { RendererProps } from "@jsonforms/vue";
-import { rendererProps, useJsonFormsControl } from "@jsonforms/vue";
-import { FormField, Icon } from "@upmind-automation/upmind-ui";
-import { useDaisyControl } from "../util";
+import { useJsonFormsControl } from "@jsonforms/vue";
+import { FormField } from "@upmind-automation/upmind-ui";
 import { useUpload } from "@upmind-automation/headless-vue";
-import { UseTimeAgo } from "@vueuse/components";
+import { useUpmindUIRenderer } from "@upmind-automation/upmind-ui";
+import { useStyles } from "@upmind-automation/upmind-ui";
 
-const props = defineProps<RendererProps<ControlElement>>();
-const input = ref<HTMLInputElement>();
+import vueFilePond from "vue-filepond";
+import "filepond/dist/filepond.min.css";
+import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css";
 
-const inputControl = useDaisyControl(
-  useJsonFormsControl(props),
-  (target: any) => file.value || target?.value || undefined
+import FilePondPluginFileValidateType from "filepond-plugin-file-validate-type";
+import FilePondPluginImagePreview from "filepond-plugin-image-preview";
+
+import config from "../form.config";
+import type { ComputedRef } from "vue";
+
+const FilePond = vueFilePond(
+  FilePondPluginFileValidateType,
+  FilePondPluginImagePreview
 );
 
-const {
-  created,
-  file,
-  name,
-  src,
-  errors,
-  meta,
-  add,
-  remove,
-  getImageByHash,
-  stop,
-} = useUpload(inputControl.appliedOptions.value.field);
+const props = defineProps<RendererProps<ControlElement>>();
+const fileTypes = ["image/jpeg", "image/png", "image/gif", "image/webp"];
+
+const { formFieldProps, appliedOptions } = useUpmindUIRenderer(
+  useJsonFormsControl(props)
+);
+
+const { add, remove, stop } = useUpload(appliedOptions.value.field);
+
+const styles = useStyles(["form.file"], {}, config) as ComputedRef<{
+  form: {
+    file: string;
+  };
+}>;
 
 onBeforeUnmount(() => stop());
 
-if (inputControl.control.value?.data) {
-  getImageByHash(inputControl.control.value.data);
+async function onAddFile(error: any, file: any) {
+  await add(file.file);
 }
 
-async function onChange(event: Event) {
-  const target = event.currentTarget as HTMLInputElement;
-  if (target?.files?.[0]) {
-    try {
-      await add(target.files[0] as any);
-    } catch (error) {}
-  }
-  inputControl.onChange(event);
-}
-
-function onRemove(event: Event) {
-  remove();
-  if (input.value) {
-    input.value.value = "";
-  }
-  inputControl.onChange(event);
-}
-
-function onOpen() {
-  input.value?.click();
-}
-
-const controlWrapper = computed(() => ({
-  ...inputControl.controlWrapper.value,
-  name: inputControl.control.value.path,
-}));
-
-const { control, handleChange, appliedOptions, isFocused, styles } =
-  inputControl;
+const labelText = computed(() => {
+  return `Drag & Drop your image or <span class="filepond--label-action">Browse</span`;
+});
 </script>
 
 <script lang="ts">
 import { and, or, uiTypeIs, optionIs, formatIs } from "@jsonforms/core";
 
 export const tester = {
-  rank: 3,
+  rank: 2,
   controlType: and(
     uiTypeIs("Control"),
-    or(optionIs("type", "file"), formatIs("file"), optionIs("type", "image"))
+    or(formatIs("file"), optionIs("type", "image"))
   ),
 };
 </script>
+
+<style>
+.filepond--image-preview-wrapper {
+  @apply rounded-lg !important;
+  border: 0 !important;
+}
+
+.filepond--image-preview {
+  background-color: transparent !important;
+}
+</style>
