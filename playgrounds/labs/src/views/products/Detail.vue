cda741514bc083a8e4cd7a37b49fd2ac2dac7afc
Rafael Macedo <rafaelmacedo4@gmail.com>
2025-06-12 11:44:38 +0100

    refactor: enhance caching and query client configuration, add product detail view, and update routing in product catalogue

---
+++ playgrounds/labs/src/views/products/Detail.vue
@@ -0,0 +1,14 @@
+<template>
+  <Button @click="goBack">Back</Button>
+</template>
+
+<script lang="ts" setup>
+import { useRouter } from "vue-router";
+import { Button } from "@upmind-automation/upmind-ui";
+
+const router = useRouter();
+
+function goBack() {
+  router.push({ name: "products.catalogue" });
+}
+</script>
