09f9365964e93c2df03ed9ee14afc6d46f913312
Rafael Macedo <rafaelmacedo4@gmail.com>
2025-06-13 00:41:04 +0100

    chore: add @tanstack/vue-query-devtools for enhanced debugging and integrate VueQueryDevtools in App component

--- playgrounds/labs/src/App.vue
+++ playgrounds/labs/src/App.vue
@@ -1,9 +1,11 @@
 <template>
+  <VueQueryDevtools buttonPosition="bottom-left" />
   <Upm :theme="theme" :logo="logo"> </Upm>
 </template>
 
 <script lang="ts" setup>
 import logo from "@/assets/logo.svg?url";
-import { Upm } from "@upmind-automation/client-vue";
 import theme from "./assets/theme";
+import { Upm } from "@upmind-automation/client-vue";
+import { VueQueryDevtools } from "@tanstack/vue-query-devtools";
 </script>
