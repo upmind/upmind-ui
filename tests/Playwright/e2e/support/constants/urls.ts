0dd5fcd2b99a6a15be2880c5589c1657897e2a94
Nathan Robinson <nathan.robinson@upmind.com>
2025-06-13 14:31:26 +0000

    QA: Additional coverage for product meta slot

--- tests/Playwright/e2e/support/constants/urls.ts
+++ tests/Playwright/e2e/support/constants/urls.ts
@@ -15,11 +15,12 @@ export const URLs = {
   /* ADD PRODUCT URLS */
   devBlocks: `http://collabstudio.local:5173/product/add/3de78642-de53-9714-542c-21208469530d`,
   starterHosting: `${baseUrl}product/add/3de78642-de53-9714-76df-21208469530d`,
+  advancedHosting: `http://collabstudio.local:5173/product/add/4d036794-24d0-e710-965b-3153698d582e`,
+  goldPlanHosting: `${baseUrl}product/add/5d085e69-d562-3719-4e8a-218e940d4237`,
   logoDesign: `http://collabstudio.local:5173/product/add/47d73824-8507-9315-345f-81e642d59e06`,
   developerRetainer: `http://collabstudio.local:5173/product/add/8d632507-9806-5d1e-572f-8174e234e98d`,
   meeting: `http://collabstudio.local:5173/product/add/47d73824-8507-9315-385b-81e642d59e06`,
-  advancedHosting: `http://collabstudio.local:5173/product/add/4d036794-24d0-e710-965b-3153698d582e`,
-  consultingBlock: `http://collabstudio.local:5173/product/add/5952098d-3de4-0917-e88b-31578626e347`,
+  consultingBlock: `http://qa-automation.local:5173/product/add/20403869-6e54-721d-264c-518d9305e7d2`,
 
   billingTermsPromo: `http://collabstudio.local:5173/product/add/20403869-6e54-721d-254a-518d9305e7d2`,
 
