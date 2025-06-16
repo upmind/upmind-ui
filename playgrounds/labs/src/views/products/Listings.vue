b72744d4259f55fd4a76cb6412f9e8bf93dc1329
Dominic Da Costa <dominic@thecollaboration.studio>
2025-06-16 15:00:58 +0200

    feat(products): refactor product filtering methods and implement InfiniteProducts component

--- playgrounds/labs/src/views/products/Listings.vue
+++ playgrounds/labs/src/views/products/Listings.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="flex gap-2">
     <ProductCategories class="w-1/3" v-model="modelValue" />
-    <PaginatedProducts
-      :limit="6"
+    <InfiniteProducts
+      v-if="!modelValue"
+      class="w-2/3"
+      :skeletonCount="4"
+      :limit="4"
+      :categoryId="modelValue"
+    />
+
+    <!-- <PaginatedProducts
+      :limit="4"
       class="w-2/3"
       :categoryId="modelValue"
-      :skeletonCount="6"
-    />
+      :skeletonCount="4"
+    /> -->
   </div>
 </template>
 
@@ -14,6 +22,7 @@
 import { ref } from "vue";
 
 import PaginatedProducts from "./Paginated.vue";
+import InfiniteProducts from "./Infinite.vue";
 import ProductCategories from "./Categories.vue";
 
 const modelValue = ref("");
