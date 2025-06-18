480b00426523db9b25475763fbdc66f8f52be323
Rafael Macedo <rafaelmacedo4@gmail.com>
2025-06-18 12:19:07 +0100

    Refactor category rendering by extracting `CategoryItem` component
    Replaced inline category rendering logic in `Categories.vue` with a reusable `CategoryItem` component. Simplified structure, improved readability, and introduced support for nested category hierarchies. Streamlined imports for better organization.

--- playgrounds/labs/src/views/products/Categories.vue
+++ playgrounds/labs/src/views/products/Categories.vue
@@ -16,21 +16,13 @@
           <span class="font-bold md:text-2xl">All</span>
         </UpmCard>
 
-        <template v-for="category in categories" :key="category.id">
-          <UpmCard
-            as="li"
-            @click="() => selectCategory(category.id)"
-            class="flex items-end rounded-none border-b border-primary bg-gradient-to-r from-primary to-primary bg-[length:0%_4px] bg-left-bottom bg-no-repeat px-6 py-5 text-xl transition-all duration-300 ease-in-out"
-            :class="{
-              'text-auto hover:text-dm/25 dark:hover:text-dm-contrast/50':
-                modelValue !== category.id,
-              'border-b-primary bg-[length:100%_4px] text-primary':
-                modelValue === category.id,
-            }"
-          >
-            <span class="font-bold md:text-2xl">{{ category.name }}</span>
-          </UpmCard>
-        </template>
+        <CategoryItem
+          v-for="category in categories"
+          v-model="modelValue"
+          :key="category.id"
+          :category="category"
+          :depth="0"
+        />
       </Loading>
     </ul>
   </div>
@@ -39,10 +31,11 @@
 <script setup lang="ts">
 import { useVModel } from "@vueuse/core";
 
-import { useProductCategories } from "@upmind-automation/headless";
+import { HTMLAttributes } from "vue";
 import { UpmCard } from "@upmind-automation/client-vue";
 import { Loading } from "@upmind-automation/upmind-ui";
-import { HTMLAttributes } from "vue";
+import CategoryItem from "./CategoryItem.vue";
+import { useProductCategories } from "@upmind-automation/headless";
 
 const props = withDefaults(
   defineProps<{
