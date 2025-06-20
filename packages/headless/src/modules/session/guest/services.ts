9363bc8c8a9e286f35ac56778c09c464dc30693f
Dominic Da Costa <dominic@thecollaboration.studio>
2025-06-20 10:18:56 +0200

    ensure recaptcha is cleared after any/all requests

--- packages/headless/src/modules/session/guest/services.ts
+++ packages/headless/src/modules/session/guest/services.ts
@@ -182,11 +182,11 @@ async function register({ model }: GuestContext<RegisterModel>) {
     url: useUrl("clients/register"),
     data,
   })
-    .then(({ data }: any) => {
+    .then(({ data }: any) => data)
+    .then(loadUser)
+    .finally(() => {
       recaptcha.clear(); // clear our recaptcha token that has been used
-      return data;
-    })
-    .then(loadUser);
+    });
 }
 
 async function recover({ model }: GuestContext<RecoverModel>) {
@@ -206,10 +206,11 @@ async function recover({ model }: GuestContext<RecoverModel>) {
   return post({
     url: useUrl("clients/password_reset"),
     data,
-  }).then(({ data }: any) => {
-    recaptcha.clear(); // clear our recaptcha token that has been used
-    return data;
-  });
+  })
+    .then(({ data }: any) => data)
+    .finally(() => {
+      recaptcha.clear(); // clear our recaptcha token that has been used
+    });
 }
 
 // -----------------------------------------------------------------------------
