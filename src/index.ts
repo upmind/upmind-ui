bf6332450f0a4bf96c79b08f5203defb86a2ac3d
Rhodri Jones <rhodri.jones@upmind.com>
2025-06-23 12:30:40 +0100

    export card

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ export * from "./ui/autocomplete";
 export * from "./ui/avatar";
 export * from "./ui/badge";
 export * from "./ui/button";
+export * from "./ui/card";
 export * from "./ui/carousel";
 export * from "./ui/checkbox";
 export * from "./ui/checkbox-cards";
