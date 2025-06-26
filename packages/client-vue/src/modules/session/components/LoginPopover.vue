5b0674fbe0c757a7d657ca43160a075b878196a4
Nathan Robinson <nathan.robinson@upmind.com>
2025-06-26 08:12:17 +0000

    QA: login, registration and error handling test updates

--- packages/client-vue/src/modules/session/components/LoginPopover.vue
+++ packages/client-vue/src/modules/session/components/LoginPopover.vue
@@ -1,11 +1,12 @@
 <template>
   <Popover>
-    <PopoverTrigger>
+    <PopoverTrigger data-testid="popover-trigger">
       <slot></slot>
     </PopoverTrigger>
     <PopoverContent
       class="relative z-30 mt-4 h-screen w-auto border-0 border-t p-0 md:mt-8 md:h-auto md:border"
       align="end"
+      data-testid="popover-content"
     >
       <div class="flex h-full flex-col md:flex-row">
         <div class="w-screen p-8 md:w-[26rem]">
