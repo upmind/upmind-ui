ac4b3331e9f868cdfbf2ea930c6b952c154f8e68
Dominic Da Costa <dominic@thecollaboration.studio>
2025-07-04 09:03:40 +0200

    refactor: update layout structure in Recommendations component and enhance theme variant handling

--- packages/client-vue/src/modules/product/Recommendations.vue
+++ packages/client-vue/src/modules/product/Recommendations.vue
@@ -1,55 +1,57 @@
 <template>
-  <aside v-auto-animate>
-    <div class="flex flex-col items-center justify-center p-2">
-      <SmartTitle
-        i18n-key="recommendations.header.title"
-        size="3xl"
-        align="center"
-      />
+  <Layout>
+    <aside v-auto-animate>
+      <div class="flex flex-col items-center justify-center p-2">
+        <SmartTitle
+          i18n-key="recommendations.header.title"
+          size="3xl"
+          align="center"
+        />
 
-      <p
-        class="text-emphasis-medium m-0 mb-12 mt-4 max-w-md text-center text-lg leading-normal"
-      >
-        {{ t("recommendations.header.subtitle") }}
-      </p>
-    </div>
-
-    <CardsCarousel
-      :loading="meta?.isLoading"
-      :processing="meta?.isProcessing"
-      :refreshing="meta?.isRefreshing"
-      :items="recommendations"
-      @resolve="doAdd"
-      @fetch="fetchRecommendation"
-    />
-
-    <Configure
-      v-if="basketItem?.id"
-      :modelValue="basketItem"
-      @resolve="doClose"
-    />
-
-    <Card
-      class="md:bg-base mt-8 flex flex-col items-center justify-between bg-transparent !p-0 shadow-none md:mt-8 md:flex-row md:!px-8 md:!py-6 md:shadow-sm"
-    >
-      <div
-        class="text-md order-last mt-4 text-center font-medium md:order-first md:mt-0 md:text-left"
-      >
-        {{ t("recommendations.toolbar.title", { count: products?.length }) }}
+        <p
+          class="text-emphasis-medium m-0 mb-12 mt-4 max-w-md text-center text-lg leading-normal"
+        >
+          {{ t("recommendations.header.subtitle") }}
+        </p>
       </div>
 
-      <Button
-        @click="doClose"
-        :label="t('recommendations.toolbar.actions.continue')"
-        color="primary"
-        class="w-full md:w-auto"
+      <CardsCarousel
+        :loading="meta?.isLoading"
+        :processing="meta?.isProcessing"
+        :refreshing="meta?.isRefreshing"
+        :items="recommendations"
+        @resolve="doAdd"
+        @fetch="fetchRecommendation"
+      />
+
+      <Configure
+        v-if="basketItem?.id"
+        :modelValue="basketItem"
+        @resolve="doClose"
+      />
+
+      <Card
+        class="md:bg-base mt-8 flex flex-col items-center justify-between bg-transparent !p-0 shadow-none md:mt-8 md:flex-row md:!px-8 md:!py-6 md:shadow-sm"
       >
-        <template #append>
-          <Icon icon="arrow-right" size="2xs" />
-        </template>
-      </Button>
-    </Card>
-  </aside>
+        <div
+          class="text-md order-last mt-4 text-center font-medium md:order-first md:mt-0 md:text-left"
+        >
+          {{ t("recommendations.toolbar.title", { count: products?.length }) }}
+        </div>
+
+        <Button
+          @click="doClose"
+          :label="t('recommendations.toolbar.actions.continue')"
+          color="primary"
+          class="w-full md:w-auto"
+        >
+          <template #append>
+            <Icon icon="arrow-right" size="2xs" />
+          </template>
+        </Button>
+      </Card>
+    </aside>
+  </Layout>
 </template>
 
 <script lang="ts" setup>
@@ -71,6 +73,7 @@ import { Button, Icon, Card } from "@upmind-automation/upmind-ui";
 import Configure from "../recommendations/components/Configure.vue";
 import CardsCarousel from "../recommendations/components/CardsCarousel.vue";
 import SmartTitle from "../../components/content/SmartTitle.vue";
+import Layout from "../../components/layout/Layout.vue";
 
 // -----------------------------------------------------------------------------
 
