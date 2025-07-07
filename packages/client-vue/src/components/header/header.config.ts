8c06b39c3349269f14f1436d2de5b19e6f78cf6d
Rhodri Jones <rhodri.jones@upmind.com>
2025-07-07 17:16:17 +0100

    header avatar tweaks

--- packages/client-vue/src/components/header/header.config.ts
+++ packages/client-vue/src/components/header/header.config.ts
@@ -4,8 +4,8 @@ import { cva } from "class-variance-authority";
 export default {
   header: {
     avatar: {
-      login: cva("bg-secondary-background text-secondary-foreground"),
-      session: cva("bg-secondary-background text-secondary-foreground")
+      login: cva("bg-primary-background text-primary-foreground"),
+      session: cva("bg-primary-background text-primary-foreground")
     }
   }
 };
