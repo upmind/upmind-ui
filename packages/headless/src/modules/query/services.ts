8209c6c25d881178e9b04b1a89024bd2dfc1a780
Dominic Da Costa <dominic@thecollaboration.studio>
2025-07-08 14:07:42 +0200

    disabled forbidden modal

--- packages/headless/src/modules/query/services.ts
+++ packages/headless/src/modules/query/services.ts
@@ -35,13 +35,7 @@ function handleError(
   error: QueryResponse["error"]
 ): Promise<never> {
   // of we have a system error, we want to add some feedback for the ui
-  if (
-    includes(
-      [responseCodes.Forbidden, responseCodes.Too_Many_Requests],
-      status
-    ) ||
-    status >= 500
-  ) {
+  if (includes([responseCodes.Too_Many_Requests], status) || status >= 500) {
     add({
       type: messageTypes.ERROR,
       title: "Service temporarily unavailable",
