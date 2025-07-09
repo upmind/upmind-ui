5922a725b413672cf37039063fc8bae044df3f5e
Rhodri Jones <rhodri.jones@upmind.com>
2025-07-09 13:07:51 +0100

    layout disable default (where we require template slots)

--- src/ui/layout/Layout.ce.vue
+++ src/ui/layout/Layout.ce.vue
@@ -37,7 +37,7 @@ const layoutComponent = computed(() => {
     case "full":
       return FullLayout;
     default:
-      return DefaultLayout;
+      return props.noDefault ? FullLayout : DefaultLayout;
   }
 });
 </script>
