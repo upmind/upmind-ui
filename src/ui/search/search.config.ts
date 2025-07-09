c56ff90e73006a396c5ff2c2f1cbca2a53f41984
Rhodri Jones <rhodri.jones@upmind.com>
2025-07-09 11:43:07 +0100

    select, search and combobox styling and consistency improvements

--- src/ui/search/search.config.ts
+++ src/ui/search/search.config.ts
@@ -11,7 +11,7 @@ export default {
     ),
     divider: cva("mx-3 h-[1px] border-t"),
     item: cva(
-      "text-emphasis-medium hover:text-emphasis-none cursor-pointer rounded-lg px-3 py-2 text-sm hover:bg-control-active hover:text-white"
+      "hover:bg-control-active-hover focus:bg-control-active-hover cursor-pointer rounded-lg px-3 py-2 text-sm text-control-foreground"
     )
   }
 };
