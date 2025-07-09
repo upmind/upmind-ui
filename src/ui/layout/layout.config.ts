89a12cedc667f05fde73b20693d88c82b22a6813
Dominic Da Costa <dominic@thecollaboration.studio>
2025-07-09 08:41:02 +0200

    fix: ensure controlsRoot has consistent width in layout configuration

--- src/ui/layout/layout.config.ts
+++ src/ui/layout/layout.config.ts
@@ -9,13 +9,13 @@ export default {
     root: cva(
       "max-w-app z-10 mx-auto flex w-full flex-col flex-wrap items-start justify-start gap-6 pb-16 pt-10 md:pb-32 md:pt-16"
     ),
-    controlsRoot: cva("border-b bg-base-background py-4"),
+    controlsRoot: cva("w-full border-b bg-base-background py-4"),
     controls: cva("max-w-app mx-auto")
   },
 
   full: {
     root: cva("z-10 mx-auto w-full flex-wrap items-start justify-start"),
-    controlsRoot: cva("border-b bg-base-background py-4"),
+    controlsRoot: cva("w-full border-b bg-base-background py-4"),
     controls: cva("max-w-app mx-auto"),
     header: cva("max-w-app mx-auto pb-10 pt-10 md:pb-20 md:pt-14"),
     contentRoot: cva("bg-base-background pb-10 pt-10 md:pb-16 md:pt-20"),
