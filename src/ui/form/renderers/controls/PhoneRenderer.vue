0452a2640af36649311de0f3e595444bebb119bb
Dominic Da Costa <dominic@thecollaboration.studio>
2025-07-11 14:15:01 +0200

    refactor: simplify form submission and validation logic by removing redundant functions and improving error handling

--- src/ui/form/renderers/controls/PhoneRenderer.vue
+++ src/ui/form/renderers/controls/PhoneRenderer.vue
@@ -32,7 +32,7 @@
 
     <template #messages>
       <FormMessage
-        v-if="formFieldProps.dirty && errors"
+        v-if="formFieldProps.touched && errors"
         :errors="[errorsMapped]"
         :formMessageId="`form-item-message-${control.id}`"
         :name="control.path"
@@ -72,7 +72,8 @@ import {
   filter,
   isString,
   first,
-  isEmpty
+  isEmpty,
+  has
 } from "lodash-es";
 
 // --- types
@@ -121,7 +122,7 @@ const exampleNumber = computed(() => {
 });
 
 const errors = computed(() => {
-  if (control?.value?.errors) {
+  if (!isEmpty(formFieldProps?.value?.errors)) {
     try {
       parsePhoneNumberWithError(phone.value.number, {
         defaultCountry: phone?.value?.country
@@ -240,16 +241,18 @@ import {
   isStringControl,
   isObjectControl,
   schemaMatches,
-  or
+  or,
+  formatIs
 } from "@jsonforms/core";
 
 export const tester = {
-  rank: 2,
+  rank: 10,
   controlType: and(
     or(isStringControl, isObjectControl),
-    schemaMatches(
-      schema =>
-        "phone_country_code" in schema && !!(schema as any).phone_country_code
+    or(
+      formatIs("phone"),
+      schemaMatches(schema => schema.format === "international_phone"),
+      schemaMatches(schema => has(schema, "phone_country_code"))
     )
   )
 };
