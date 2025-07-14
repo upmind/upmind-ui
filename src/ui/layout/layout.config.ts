c35cebbd315dabdca9ce9b6da8d7b496055e8548
Rhodri Jones <rhodri.jones@upmind.com>
2025-07-14 14:03:11 +0100

    control min-h

--- src/ui/layout/layout.config.ts
+++ src/ui/layout/layout.config.ts
@@ -3,7 +3,7 @@ import { cva } from "class-variance-authority";
 export default {
   control: {
     root: cva(
-      "flex h-12 w-full items-center justify-center border-b bg-base-background"
+      "flex min-h-12 w-full items-center justify-center border-b bg-base-background"
     ),
     content: cva("max-w-app flex w-full items-center justify-between"),
     controls: cva("flex-grow")
