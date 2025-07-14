3f1397bc6a98df243e9e9d9c7f57af70deb3a168
Rhodri Jones <rhodri.jones@upmind.com>
2025-07-14 15:57:25 +0100

    DAC label primary

--- packages/client-vue/src/modules/domain/domain.config.ts
+++ packages/client-vue/src/modules/domain/domain.config.ts
@@ -50,7 +50,7 @@ export default {
       label: cva("text-2xs m-0 uppercase", {
         variants: {
           isAvailable: {
-            true: "text-secondary",
+            true: "text-primary",
             false: "text-emphasis-disabled"
           }
         }
