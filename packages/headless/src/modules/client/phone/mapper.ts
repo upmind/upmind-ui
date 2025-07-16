644e8a7c7ce6e7be5271eefc0300b1d774f2d4a7
Dominic Da Costa <dominic@thecollaboration.studio>
2025-07-16 09:26:36 +0200

    refactor: simplify address and company mapping functions by removing omitBy and using nullish coalescing

--- packages/headless/src/modules/client/phone/mapper.ts
+++ packages/headless/src/modules/client/phone/mapper.ts
@@ -2,7 +2,7 @@
 import parsePhoneNumber, { CountryCode } from "libphonenumber-js";
 
 // --- utils
-import { map, get, compact, isArray, isNil, omitBy } from "lodash-es";
+import { map, get, compact, isArray } from "lodash-es";
 
 // --- types
 import type { IPhone } from "@upmind-automation/types";
@@ -40,12 +40,11 @@ export function mapPhone(raw: IPhone): Phone {
 }
 
 export function mapIPhone(data: PhoneModel): IPhone {
-  return omitBy(
-    {
-      phone: data.phone.nationalNumber, // without the country code
-      phone_code: `+${data.phone.countryCallingCode}`,
-      phone_country_code: data.phone.country
-    } as IPhone,
-    isNil
-  ) as IPhone;
+  return {
+    phone: data.phone.nationalNumber ?? null, // without the country code
+    phone_code: data.phone.countryCallingCode
+      ? `+${data.phone.countryCallingCode}`
+      : null,
+    phone_country_code: data.phone.country ?? null
+  } as IPhone;
 }
