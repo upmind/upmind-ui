37bcdcc4b9cc4ad2bfcfe939e503e65233099db7
Rhodri Jones <rhodri.jones@upmind.com>
2025-07-28 11:37:10 +0000

    feature/terms-and-conditions

--- tests/Playwright/e2e/support/constants/checkout/test-cases/webcentral/StarterHosting.ts
+++ tests/Playwright/e2e/support/constants/checkout/test-cases/webcentral/StarterHosting.ts
@@ -1,8 +1,8 @@
 export const StarterHosting = [
   /* Register New Domain */
   {
-    name: "Billing term: 1 Month - Account Domain Name: Register New Domain @webcentral @starter-hosting @new-domain",
-    radioSelection: [[0, 0]],
+    name: "Billing term: 1 Month - Account Domain Name: Register New Domain @starter-hosting @new-domain",
+    billingTerm: 0,
     accordionSelection: [0],
     total: "£4.00",
     billingCycle: "1 month",
@@ -10,8 +10,8 @@ export const StarterHosting = [
     domainName: "wcautotestingjghfjhf.uk"
   },
   {
-    name: "Billing term: 1 year - Account Domain Name: Register New Domain @webcentral @starter-hosting @new-domain",
-    radioSelection: [[0, 1]],
+    name: "Billing term: 1 year - Account Domain Name: Register New Domain @starter-hosting @new-domain",
+    billingTerm: 1,
     accordionSelection: [0],
     total: "£40.00",
     billingCycle: "1 year",
@@ -19,8 +19,8 @@ export const StarterHosting = [
     domainName: "wcautotestingasdfg.uk"
   },
   {
-    name: "Billing term: 2 year - Account Domain Name: Register New Domain @webcentral @starter-hosting @new-domain",
-    radioSelection: [[0, 2]],
+    name: "Billing term: 2 year - Account Domain Name: Register New Domain @starter-hosting @new-domain",
+    billingTerm: 2,
     accordionSelection: [0],
     total: "£60.00",
     billingCycle: "2 year",
@@ -30,8 +30,8 @@ export const StarterHosting = [
 
   /* Transfer Domain */
   {
-    name: "Billing term: 1 Month - Account Domain Name: Transfer Domain @webcentral @starter-hosting @transfer-domain",
-    radioSelection: [[0, 0]],
+    name: "Billing term: 1 Month - Account Domain Name: Transfer Domain @starter-hosting @transfer-domain",
+    billingTerm: 0,
     accordionSelection: [1],
     total: "£4.00",
     billingCycle: "1 month",
@@ -39,8 +39,8 @@ export const StarterHosting = [
     domainName: "wcautotestingjghfjhf.uk"
   },
   {
-    name: "Billing term: 1 year - Account Domain Name: Transfer Domain @webcentral @starter-hosting @transfer-domain",
-    radioSelection: [[0, 1]],
+    name: "Billing term: 1 year - Account Domain Name: Transfer Domain @starter-hosting @transfer-domain",
+    billingTerm: 1,
     accordionSelection: [1],
     total: "£40.00",
     billingCycle: "1 year",
@@ -48,8 +48,8 @@ export const StarterHosting = [
     domainName: "wcautotestingasdfg.uk"
   },
   {
-    name: "Billing term: 2 year - Account Domain Name: Transfer Domain @webcentral @starter-hosting @transfer-domain",
-    radioSelection: [[0, 2]],
+    name: "Billing term: 2 year - Account Domain Name: Transfer Domain @starter-hosting @transfer-domain",
+    billingTerm: 2,
     accordionSelection: [1],
     total: "£60.00",
     billingCycle: "2 year",
@@ -59,8 +59,8 @@ export const StarterHosting = [
 
   /* Use Existing Domain */
   {
-    name: "Billing term: 1 Month - Account Domain Name: Register New Domain @webcentral @starter-hosting @existing-domain",
-    radioSelection: [[0, 0]],
+    name: "Billing term: 1 Month - Account Domain Name: Register New Domain @starter-hosting @existing-domain",
+    billingTerm: 0,
     accordionSelection: [2],
     total: "£4.00",
     billingCycle: "1 month",
@@ -68,8 +68,8 @@ export const StarterHosting = [
     domainName: "wcautotestingjghfjhf.uk"
   },
   {
-    name: "Billing term: 1 year - Account Domain Name: Register New Domain @webcentral @starter-hosting @existing-domain",
-    radioSelection: [[0, 1]],
+    name: "Billing term: 1 year - Account Domain Name: Register New Domain @starter-hosting @existing-domain",
+    billingTerm: 1,
     accordionSelection: [2],
     total: "£40.00",
     billingCycle: "1 year",
@@ -77,8 +77,8 @@ export const StarterHosting = [
     domainName: "wcautotestingasdfg.uk"
   },
   {
-    name: "Billing term: 2 year - Account Domain Name: Register New Domain @webcentral @starter-hosting @existing-domain",
-    radioSelection: [[0, 2]],
+    name: "Billing term: 2 year - Account Domain Name: Register New Domain @starter-hosting @existing-domain",
+    billingTerm: 2,
     accordionSelection: [2],
     total: "£60.00",
     billingCycle: "2 year",
