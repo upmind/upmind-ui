829681e1cc2ce889be862de2cea1debdbc2703b7
Rhodri Jones <rhodri.jones@upmind.com>
2025-07-29 12:01:09 +0100

    image navigation and icon sizing

--- src/ui/icon/icon.config.ts
+++ src/ui/icon/icon.config.ts
@@ -1,28 +1,25 @@
 import { cva } from "class-variance-authority";
 // -----------------------------------------------------------------------------
 
-export const iconVariants = cva(
-  "inline-flex aspect-square flex-shrink-0 align-middle [&>svg]:size-full",
-  {
-    variants: {
-      size: {
-        auto: "size-[1lh]",
-        nano: "size-4",
-        "2xs": "size-5",
-        xs: "size-6",
-        sm: "size-8",
-        md: "size-10",
-        lg: "size-12",
-        xl: "size-14",
-        "2xl": "size-16",
-        "3xl": "size-20"
-      },
-      defaultVariants: {
-        size: "auto"
-      }
+export const iconVariants = cva("[&>svg]:size-full", {
+  variants: {
+    size: {
+      auto: "",
+      nano: "size-4",
+      "2xs": "size-5",
+      xs: "size-6",
+      sm: "size-8",
+      md: "size-10",
+      lg: "size-12",
+      xl: "size-14",
+      "2xl": "size-16",
+      "3xl": "size-20"
+    },
+    defaultVariants: {
+      size: "auto"
     }
   }
-);
+});
 
 export default {
   icon: iconVariants
