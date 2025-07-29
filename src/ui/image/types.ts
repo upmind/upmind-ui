19a973cb3bba5454e4baf76bf932b100ca6b1d88
Rhodri Jones <rhodri.jones@upmind.com>
2025-07-29 12:43:05 +0100

    image fit prop

--- src/ui/image/types.ts
+++ src/ui/image/types.ts
@@ -1,12 +1,15 @@
 import type { VariantProps } from "class-variance-authority";
 import type { rootVariant } from "./image.config";
+import type { HTMLAttributes } from "vue";
 
 export type RootVariants = VariantProps<typeof rootVariant>;
 
 export interface ImageProps {
   images: ImageItem[] | ImageItem;
   ratio?: RootVariants["ratio"];
-  class?: string;
+  fit?: RootVariants["fit"];
+  // ---
+  class?: HTMLAttributes["class"];
 }
 
 export interface ImageItem {
