27c50fe430d87fdde512319467b5f55edaf689bf
Rhodri Jones <rhodri.jones@upmind.com>
2025-08-01 10:29:25 +0000

    feature/promotional-and-text-brand-styles-improvements

--- packages/client-vue/src/components/header/header.config.ts
+++ packages/client-vue/src/components/header/header.config.ts
@@ -5,7 +5,7 @@ export default {
   header: {
     avatar: {
       login: cva("bg-primary-background text-foreground"),
-      session: cva("bg-primary-background text-foreground")
+      session: cva("bg-primary-background text-primary-foreground")
     }
   }
 };
