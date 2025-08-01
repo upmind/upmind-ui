3fd201d23cd70b750a23f79613fe3b9a824dbfed
Rhodri Jones <rhodri.jones@upmind.com>
2025-08-01 16:05:40 +0100

    select cards WIP 2

--- src/ui/select-cards/components/Item.vue
+++ src/ui/select-cards/components/Item.vue
@@ -1,11 +1,16 @@
 <template>
-  <li class="list-none">
-    {{ label }}
+  <li class="flex w-full list-none justify-between text-md font-medium">
+    <span class="flex gap-2">
+      {{ label }}
+      <Badge v-if="badge" v-bind="badge" />
+    </span>
+    <span>{{ appendLabel }}</span>
   </li>
 </template>
 
 <script setup lang="ts">
 import type { SelectCardsItemProps } from "../types";
+import { Badge } from "../../badge";
 
-const props = defineProps<SelectCardsItemProps>();
+defineProps<SelectCardsItemProps>();
 </script>
