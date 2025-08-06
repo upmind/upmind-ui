f34fb97712116aee48238a512235a91b4df446cf
Rhodri Jones <rhodri.jones@upmind.com>
2025-08-06 12:05:57 +0100

    combobox gap

--- src/ui/combobox/combobox.config.ts
+++ src/ui/combobox/combobox.config.ts
@@ -2,7 +2,7 @@ import { cva } from "class-variance-authority";
 import { ringClasses, invalidRingClasses } from "../input/input.config";
 
 export const triggerVariants = cva(
-  "h-auto min-h-10 justify-start overflow-hidden rounded px-3 transition-all duration-300",
+  "h-auto min-h-10 justify-start gap-2 overflow-hidden rounded px-3 transition-all duration-300",
   {
     variants: {
       width: {
