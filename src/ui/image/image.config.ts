e2dce1fb0cabb252606e3abf4279a111b553d436
Rhodri Jones <rhodri.jones@upmind.com>
2025-08-11 16:40:58 +0100

    image height/container improvements

--- src/ui/image/image.config.ts
+++ src/ui/image/image.config.ts
@@ -27,42 +27,27 @@ export const rootVariant = cva("h-full w-full", {
 
 export default {
   image: {
-    container: cva("h-full overflow-hidden rounded-lg"),
+    container: cva("h-full overflow-hidden rounded-lg", {
+      variants: {
+        ratio: {
+          default: "",
+          "1:1": "aspect-square",
+          "4:3": "aspect-[4/3]",
+          "3:2": "aspect-[3/2]",
+          "16:9": "aspect-video",
+          "18:6": "aspect-[18/6]"
+        }
+      },
+      defaultVariants: {
+        ratio: "1:1"
+      }
+    }),
     root: rootVariant,
     icon: cva("opacity-25"),
     carousel: {
-      root: cva("h-full overflow-hidden rounded-lg", {
-        variants: {
-          ratio: {
-            default: "",
-            "1:1": "aspect-square",
-            "4:3": "aspect-[4/3]",
-            "3:2": "aspect-[3/2]",
-            "16:9": "aspect-video",
-            "18:6": "aspect-[18/6]"
-          }
-        },
-        defaultVariants: {
-          ratio: "1:1"
-        }
-      }),
       content: cva(""),
       list: cva("m-0 flex p-0"),
-      item: cva("pl-0", {
-        variants: {
-          ratio: {
-            default: "h-auto w-full",
-            "1:1": "h-full w-full",
-            "4:3": "h-full w-full",
-            "3:2": "h-full w-full",
-            "16:9": "h-full w-full",
-            "18:6": "h-full w-full"
-          }
-        },
-        defaultVariants: {
-          ratio: "1:1"
-        }
-      })
+      item: cva("pl-0")
     },
     nav: {
       root: cva("absolute bottom-[22px] right-[22px] flex gap-0.5"),
