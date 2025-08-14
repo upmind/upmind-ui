52e7d1ab4a9a32d82c1da5d1c3ce2c0b9dd04078
Dominic Da Costa <dominic@thecollaboration.studio>
2025-08-14 15:43:04 +0200

    feat: add tailwindcss plugin to Vite configuration for improved styling support

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 import dts from 'vite-plugin-dts';
+import tailwindcss from "@tailwindcss/vite";
+
 
 export default defineConfig({
   plugins: [
     vue(),
+    tailwindcss(),
     dts({
       entryRoot: "src",
       outDir: "dist",
