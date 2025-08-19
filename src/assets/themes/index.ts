7c80e748a33ac67793843820497133b95e0c97a5
Dominic Da Costa <dominic@thecollaboration.studio>
2025-08-19 12:59:20 +0200

    refactor: remove unused dark and light theme files and update theme interface

--- src/assets/themes/index.ts
+++
@@ -1,5 +0,0 @@
-// -- internal
-import light from "./light";
-import dark from "./dark";
-// -----------------------------------------------------------------------------
-export default [light, dark];
