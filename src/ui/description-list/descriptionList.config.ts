303ff309ce1858ef2a8c21cbfbfe0d28ed19d6e2
Rhodri Jones <rhodri.jones@upmind.com>
2025-08-19 17:11:56 +0100

    descriptionlist v-auto-animate

--- src/ui/description-list/descriptionList.config.ts
+++ src/ui/description-list/descriptionList.config.ts
@@ -5,7 +5,7 @@ import { cva } from "class-variance-authority";
 export default {
   list: {
     root: cva("text-sm/loose"),
-    term: cva("text-emphasis-medium flex-shrink-0 text-left"),
+    term: cva("text-emphasis-medium text-left"),
     description: cva("text-right"),
     item: cva("flex items-center justify-between"),
     slotItem: cva("flex items-center justify-between")
