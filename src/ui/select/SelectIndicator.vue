064f12f9383cc608df8b5f8a62337c264939f37e
Rhodri Jones <rhodri.jones@upmind.com>
2025-08-22 16:25:56 +0100

    checkbox and radio, no border/shadow-border when selected

--- src/ui/select/SelectIndicator.vue
+++ src/ui/select/SelectIndicator.vue
@@ -3,9 +3,9 @@
     v-bind="forwardedProps"
     :class="
       cn(
-        'size-lh focus-visible:ring-ring shadow-border data-[state=checked]:border-control-active-background text-primary ring-offset-background flex aspect-square h-4.5 w-4.5 items-center justify-center rounded-full focus:outline-hidden focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+        'focus-visible:ring-ring shadow-border text-primary ring-offset-background flex aspect-square h-4.5 w-4.5 items-center justify-center rounded-full border-none focus:outline-hidden focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
 
-        'data-[state=checked]:text-control-active-foreground bg-base-background text-control-foreground focus-within:ring-control-active active:ring-control-active data-[state=checked]:bg-control-active-background shrink-0 leading-normal ring-offset-2 outline-hidden [transition:border-color_200ms_ease-in-out] focus-within:ring-2 focus-within:ring-offset-2 focus-within:outline-hidden active:ring-2 active:ring-offset-2',
+        'data-[state=checked]:text-control-active-foreground bg-base-background text-control-foreground data-[state=checked]:bg-control-active data-[state=checked]:shadow-border-none shrink-0',
         props.class,
         props.noInput ? 'sr-only' : ''
       )
@@ -21,10 +21,8 @@
 import { cn } from "../../utils";
 import { Circle } from "lucide-vue-next";
 import {
-  SelectItem,
   SelectItemIndicator,
   type SelectItemProps,
-  SelectItemText,
   useForwardProps
 } from "radix-vue";
 
