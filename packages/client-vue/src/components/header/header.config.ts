1aeaee0752d028da92fa9a99bacd1c407a41c6a5
Rhodri Jones <rhodri.jones@upmind.com>
2025-08-28 16:21:11 +0100

    default header layout

--- packages/client-vue/src/components/header/header.config.ts
+++ packages/client-vue/src/components/header/header.config.ts
@@ -9,9 +9,13 @@ export default {
     container: cva("mx-auto flex w-full items-center justify-between", {
       variants: {
         layout: {
+          default: "max-w-app-lg",
           full: "max-w-app",
           enclosed: "max-w-app-lg"
         }
+      },
+      defaultVariants: {
+        layout: "default"
       }
     }),
     anchor: cva(
