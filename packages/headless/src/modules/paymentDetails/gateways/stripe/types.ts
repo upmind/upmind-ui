f624dd861763cb486967a345bb5a64195e36b402
Dominic Da Costa <dominic@thecollaboration.studio>
2025-09-08 17:04:47 +0200

    fix: enhance payment processing by adding currency validation and minor unit conversion added SEPA Added IDEAL

--- packages/headless/src/modules/paymentDetails/gateways/stripe/types.ts
+++ packages/headless/src/modules/paymentDetails/gateways/stripe/types.ts
@@ -36,7 +36,7 @@ export enum STRIPE_PAYMENT_METHOD_TYPES {
   FPX = "fpx", // NYS
   GIROPAY = "giropay", // NYS
   GRABPAY = "grabpay", // NYS
-  IDEAL = "ideal", // NYS
+  IDEAL = "ideal",
   KLARNA = "klarna", // NYS
   KONBINI = "konbini", // NYS
   OXXO = "oxxo", // NYS
@@ -45,10 +45,57 @@ export enum STRIPE_PAYMENT_METHOD_TYPES {
   PAYPAL = "paypal",
   PIX = "pix", // NYS
   PROMPTPAY = "promptpay", // NYS
-  SEPA_DEBIT = "sepa_debit", // NYS
+  SEPA_DEBIT = "sepa_debit",
   SOFORT = "sofort", // NYS
   US_BANK_ACCOUNT = "us_bank_account", // NYS
   WECHAT_PAY = "wechat_pay" // NYS
+}
+
+export enum PAYPAL_PC {
+  // PayPal Presentment Currencies
+  AUD = "AUD", // Australian Dollar
+  CAD = "CAD", // Canadian Dollar
+  CHF = "CHF", // Swiss Franc
+  CZK = "CZK", // Czech Koruna
+  DKK = "DKK", // Danish Krone
+  EUR = "EUR", // Euro
+  GBP = "GBP", // British Pound
+  HKD = "HKD", // Hong Kong Dollar
+  NOK = "NOK", // Norwegian Krone
+  NZD = "NZD", // New Zealand Dollar
+  PLN = "PLN", // Polish Zloty
+  SEK = "SEK", // Swedish Krona
+  SGD = "SGD", // Singapore Dollar
+  USD = "USD" // US Dollar
+}
+
+export enum SEPA_PC {
+  // SEPA Presentment Currencies
+  EUR = "EUR" // Euro
+}
+
+export enum IDEAL_PC {
+  // iDEAL Presentment Currencies
+  EUR = "EUR" // Euro
+}
+
+export enum ZERO_DECIMAL_CURRENCIES {
+  BIF = "BIF", // Burundian Franc
+  CLP = "CLP", // Chilean Peso
+  DJF = "DJF", // Djiboutian Franc
+  GNF = "GNF", // Guinean Franc
+  JPY = "JPY", // Japanese Yen
+  KMF = "KMF", // Comorian Franc
+  KRW = "KRW", // South Korean Won
+  MGA = "MGA", // Malagasy Ariary
+  PYG = "PYG", // Paraguayan Guarani
+  RWF = "RWF", // Rwandan Franc
+  UGX = "UGX", // Ugandan Shilling
+  VND = "VND", // Vietnamese Dong
+  VUV = "VUV", // Vanuatu Vatu
+  XAF = "XAF", // Central African Franc
+  XOF = "XOF", // West African Franc
+  XPF = "XPF" // CFP Franc
 }
 
 export interface StripeContext extends GatewayContext {
