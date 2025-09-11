22d64ed60b2ae7a35f8b0833a2d98a9ee6115ba2
Dominic Da Costa <dominic@thecollaboration.studio>
2025-09-11 13:26:45 +0200

    fix getTracking double encoding/parsing

--- packages/headless/src/modules/system/analytics/useTracking.ts
+++ packages/headless/src/modules/system/analytics/useTracking.ts
@@ -127,6 +127,7 @@ export const useTracking = () => {
   function getTracking() {
     return new Promise((resolve, reject) => {
       const cookie = getCookie(UPM_TRACK_COOKIE);
+
       if (!cookie)
         return reject(
           new DetailedError(
