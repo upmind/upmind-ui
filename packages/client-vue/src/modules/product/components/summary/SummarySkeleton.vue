00bb365dca432bb6e80ac63c0d5fce17ce1f3b56
Rhodri Jones <rhodri.jones@upmind.com>
2025-09-12 10:18:27 +0100

    config header and summary skeleton

---
+++ packages/client-vue/src/modules/product/components/summary/SummarySkeleton.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="mt-4 flex flex-col gap-3">
+    <div class="flex justify-between gap-2">
+      <Skeleton class="h-5 w-24" />
+      <Skeleton class="h-5 w-32" />
+    </div>
+
+    <div class="flex justify-between gap-2">
+      <Skeleton class="h-5 w-32" />
+      <Skeleton class="h-5 w-44" />
+    </div>
+
+    <div class="flex justify-between gap-2">
+      <Skeleton class="h-5 w-24" />
+      <Skeleton class="h-5 w-28" />
+    </div>
+
+    <div class="mt-2 flex items-end justify-between gap-2">
+      <Skeleton class="h-8 w-20" />
+      <Skeleton class="h-10 w-40" />
+    </div>
+
+    <Skeleton class="mt-6 h-12 w-full rounded-full" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Skeleton, useStyles } from "@upmind-automation/upmind-ui";
+</script>
