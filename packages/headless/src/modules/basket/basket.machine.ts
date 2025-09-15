1738bd7fb03ffe7a990295b6a23ac6571b8a5514
Dominic Da Costa <dominic@thecollaboration.studio>
2025-09-15 16:10:43 +0200

    feat: add restart mechanism for basket process upon order completion

--- packages/headless/src/modules/basket/basket.machine.ts
+++ packages/headless/src/modules/basket/basket.machine.ts
@@ -389,8 +389,14 @@ export default createMachine(
       // TODO: actual payment node.
 
       complete: {
-        id: "complete"
-        // type: "final"
+        id: "complete",
+        // restart the baslket process once the order is complete
+        after: {
+          wait: {
+            target: "loading",
+            actions: ["clearBasket", "clearActors"]
+          }
+        }
       }
     },
     on: {
