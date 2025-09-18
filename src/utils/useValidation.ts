9a60d5b907139797d48ecf7308edb3328110305b
Rafael Macedo <rafaelmacedo4@gmail.com>
2025-09-18 13:59:20 +0100

    Improve i18n translation handling and refactor validation logic
    Updated translation logic to simplify key handling and added `trimStart` for better key processing. Enhanced validation utility by refining the schema validation flow and removing redundant parameters for cleaner and more focused functionality.

--- src/utils/useValidation.ts
+++ src/utils/useValidation.ts
@@ -1,26 +1,23 @@
 // --- external
-import Ajv from "ajv";
-import ajvErrors from "ajv-errors";
 import { createAjv, type JsonSchema } from "@jsonforms/core";
+import Ajv from "ajv";
+
+import ajvErrors from "ajv-errors";
 
 // --- internal
 
 // --- utils
-import { isFunction } from "lodash-es";
 
 // --- types
 import type { ErrorObject } from "ajv";
+
 // -----------------------------------------------------------------------------
 
-export const useValidation = (
-  ajv?: Ajv,
-  validate?: (schema: JsonSchema, data: Record<string, any>) => ErrorObject[]
-) => {
+export const useValidation = (ajv?: Ajv) => {
   // use JSON Forms version of AJV as it has formats and other keywords already
 
   const initial = !ajv;
   const ajvInstance = ajv ?? createAjv({ useDefaults: true, verbose: false });
-  const validationOverride = validate;
 
   if (initial) {
     ajvErrors(ajvInstance, {
@@ -35,13 +32,10 @@ export const useValidation = (
       schema: JsonSchema,
       data: Record<string, any>
     ): ErrorObject[] => {
-      if (isFunction(validationOverride))
-        return validationOverride(schema, data);
-
-      const validator = ajvInstance.compile(schema);
-      const valid = validator(data);
+      const validate = ajvInstance.compile(schema);
+      const valid = validate(data);
       if (!valid) {
-        return validator.errors ?? [];
+        return validate.errors ?? [];
       }
       return [];
     }
