28cf204c1bd39a2692dc3e43d6c2fc88b7df2e36
Rhodri Jones <rhodri.jones@upmind.com>
2025-09-26 13:11:54 +0100

    textarea hover

--- src/ui/textarea/textarea.config.ts
+++ src/ui/textarea/textarea.config.ts
@@ -3,7 +3,7 @@ import { cva } from "class-variance-authority";
 import { ringClasses, invalidRingClasses } from "../../assets/ring.styles";
 
 export const textareaVariants = cva(
-  `bg-background-control-surface hover:border-border-control-hover border-border-control-default text-control-foreground placeholder:text-muted-foreground flex w-full rounded border transition-all duration-200 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:cursor-not-allowed disabled:opacity-50 ${ringClasses} ${invalidRingClasses}`,
+  `bg-background-control-surface [&:hover,&[data-hover=true]]:shadow-border-border-control-hover border-border-control-default text-control-foreground placeholder:text-muted-foreground flex w-full rounded border transition-all duration-200 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:cursor-not-allowed disabled:opacity-50 ${ringClasses} ${invalidRingClasses}`,
   {
     variants: {
       size: {
