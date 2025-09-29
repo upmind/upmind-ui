761801c2f33ef0a446ff643b6f0837f2f4cd2270
Rhodri Jones <rhodri.jones@upmind.com>
2025-09-29 12:07:48 +0100

    position loading spinner centrally

--- src/ui/button/Button.ce.vue
+++ src/ui/button/Button.ce.vue
@@ -38,7 +38,11 @@
       />
     </slot>
 
-    <Spinner v-if="loading" size="sm" class="absolute" />
+    <Spinner
+      v-if="loading"
+      size="sm"
+      class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+    />
   </component>
 </template>
 
