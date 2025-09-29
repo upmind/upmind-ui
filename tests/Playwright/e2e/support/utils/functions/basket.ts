aa260f69d86648f90f3ab69bd2066449bd1d6eb4
Nathan Robinson <nathan.robinson@upmind.com>
2025-09-29 15:24:23 +0000

    QA: Edit Product tests

--- tests/Playwright/e2e/support/utils/functions/basket.ts
+++ tests/Playwright/e2e/support/utils/functions/basket.ts
@@ -1,14 +1,18 @@
 import { request, APIRequestContext, Page } from "@playwright/test";
 import { URLs } from "../../constants/urls";
 
-export async function getCurrentOrder(token: string) {
-  const response = await fetch(`${URLs.apiUrl}api/orders/current`, {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${token}`,
-      Accept: "application/json"
+export async function getBasketProducts(token: string) {
+  const response = await fetch(
+    `${URLs.apiUrl}api/orders/current?with=address%2Caddress.country%2Ccurrency%2Ccustom_fields.field%2Cpromotions%2Ctaxes%2Ctaxes.tax_tag_data%2Cproducts.product.image%2Cproducts.product.images%2Cproducts.product.prices%2Cproducts.product.products_attributes%2Cproducts.product.products_attributes.category%2Cproducts.product.products_options%2Cproducts.product.products_options.category%2Cproducts.product.products_options.prices%2Cproducts.product.provision_blueprint%2Cproducts.product.provision_field_values%2Cproducts.tags%2Cproducts.product.related%2Cproducts.product.category%2Cproducts.product.category.top_category.top_category.top_category.top_category&lang=en`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        Accept: "application/json",
+        Origin: `${URLs.apiOrigin}`
+      }
     }
-  });
+  );
 
   if (!response.ok) {
     const errorText = await response.text();
@@ -16,9 +20,9 @@ export async function getCurrentOrder(token: string) {
       `Failed to fetch current order: ${response.status} ${response.statusText} - ${errorText}`
     );
   }
-  const data = await response.json();
-  //console.log(`Current Basket: ${JSON.stringify(data)}`);
-  return data;
+  const body = await response.json();
+  console.log(`Current Basket: ${JSON.stringify(body.data.products)}`);
+  return body.data.products ?? null;
 }
 
 export async function getCurrentOrderId(token: string): Promise<string | null> {
