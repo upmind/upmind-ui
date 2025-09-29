6c6db21b190dc74a142cc93af1c81b46173560aa
Rhodri Jones <rhodri.jones@upmind.com>
2025-09-29 12:38:45 +0100

    tooltip same colours as alerts

--- src/ui/tooltip/tooltip.config.ts
+++ src/ui/tooltip/tooltip.config.ts
@@ -2,25 +2,23 @@
 import { cva } from "class-variance-authority";
 // -----------------------------------------------------------------------------
 
+export const variants = {
+  color: {
+    neutral: "bg-background-accent-neutral text-text-accent-neutral-contrast",
+    promo: "bg-background-accent-promo text-text-accent-promo-contrast",
+    danger: "bg-background-accent-danger text-text-accent-danger-contrast",
+    warning: "bg-background-accent-warning text-text-accent-warning-contrast",
+    success: "bg-background-accent-success text-text-accent-success-contrast",
+    info: "bg-background-accent-info text-text-accent-info-contrast"
+  }
+};
+
 export const contentVariants = cva(
   "animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 overflow-hidden rounded-xs border-transparent px-3 py-1.5 text-sm",
   {
-    variants: {
-      color: {
-        base: "text-base-muted bg-base-foreground",
-        primary: "bg-primary text-primary-foreground",
-        secondary: "bg-secondary text-secondary-foreground",
-        accent: "bg-accent text-accent-foreground",
-        promotion: "bg-promotion text-promotion-foreground",
-        destructive: "bg-destructive text-destructive-foreground",
-        success: "bg-success text-success-foreground",
-        info: "bg-info text-info-foreground",
-        error: "bg-error text-error-foreground",
-        warning: "bg-warning text-warning-foreground"
-      }
-    },
+    variants,
     defaultVariants: {
-      color: "base"
+      color: "neutral"
     }
   }
 );
@@ -28,20 +26,17 @@ export const contentVariants = cva(
 export const arrowVariants = cva("", {
   variants: {
     color: {
-      base: "text-base-foreground",
-      primary: "text-primary",
-      secondary: "text-secondary",
-      accent: "text-accent",
-      promotion: "text-promotion",
-      destructive: "text-destructive",
-      success: "text-success",
-      info: "text-info",
-      error: "text-error",
-      warning: "text-warning"
+      neutral: "text-text-accent-neutral",
+      promo: "text-text-accent-promo",
+      danger: "text-text-accent-danger",
+      warning: "text-text-accent-warning",
+      success: "text-text-accent-success",
+      info: "text-text-accent-info",
+      error: "text-text-accent-error"
     }
   },
   defaultVariants: {
-    color: "base"
+    color: "neutral"
   }
 });
 
