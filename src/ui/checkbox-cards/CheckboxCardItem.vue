74cf53019bbc98eb7d238fdc76b59d51af27547e
Rhodri Jones <rhodri.jones@upmind.com>
2025-10-07 13:26:28 +0100

    checkbox and radio polish

--- src/ui/checkbox-cards/CheckboxCardItem.vue
+++ src/ui/checkbox-cards/CheckboxCardItem.vue
@@ -17,10 +17,10 @@
       <span
         :class="
           cn(
-            ringClasses,
+            'group-focus-visible:ring-bg-control-ring group-focus:ring-bg-control-ring group-active:ring-bg-control-ring outline-hidden group-focus:ring-2 group-focus:ring-offset-2 group-focus-visible:ring-2 group-focus-visible:ring-offset-2 group-active:ring-2',
             'ring-offset-bg-control-surface',
             'data-[state=unchecked]:shadow-control-default data-[state=checked]:shadow-control-checked text-primary aspect-square h-4 w-4 rounded-sm disabled:cursor-not-allowed disabled:opacity-50',
-            'text-control-foreground data-[state=checked]:bg-control-checked data-[state=unchecked]:group-hover:shadow-control-hover flex shrink-0 cursor-pointer items-center justify-center transition-all duration-200'
+            'text-control-foreground data-[state=checked]:bg-control-checked data-[state=unchecked]:group-hover:shadow-control-hover flex shrink-0 cursor-pointer items-center justify-center transition-none duration-0'
           )
         "
         :data-state="props.checked ? 'checked' : 'unchecked'"
@@ -43,7 +43,6 @@ import {
   useForwardProps,
   type ToggleGroupItemProps
 } from "radix-vue";
-import { ringClasses } from "../../assets/ring.styles";
 import { computed, type HTMLAttributes } from "vue";
 
 const props = defineProps<
--- src/ui/radio-group/RadioGroupItem.vue
+++ src/ui/radio-group/RadioGroupItem.vue
@@ -6,7 +6,7 @@
         ringClasses,
         'ring-offset-bg-control-surface',
         'data-[state=unchecked]:shadow-control-default data-[state=checked]:shadow-control-checked text-primary aspect-square h-4 w-4 rounded-full disabled:cursor-not-allowed disabled:opacity-50',
-        'text-control-foreground data-[state=checked]:bg-control-checked data-[state=unchecked]:hover:shadow-control-hover shrink-0 cursor-pointer transition-all duration-200',
+        'text-control-foreground data-[state=checked]:bg-control-checked data-[state=unchecked]:hover:shadow-control-hover shrink-0 cursor-pointer transition-none duration-0',
         props.class,
         props.noInput ? 'sr-only' : ''
       )
