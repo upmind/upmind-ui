2067539662b530cdde586e27335f2e6e62519270
Rhodri Jones <rhodri.jones@upmind.com>
2025-10-09 10:57:25 +0100

    button group styling improvements

--- src/ui/button-group/buttonGroup.config.ts
+++ src/ui/button-group/buttonGroup.config.ts
@@ -3,7 +3,7 @@ import { cva, cx } from "class-variance-authority";
 import { focusWithinRing } from "../../assets/ring.styles";
 
 export const rootVariants = cva(
-  `control-radius divide-border-control-default shadow-control-default [&:hover,&:focus-within]:shadow-control-hover flex divide-x overflow-hidden ${focusWithinRing}`
+  `control-radius divide-border-control-default shadow-control-default bg-control-surface [&:hover,&:focus-within]:shadow-control-hover flex divide-x overflow-hidden ${focusWithinRing}`
 );
 
 export const buttonVariants = cva("px-3 py-1");
