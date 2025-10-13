dfe1223b81cb4cfde90237bd4e61a747bd75681a
Rhodri Jones <rhodri.jones@upmind.com>
2025-10-13 15:46:54 +0100

    drawer housekeeping

--- src/ui/drawer/DrawerContent.vue
+++ src/ui/drawer/DrawerContent.vue
@@ -43,11 +43,8 @@ const forwarded = useForwardPropsEmits(props, emits);
           props.class
         )
       "
+      data-testid="drawer-content"
     >
-      <!-- <div
-        class="drag-handle bg-base-muted mx-auto mt-4 min-h-2 w-[100px] rounded-full"
-        data-testid="drawer-content"
-      /> -->
       <slot />
     </DrawerContent>
   </DrawerPortal>
