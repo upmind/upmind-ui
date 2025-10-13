1463148b75457dc3c687bdd65bb880d1948824b3
Rhodri Jones <rhodri.jones@upmind.com>
2025-10-13 11:57:33 +0100

    loading variants

--- src/ui/loading/loading.config.ts
+++ src/ui/loading/loading.config.ts
@@ -5,25 +5,49 @@ import { cva } from "class-variance-authority";
 export const loadingVariants = cva();
 
 // -----------------------------------------------------------------------------
+const variants = {
+  isActive: {
+    true: "",
+    false: ""
+  },
+  isTransparent: {
+    true: "",
+    false: ""
+  }
+};
 export default {
   loading: {
     root: cva("", {
-      variants: {
-        isTransparent: {
-          true: "bg-core-surface/60",
-          false: "bg-core-surface"
+      variants,
+      compoundVariants: [
+        {
+          isActive: true,
+          isTransparent: true,
+          class: "bg-core-surface/60"
+        },
+        {
+          isActive: true,
+          isTransparent: false,
+          class: "bg-core-surface"
         }
-      }
+      ]
     }),
     spinner: cva(
       "text-control-default z-50 flex w-full items-center justify-center",
       {
-        variants: {
-          isTransparent: {
-            true: "bg-core-surface/60",
-            false: "bg-core-surface"
+        variants,
+        compoundVariants: [
+          {
+            isActive: true,
+            isTransparent: true,
+            class: "bg-core-surface/60"
+          },
+          {
+            isActive: true,
+            isTransparent: false,
+            class: "bg-core-surface"
           }
-        }
+        ]
       }
     )
   }
