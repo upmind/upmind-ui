5c331451b50a0760e84a59accff153b6ef29dff0
Rhodri Jones <rhodri.jones@upmind.com>
2025-10-16 13:50:06 +0100

    stablepoint tokens

---
+++ playgrounds/labs/src/assets/themes/stablepoint/index.ts
@@ -0,0 +1,10 @@
+import { type Theme } from "@upmind-automation/upmind-ui";
+import uiConfig from "./ui.config";
+import tokens from "./tokens.css?raw";
+
+export default {
+  name: "stablepoint",
+  id: "stablepoint",
+  uiConfig,
+  tokens
+} as Theme;
