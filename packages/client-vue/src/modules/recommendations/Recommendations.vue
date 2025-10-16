533d604b1548410b100d7c481fe48e8adaac2239
Rhodri Jones <rhodri.jones@upmind.com>
2025-10-16 16:42:35 +0100

    replace basket length with basket count

--- packages/client-vue/src/modules/recommendations/Recommendations.vue
+++ packages/client-vue/src/modules/recommendations/Recommendations.vue
@@ -48,7 +48,7 @@
           <div
             class="text-md order-last mt-4 text-center font-medium md:order-first md:mt-0 md:text-left"
           >
-            {{ t("cart.basket_items", { count: products?.length }) }}
+            {{ t("cart.basket_items", { count: count ?? 0 }) }}
           </div>
 
           <Button
@@ -92,7 +92,7 @@ const { navigateNext, isResolved } = useRoutingEngine();
 
 await isResolved(ROUTE.RECOMMENDATIONS);
 
-const { products } = useBasket();
+const { count } = useBasket();
 const {
   seen,
   isReady,
