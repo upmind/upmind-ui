ee47fd9a88d034d66d38396d0d9d0f5fbea3e8f6
Rhodri Jones <rhodri.jones@upmind.com>
2025-10-20 19:07:36 +0100

    two column LTR cva

--- src/ui/layout/TwoColumnLTR.vue
+++ src/ui/layout/TwoColumnLTR.vue
@@ -1,93 +1,130 @@
 <template>
-  <article class="bg-surface lg:canvas-gradient w-full">
-    <header class="flex w-full justify-center">
-      <div
-        class="basis-app-content bg-surface box-content min-w-0 shrink px-8 pb-1 lg:px-18"
-      >
-        <div class="lg:w-app-content flex h-[100px] items-end">
+  <article :class="styles.twoColumnLTR.root">
+    <header :class="styles.twoColumnLTR.header.row">
+      <div :class="styles.twoColumnLTR.header.contentWrapper">
+        <div :class="styles.twoColumnLTR.header.contentInner">
           <slot name="header-left" />
         </div>
       </div>
 
-      <div
-        class="basis-app-aside bg-surface box-content min-w-0 shrink px-8 pb-1 lg:bg-transparent lg:px-18"
-      >
-        <div class="lg:w-app-aside flex h-[100px] items-end justify-end">
+      <div :class="styles.twoColumnLTR.header.asideWrapper">
+        <div :class="styles.twoColumnLTR.header.asideInner">
           <slot name="header-right" />
         </div>
       </div>
     </header>
 
-    <section class="flex w-full justify-center">
-      <div
-        class="basis-app-content bg-surface box-content min-w-0 shrink px-8 pt-18 lg:px-18"
-      >
-        <div class="w-app-content">
+    <section :class="styles.twoColumnLTR.contentHeader.row">
+      <div :class="styles.twoColumnLTR.contentHeader.contentWrapper">
+        <div :class="styles.twoColumnLTR.contentHeader.contentInner">
           <slot name="content-header" />
         </div>
       </div>
 
-      <div
-        class="basis-app-aside box-content hidden min-w-0 shrink px-18 pt-18 lg:block"
-      >
-        <div class="w-app-aside" />
+      <div :class="styles.twoColumnLTR.contentHeader.asideWrapper">
+        <div :class="styles.twoColumnLTR.contentHeader.asideInner" />
       </div>
     </section>
 
-    <section class="flex w-full justify-center">
-      <div
-        class="basis-app-content bg-surface box-content min-w-0 shrink px-8 pt-18 lg:px-18"
-      >
-        <div class="w-app-content flex flex-col gap-12">
+    <section :class="styles.twoColumnLTR.content.row">
+      <div :class="styles.twoColumnLTR.content.contentWrapper">
+        <div :class="styles.twoColumnLTR.content.contentInner">
           <slot name="content" />
         </div>
       </div>
 
-      <div
-        class="basis-app-aside box-content hidden min-w-0 shrink px-18 pt-9 lg:block"
-      >
-        <aside class="w-app-aside sticky top-0 pt-9">
+      <div :class="styles.twoColumnLTR.content.asideWrapper">
+        <aside :class="styles.twoColumnLTR.content.asideInner">
           <slot name="aside" />
         </aside>
       </div>
     </section>
 
-    <div class="flex w-full justify-center">
-      <div
-        class="basis-app-content bg-surface box-content min-w-0 shrink px-8 lg:px-18"
-      >
-        <div class="w-app-content h-11" />
+    <div :class="styles.twoColumnLTR.spacer.row">
+      <div :class="styles.twoColumnLTR.spacer.contentWrapper">
+        <div :class="styles.twoColumnLTR.spacer.contentInner" />
       </div>
 
-      <div
-        class="basis-app-aside box-content hidden min-w-0 shrink px-18 lg:block"
-      >
-        <div class="w-app-aside h-11" />
+      <div :class="styles.twoColumnLTR.spacer.asideWrapper">
+        <div :class="styles.twoColumnLTR.spacer.asideInner" />
       </div>
     </div>
 
-    <div class="flex w-full items-end justify-center">
-      <div
-        class="basis-app-content bg-surface box-content min-w-0 shrink px-8 pb-9 lg:px-18 lg:pt-18"
-      >
-        <div class="w-app-content">
+    <footer :class="styles.twoColumnLTR.footer.row">
+      <div :class="styles.twoColumnLTR.footer.contentWrapper">
+        <div :class="styles.twoColumnLTR.footer.contentInner">
           <slot name="footer" />
         </div>
       </div>
 
-      <div
-        class="basis-app-aside box-content min-w-0 shrink px-18 pb-9 lg:pt-18"
-      >
-        <div class="w-app-aside">
+      <div :class="styles.twoColumnLTR.footer.asideWrapper">
+        <div :class="styles.twoColumnLTR.footer.asideInner">
           <slot name="footer-aside" />
         </div>
       </div>
-    </div>
+    </footer>
   </article>
 </template>
 
 <script lang="ts" setup>
-import type { VariantProps } from "./types";
+import { computed, type ComputedRef } from "vue";
+import { useStyles } from "../../../utils";
+import config from "../layout.config";
+import type { VariantProps } from "../types";
 
 defineProps<VariantProps>();
+
+const meta = computed(() => ({}));
+
+const styles = useStyles(
+  [
+    "twoColumnLTR",
+    "twoColumnLTR.header",
+    "twoColumnLTR.contentHeader",
+    "twoColumnLTR.content",
+    "twoColumnLTR.spacer",
+    "twoColumnLTR.footer"
+  ],
+  meta,
+  config
+) as ComputedRef<{
+  twoColumnLTR: {
+    root: string;
+    header: {
+      row: string;
+      contentWrapper: string;
+      contentInner: string;
+      asideWrapper: string;
+      asideInner: string;
+    };
+    contentHeader: {
+      row: string;
+      contentWrapper: string;
+      contentInner: string;
+      asideWrapper: string;
+      asideInner: string;
+    };
+    content: {
+      row: string;
+      contentWrapper: string;
+      contentInner: string;
+      asideWrapper: string;
+      asideInner: string;
+    };
+    spacer: {
+      row: string;
+      contentWrapper: string;
+      contentInner: string;
+      asideWrapper: string;
+      asideInner: string;
+    };
+    footer: {
+      row: string;
+      contentWrapper: string;
+      contentInner: string;
+      asideWrapper: string;
+      asideInner: string;
+    };
+  };
+}>;
 </script>
