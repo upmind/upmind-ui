974a81770d1255ef8e9fbfe582ef1eab3a7e4301
Rhodri Jones <rhodri.jones@upmind.com>
2025-10-20 18:29:03 +0100

    mobile designs

--- src/ui/layout/TwoColumnLTR.vue
+++ src/ui/layout/TwoColumnLTR.vue
@@ -1,51 +1,67 @@
 <template>
-  <article class="canvas-gradient w-full">
+  <article class="bg-surface lg:canvas-gradient w-full">
     <header class="flex w-full justify-center">
-      <div class="basis-app-content box-content bg-surface min-w-0 shrink px-18 pb-1">
-        <div class="w-app-content flex h-[100px] items-end">
+      <div
+        class="basis-app-content bg-surface box-content min-w-0 shrink px-8 pb-1 lg:px-18"
+      >
+        <div class="lg:w-app-content flex h-[100px] items-end">
           <slot name="header-left" />
         </div>
       </div>
 
-      <div class="basis-app-aside box-content min-w-0 shrink px-18 pb-1">
-        <div class="w-app-aside flex h-[100px] items-end justify-end">
+      <div
+        class="basis-app-aside bg-surface box-content min-w-0 shrink px-8 pb-1 lg:bg-transparent lg:px-18"
+      >
+        <div class="lg:w-app-aside flex h-[100px] items-end justify-end">
           <slot name="header-right" />
         </div>
       </div>
     </header>
 
     <section class="flex w-full justify-center">
-      <div class="basis-app-content box-content bg-surface min-w-0 shrink px-18 pt-18">
+      <div
+        class="basis-app-content bg-surface box-content min-w-0 shrink px-8 pt-18 lg:px-18"
+      >
         <div class="w-app-content">
           <slot name="content-header" />
         </div>
       </div>
 
-      <div class="basis-app-aside box-content min-w-0 shrink px-18 pt-18">
-        <div class="w-app-aside h-4" />
+      <div
+        class="basis-app-aside box-content hidden min-w-0 shrink px-18 pt-18 lg:block"
+      >
+        <div class="w-app-aside" />
       </div>
     </section>
 
     <section class="flex w-full justify-center">
-      <div class="basis-app-content box-content bg-surface min-w-0 shrink px-18 pt-18">
+      <div
+        class="basis-app-content bg-surface box-content min-w-0 shrink px-8 pt-18 lg:px-18"
+      >
         <div class="w-app-content flex flex-col gap-12">
           <slot name="content" />
         </div>
       </div>
 
-      <div class="basis-app-aside box-content min-w-0 shrink px-18 pt-18">
-        <aside class="w-app-aside sticky top-0">
+      <div
+        class="basis-app-aside box-content hidden min-w-0 shrink px-18 pt-9 lg:block"
+      >
+        <aside class="w-app-aside sticky top-0 pt-9">
           <slot name="aside" />
         </aside>
       </div>
     </section>
 
     <footer class="flex w-full justify-center">
-      <div class="basis-app-content box-content bg-surface min-w-0 shrink px-18">
+      <div
+        class="basis-app-content bg-surface box-content min-w-0 shrink px-8 lg:px-18"
+      >
         <div class="w-app-content h-32" />
       </div>
 
-      <div class="basis-app-aside box-content min-w-0 shrink px-18">
+      <div
+        class="basis-app-aside box-content hidden min-w-0 shrink px-18 lg:block"
+      >
         <div class="w-app-aside h-32" />
       </div>
     </footer>
@@ -57,15 +73,3 @@ import type { VariantProps } from "./types";
 
 defineProps<VariantProps>();
 </script>
-
-<style scoped>
-.canvas-gradient {
-  background:
-    linear-gradient(to right, var(--color-core-surface) 50%, transparent 50%),
-    linear-gradient(
-      to bottom right,
-      var(--color-primary-default) 0%,
-      var(--color-primary-default-stop) 100%
-    );
-}
-</style>
