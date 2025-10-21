700262af8aae6ea477e3b670353055c5d23222fd
Rhodri Jones <rhodri.jones@upmind.com>
2025-10-21 09:05:04 +0100

    Split out footer

--- packages/client-vue/src/components/footer/Footer.vue
+++ packages/client-vue/src/components/footer/Footer.vue
@@ -17,58 +17,18 @@
       <section
         class="flex flex-col justify-between gap-2 text-center md:flex-row md:gap-0 md:text-left"
       >
-        <div
-          class="text-muted flex w-full items-center justify-center gap-2 text-sm md:justify-start"
-        >
-          <img
-            v-if="upmindLogo"
-            :src="upmindLogo"
-            alt="Upmind logo"
-            width="16"
-            height="16"
-          />
-          <p>
-            {{ t("text.powered_by") }}
-            <Link
-              size="inherit"
-              color="inherit"
-              href="https://upmind.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              class="inline-block underline underline-offset-2"
-              >Upmind</Link
-            >
-          </p>
-        </div>
+        <PoweredByUpmind />
 
-        <p class="text-muted text-sm whitespace-nowrap">
-          {{
-            t("text.copyright", {
-              year: new Date().getFullYear(),
-              name
-            })
-          }}
-        </p>
+        <Copyright />
       </section>
     </div>
   </footer>
 </template>
 
 <script lang="ts" setup>
-// --- external
-import { useI18n } from "vue-i18n";
-
-// --- internal
-import { useBrand } from "@upmind-automation/headless";
-
 // --- components
 import UpmCurrency from "../../modules/basket/components/CurrencySwitcher.vue";
 import UpmLocale from "../../components/LocaleSwitcher.vue";
-import { Link } from "@upmind-automation/upmind-ui";
-
-// --- assets
-import upmindLogo from "../../assets/mark.svg";
-
-const { t } = useI18n();
-const { name } = useBrand();
+import PoweredByUpmind from "./PoweredByUpmind.vue";
+import Copyright from "./Copyright.vue";
 </script>
