cadee72e1dbff97aa2a4d0ab11cd936c9d9692eb
Rhodri Jones <rhodri.jones@upmind.com>
2025-10-22 13:29:59 +0100

    module login templates

---
+++ packages/client-vue/src/modules/session/LoginLTR.vue
@@ -0,0 +1,24 @@
+<template>
+  <Layout :variant="LAYOUT_VARIANTS.TWO_COLUMN_LTR" :currency="false">
+    <template #content-header>
+      <LoginHeader />
+    </template>
+
+    <template #content>
+      <LoginForm />
+    </template>
+
+    <template #aside>
+      <LoginAside />
+    </template>
+  </Layout>
+</template>
+
+<script lang="ts" setup>
+import { LAYOUT_VARIANTS } from "@upmind-automation/upmind-ui";
+import Layout from "../../components/layout/Layout.vue";
+import LoginHeader from "./components/login/LoginHeader.vue";
+import LoginForm from "./components/login/LoginForm.vue";
+import LoginAside from "./components/login/LoginAside.vue";
+</script>
+
