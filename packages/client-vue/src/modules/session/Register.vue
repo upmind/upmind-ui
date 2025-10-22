6ab421c4a31e0858ecb8d43e9c9a62007ca83a51
Rhodri Jones <rhodri.jones@upmind.com>
2025-10-22 16:50:46 +0100

    login and register views restrict supported templates

--- packages/client-vue/src/modules/session/Register.vue
+++ packages/client-vue/src/modules/session/Register.vue
@@ -1,9 +1,30 @@
 <template>
-  <RegisterSplit />
+  <component :is="templateVariant" />
 </template>
 
 <script lang="ts" setup>
+// --- external
+import { computed } from "vue";
+
+// --- components
 import RegisterLTR from "./register/RegisterLTR.vue";
 import RegisterRTL from "./register/RegisterRTL.vue";
 import RegisterSplit from "./register/RegisterSplit.vue";
+import RegisterCanvasCard from "./register/RegisterCanvasCard.vue";
+
+// --- types
+import { LAYOUT_VARIANTS } from "@upmind-automation/upmind-ui";
+
+const variant = LAYOUT_VARIANTS.CANVAS_CARD;
+
+const supportedTemplates = {
+  [LAYOUT_VARIANTS.SPLIT]: RegisterSplit,
+  [LAYOUT_VARIANTS.CANVAS_CARD]: RegisterCanvasCard,
+  [LAYOUT_VARIANTS.TWO_COLUMN_LTR]: RegisterLTR,
+  [LAYOUT_VARIANTS.TWO_COLUMN_RTL]: RegisterRTL
+};
+
+const templateVariant = computed(
+  () => supportedTemplates[variant] ?? RegisterSplit
+);
 </script>
