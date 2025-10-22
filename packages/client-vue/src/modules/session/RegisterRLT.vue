8f0c9613c80bbfdc4f0abdb26ddc2b907e66d4f2
Rhodri Jones <rhodri.jones@upmind.com>
2025-10-22 13:14:07 +0100

    modular register templates

---
+++ packages/client-vue/src/modules/session/RegisterRLT.vue
@@ -0,0 +1,23 @@
+<template>
+  <Layout :variant="LAYOUT_VARIANTS.TWO_COLUMN_RTL" :currency="false">
+    <template #content-header>
+      <RegisterHeader />
+    </template>
+
+    <template #content>
+      <RegisterForm />
+    </template>
+
+    <template #aside>
+      <RegisterAside />
+    </template>
+  </Layout>
+</template>
+
+<script lang="ts" setup>
+import { LAYOUT_VARIANTS } from "@upmind-automation/upmind-ui";
+import Layout from "../../components/layout/Layout.vue";
+import RegisterHeader from "./components/register/RegisterHeader.vue";
+import RegisterForm from "./components/register/RegisterForm.vue";
+import RegisterAside from "./components/register/RegisterAside.vue";
+</script>
---
+++ packages/client-vue/src/modules/session/components/register/RegisterHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <Header :title="t('action.create_account')">
+    <template #description>
+      <span class="font-normal"
+        >{{ t("auth.already_have_account_qn") }}&nbsp;</span
+      >
+
+      <Link
+        :to="{ name: ROUTE.SESSION_LOGIN }"
+        size="inherit"
+        :label="t('action.log_in_here')"
+      />
+    </template>
+  </Header>
+</template>
+
+<script lang="ts" setup>
+import { useI18n } from "vue-i18n";
+import { ROUTE } from "@upmind-automation/headless";
+import { Link } from "@upmind-automation/upmind-ui";
+import Header from "../../../../components/content/Header.vue";
+
+const { t } = useI18n();
+</script>
