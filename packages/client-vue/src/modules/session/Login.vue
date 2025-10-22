c34201b65f487e5f3b815cf35ac1c1f82ba0c26e
Rhodri Jones <rhodri.jones@upmind.com>
2025-10-22 15:18:14 +0100

    regiaster RTL correct naming

--- packages/client-vue/src/modules/session/Login.vue
+++ packages/client-vue/src/modules/session/Login.vue
@@ -1,7 +1,8 @@
 <template>
-  <LoginLTR />
+  <LoginRTL />
 </template>
 
 <script lang="ts" setup>
 import LoginLTR from "./login/LoginLTR.vue";
+import LoginRTL from "./login/LoginRTL.vue";
 </script>
