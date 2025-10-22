55ed4725266ecb4a2c907bd6bb591966230cafee
Rhodri Jones <rhodri.jones@upmind.com>
2025-10-22 15:18:14 +0100

    regiaster RTL correct naming

--- packages/client-vue/src/modules/session/Register.vue
+++ packages/client-vue/src/modules/session/Register.vue
@@ -1,7 +1,8 @@
 <template>
-  <RegisterLTR />
+  <RegisterRTL />
 </template>
 
 <script lang="ts" setup>
 import RegisterLTR from "./register/RegisterLTR.vue";
+import RegisterRTL from "./register/RegisterRTL.vue";
 </script>
