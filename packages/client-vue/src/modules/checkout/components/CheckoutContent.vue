c9b06e090011773420d430240d7c125addef6586
Rhodri Jones <rhodri.jones@upmind.com>
2025-10-23 12:46:59 +0100

    checkout templates and components

---
+++ packages/client-vue/src/modules/checkout/components/CheckoutContent.vue
@@ -0,0 +1,91 @@
+<template>
+  <!-- Basket Errors -->
+  <BasketErrors
+    id="basket-errors"
+    :basket-billing="!uischema.showBillingOnCheckout"
+    :basket-fields="!uischema.showFieldsOnCheckout"
+    :basket-products="!uischema.showProductsOnCheckout"
+  />
+
+  <!-- Basket Products -->
+  <Section
+    id="basket-products"
+    :show="section"
+    :title="t('cart.basket_products')"
+    v-show="uischema.showProductsOnCheckout"
+  >
+    <ProductCards />
+  </Section>
+
+  <!-- Additional Options -->
+  <Section
+    id="basket-fields"
+    :show="section"
+    :title="t('text.additional_details')"
+    v-show="uischema.showFieldsOnCheckout"
+  >
+    <Form
+      v-if="!fieldsMeta.isLoading"
+      :additional-errors="fieldsErrors?.data"
+      :model-value="fieldsModel"
+      :schema="fieldsSchema"
+      :uischema="fieldsUischema"
+      @reject="fieldsClear"
+      @resolve="fieldsUpdate"
+      @update:modelValue="fieldsUpdate"
+      no-actions
+      autosave
+      :touched="meta.showErrors"
+    />
+  </Section>
+
+  <!-- Billing Details -->
+  <Section
+    id="basket-billing"
+    :show="section"
+    :title="t('text.billing_details')"
+    v-show="uischema.showBillingOnCheckout"
+  >
+    <BillingDetails :touched="meta.showErrors" />
+  </Section>
+
+  <!-- Payment Details -->
+  <Section
+    id="payment-details"
+    :show="section"
+    :title="t('text.payment_details')"
+  >
+    <PaymentDetails />
+  </Section>
+</template>
+
+<script lang="ts" setup>
+import { useI18n } from "vue-i18n";
+import { useBasket, useBasketFields } from "@upmind-automation/headless";
+import Section from "../../../components/content/LayoutSection.vue";
+import BillingDetails from "../../billing/Billing.vue";
+import PaymentDetails from "./PaymentDetails.vue";
+import ProductCards from "../../basket/product/BasketProductCards.vue";
+import Form from "../../../components/form/Form.vue";
+import BasketErrors from "../../basket/components/BasketErrors.vue";
+
+interface Props {
+  section?: boolean;
+}
+
+withDefaults(defineProps<Props>(), {
+  section: false
+});
+
+const { t } = useI18n();
+const { meta, uischema } = useBasket();
+const {
+  errors: fieldsErrors,
+  meta: fieldsMeta,
+  model: fieldsModel,
+  schema: fieldsSchema,
+  uischema: fieldsUischema,
+  clear: fieldsClear,
+  update: fieldsUpdate
+} = useBasketFields();
+</script>
---
+++ packages/client-vue/src/modules/checkout/components/CheckoutHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <Header
+    :title="t('text.secure_checkout')"
+    :description="
+      t('cart.basket_summary_desc', {
+        count: products?.length ?? 0,
+        total: summary?.total ?? 0
+      })
+    "
+    :badge="{
+      label: t('text.fully_encrypted_title'),
+      icon: 'lock-04'
+    }"
+  />
+</template>
+
+<script lang="ts" setup>
+import { useI18n } from "vue-i18n";
+import { useBasket } from "@upmind-automation/headless";
+import Header from "../../../components/content/Header.vue";
+
+const { t } = useI18n();
+const { summary, products } = useBasket();
+</script>
