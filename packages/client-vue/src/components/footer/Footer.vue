75ab7181a74c6ca7c12e41c1139fa07f9dc8534d
Dominic Da Costa <dominic@thecollaboration.studio>
2025-10-24 11:19:13 +0200

    refactor(footer): enhance footer component logic and improve template handling

--- packages/client-vue/src/components/footer/Footer.vue
+++ packages/client-vue/src/components/footer/Footer.vue
@@ -1,25 +1,15 @@
 <template>
-  <component :is="templateVariant">
-    <template
-      #footer-actions
-      v-if="!config.state.noLocale || !config.state.noCurrency"
-    >
-      <UpmLocale data-testid="locale-selector" v-if="!config.state.noLocale" />
-
-      <UpmCurrency
-        data-testid="currency-selector"
-        v-if="!config.state.noCurrency"
-      />
+  <component :is="template" v-show="meta.isVisible">
+    <template #footer-actions v-if="meta.hasActions">
+      <UpmLocale data-testid="locale-selector" v-show="meta.showLocale" />
+      <UpmCurrency data-testid="currency-selector" v-show="meta.showCurrency" />
     </template>
 
-    <template
-      #footer-content
-      v-if="!config.state.noLogo || !config.state.noPoweredBy"
-    >
+    <template #footer-content v-if="meta.hasContent">
       <Content />
     </template>
 
-    <template #footer-copyright v-if="!config.state.noCopyright">
+    <template #footer-copyright v-if="meta.showCopyright">
       <Copyright />
     </template>
   </component>
@@ -27,15 +17,9 @@
 
 <script lang="ts" setup>
 // --- external
-import { computed } from "vue";
 
 // --- internal
 import { useFooter } from "./useFooter";
-
-// --- templates
-import FooterFlat from "./templates/FooterFlat.template.vue";
-import FooterStacked from "./templates/FooterStacked.template.vue";
-import FooterNoOptions from "./templates/FooterNoOptions.template.vue";
 
 // --- components
 import Content from "./components/Content.vue";
@@ -44,38 +28,18 @@ import UpmCurrency from "../../modules/basket/components/CurrencySwitcher.vue";
 import UpmLocale from "../../components/LocaleSwitcher.vue";
 
 // --- internal
-import { useRoutingEngine } from "@upmind-automation/headless";
-import { FOOTER_TEMPLATE } from "./types";
-
-const { currentRoute } = useRoutingEngine();
-
-const meta = computed(() => ({
-  variant: currentRoute.value?.meta?.template as FOOTER_TEMPLATE
-}));
-
-const supportedTemplates = {
-  [FOOTER_TEMPLATE.DEFAULT]: FooterStacked,
-  [FOOTER_TEMPLATE.ENCLOSED]: FooterStacked,
-  [FOOTER_TEMPLATE.FULL]: FooterStacked,
-  [FOOTER_TEMPLATE.TWO_COLUMN_LTR]: FooterFlat,
-  [FOOTER_TEMPLATE.TWO_COLUMN_RTL]: FooterFlat,
-  [FOOTER_TEMPLATE.SPLIT]: FooterNoOptions,
-  [FOOTER_TEMPLATE.CANVAS_CARD]: FooterNoOptions,
-  [FOOTER_TEMPLATE.SURFACE_BOX]: FooterNoOptions
-};
-
-const templateVariant = computed(
-  () => supportedTemplates[meta.value.variant] ?? FooterStacked
-);
+import { FOOTER_TEMPLATE, type FooterProps } from "./types";
 
 // -----------------------------------------------------------------------------
-const props = defineProps<{
-  visible?: boolean;
-  template?: FOOTER_TEMPLATE;
-  noLocale?: boolean;
-  noCurrency?: boolean;
-  noAttribution?: boolean;
-}>();
+const props = withDefaults(defineProps<FooterProps>(), {
+  visible: undefined,
+  template: undefined,
+  noLocale: undefined,
+  noCurrency: undefined,
+  noCopyright: undefined,
+  noLogo: undefined,
+  noPoweredBy: undefined
+});
 
-const { config } = useFooter(props);
+const { meta, template } = useFooter(props);
 </script>
