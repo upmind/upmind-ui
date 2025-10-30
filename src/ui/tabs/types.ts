e9765d06cdd7aa10af85f3a842cd0870e90e51a7
RhodriJonesUpmind <rhodri.jones@upmind.com>
2025-10-30 14:04:37 +0000

    tabs redesign (#54)
    * tabs redesign
    
    * render singular tab without button bloat

--- src/ui/tabs/types.ts
+++ src/ui/tabs/types.ts
@@ -1,30 +1,21 @@
 // --- external
 import { type HTMLAttributes } from "vue";
-import type { CxOptions, VariantProps } from "class-variance-authority";
+import type { CxOptions } from "class-variance-authority";
 import type {
   TabsRootProps,
-  TabsListProps,
-  TabsTriggerProps,
-  TabsContentProps
+  TabsListProps
 } from "radix-vue";
-
-// --- internal
-import type { tabsListVariants, tabsTriggerVariants } from "./tabs.config";
-type TabsTriggerVariantProps = VariantProps<typeof tabsTriggerVariants>;
-type TabsListVariantProps = VariantProps<typeof tabsListVariants>;
 
 export interface TabItem {
   label: string;
   value: string;
+  icon?: string;
   eager?: boolean;
 }
 
 export interface TabsProps extends TabsRootProps, TabsListProps {
   tabs: TabItem[];
   defaultValue?: string;
-  // ---
-  alignment?: TabsListVariantProps["alignment"];
-  width?: TabsListVariantProps["width"];
   // ---
   uiConfig?: {
     tabs: {
