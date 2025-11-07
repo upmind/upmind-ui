bc3aef5d7505fee22fa8033cfb45808a793e7a78
Dominik Piska <piska.web@gmail.com>
2025-11-07 13:59:55 +0000

    Add Account routes

---
+++ apps/portal/src/router/account/index.ts
@@ -0,0 +1,57 @@
+import { ROUTE } from "../types";
+
+export default [
+  {
+    path: "/account",
+    name: ROUTE.ACCOUNT,
+    redirect: { name: ROUTE.ACCOUNT_PROFILE }
+  },
+  {
+    path: "/account/profile",
+    name: ROUTE.ACCOUNT_PROFILE,
+    component: () => import("../../views/account/profile/Profile.vue")
+  },
+  {
+    path: "/account/child-accounts",
+    name: ROUTE.ACCOUNT_CHILD_ACCOUNTS,
+    component: () =>
+      import("../../views/account/childAccounts/ChildAccounts.vue")
+  },
+  {
+    path: "/account/delegates",
+    name: ROUTE.ACCOUNT_DELEGATES,
+    component: () => import("../../views/account/delegates/Delegates.vue")
+  },
+  {
+    path: "/account/delegates/:delegateId",
+    // path: "/account/delegates/:delegateId(${RegexMatch.UUID})",
+    name: ROUTE.ACCOUNT_DELEGATES_DELEGATE,
+    component: () => import("../../views/account/delegates/Delegate.vue")
+  },
+  {
+    path: "/account/notifications",
+    name: ROUTE.ACCOUNT_NOTIFICATIONS,
+    component: () =>
+      import("../../views/account/notifications/Notifications.vue")
+  },
+  {
+    path: "/account/email-history",
+    name: ROUTE.ACCOUNT_EMAIL_HISTORY,
+    component: () => import("../../views/account/emailHistory/EmailHistory.vue")
+  },
+  {
+    path: "/account/security",
+    name: ROUTE.ACCOUNT_SECURITY,
+    component: () => import("../../views/account/security/Security.vue")
+  },
+  {
+    path: "/account/affiliate",
+    name: ROUTE.ACCOUNT_AFFILIATE,
+    component: () => import("../../views/account/affiliate/Affiliate.vue")
+  },
+  {
+    path: "/account/notes",
+    name: ROUTE.ACCOUNT_NOTES,
+    component: () => import("../../views/account/notes/Notes.vue")
+  }
+];
---
+++ apps/portal/src/views/account/profile/Profile.vue
@@ -0,0 +1,92 @@
+<template>
+  <Layout :variant="layout">
+    <template #navigation>ss</template>
+
+    <template #default>
+      <pre>{{ currentRoute }}</pre>
+      <p>Profile</p>
+    </template>
+
+    <template #aside>ss</template>
+  </Layout>
+</template>
+<script lang="ts" setup>
+// --- external
+import {
+  // ref,
+  computed
+} from "vue";
+// import { useI18n } from "vue-i18n";
+// import { useRoute } from "vue-router";
+import { ROUTE } from "../../../router/types";
+// --- internal
+import {
+  // useBasket,
+  // useBasketFields,
+  // useDataLayer,
+  // useBrand,
+  useRoutingEngine
+} from "@upmind-automation/headless";
+// import { useStyles } from "@upmind-automation/upmind-ui";
+// import config from "./basket.config";
+
+// --- components
+import { Layout } from "@upmind-automation/client-vue";
+
+// --- types
+// import { type ComputedRef } from "vue";
+
+// -----------------------------------------------------------------------------
+
+// const { t } = useI18n();
+// const { meta, isReady, count, summary } = useBasket();
+// const { storefrontRoute } = useBrand();
+const { currentRoute, isReady, isResolved } = useRoutingEngine();
+// const route = useRoute();
+
+// const {
+//   errors: fieldsErrors,
+//   meta: fieldsMeta,
+//   model: fieldsModel,
+//   schema: fieldsSchema,
+//   uischema: fieldsUischema,
+//   clear: fieldsClear,
+//   update: fieldsUpdate
+// } = useBasketFields();
+
+// const open = ref(false);
+
+// await isReady();
+await isReady();
+await isResolved(ROUTE.ACCOUNT_PROFILE);
+
+const layout = computed(() => {
+  return currentRoute.value?.meta?.template;
+});
+
+console.log("layout", currentRoute.value);
+
+// const variant = "enclosed";
+
+// const styles = useStyles(
+//   ["basket.expand", "basket.items", "basket.customFields", "basket.aside"],
+//   { variant: layout.value },
+//   config
+// ) as ComputedRef<{
+//   basket: {
+//     aside: string;
+//     expand: string;
+//     items: {
+//       root: string;
+//       content: string;
+//     };
+//     customFields: {
+//       root: string;
+//     };
+//   };
+// }>;
+
+// const { dataLayer } = useDataLayer();
+
+// dataLayer({ event: "view_cart" }).withEcommerce().push();
+</script>
