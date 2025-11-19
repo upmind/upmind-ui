351e9a68e71c15436c744960ffa296a2d56052d5
Rhodri Jones <rhodri.jones@upmind.com>
2025-11-19 10:03:36 +0000

    tab variants

--- src/ui/tabs/tabs.config.ts
+++ src/ui/tabs/tabs.config.ts
@@ -1,24 +1,26 @@
 import { cva } from "class-variance-authority";
 
+export const variants = {
+  overflow: {
+    hidden: "overflow-x-auto overflow-y-hidden",
+    visible: ""
+  },
+  hasBorder: {
+    true: "border-surface mb-9 border-b",
+    false: ""
+  },
+  align: {
+    left: "justify-start",
+    center: "justify-center",
+    between: "justify-between",
+    right: "justify-end"
+  }
+};
+
 export const tabsRootVariants = cva(
   "relative flex items-end [scrollbar-width:none] [&::-webkit-scrollbar]:hidden",
   {
-    variants: {
-      overflow: {
-        hidden: "overflow-x-auto overflow-y-hidden",
-        visible: ""
-      },
-      hasBorder: {
-        true: "border-surface mb-9 border-b",
-        false: ""
-      },
-      align: {
-        left: "justify-start",
-        center: "justify-center",
-        between: "justify-between",
-        right: "justify-end"
-      }
-    },
+    variants,
     defaultVariants: {
       align: "left",
       hasBorder: true,
