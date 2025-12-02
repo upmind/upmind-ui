a2bbecc091a9a1c518529d496eb46e2ed984137d
Rhodri Jones <rhodri.jones@upmind.com>
2025-12-02 11:47:52 +0000

    combobox tweak

--- src/ui/select/select.config.ts
+++ src/ui/select/select.config.ts
@@ -58,7 +58,7 @@ export default {
       }
     ),
     content: cva(
-      `control-radius shadow-control-default bg-control-surface ring-primitive-control-default! my-1 border-none ${ringClasses} ${invalidRingClasses} p-0`
+      `control-radius shadow-control-default bg-control-surface ring-primitive-control-default! my-2 border-none ${ringClasses} ${invalidRingClasses} p-0`
     )
   }
 };
