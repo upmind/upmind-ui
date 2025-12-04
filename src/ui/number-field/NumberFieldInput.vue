06e88641c1cf7c3874c490906396139861b27225
Rhodri Jones <rhodri.jones@upmind.com>
2025-12-04 13:04:21 +0000

    number field minimal updated designs

--- src/ui/number-field/NumberFieldInput.vue
+++ src/ui/number-field/NumberFieldInput.vue
@@ -27,7 +27,7 @@ const handleResize = (event: InputEvent) => {
     @input="handleResize"
     :class="
       cn(
-        'border-input bg-background placeholder:text-muted-foreground flex h-10 w-full rounded-lg border py-2 text-center text-sm',
+        'border-input placeholder:text-muted-foreground flex h-10 w-full rounded-lg border py-2 text-center text-sm',
         props.class
       )
     "
