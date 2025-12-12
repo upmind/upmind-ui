f3fb034ab92874484ae051a70b2847c38315ae2a
Dominic Da Costa <dominic@thecollaboration.studio>
2025-12-12 16:30:42 +0200

    Fix unsetParam function to handle router availability and update coupon parameter retrieval

--- packages/headless/src/modules/routing/useQueryParams.ts
+++ packages/headless/src/modules/routing/useQueryParams.ts
@@ -101,9 +101,25 @@ export const useQueryParams = (route?: RouteLocation) => {
   }
 
   function unsetParam(type: string) {
+    if (router) {
+      router.replace({
+        query: {
+          ...router.currentRoute.value.query,
+          [type]: undefined
+        }
+      });
+      return;
+    }
+
+    // fallback for when router is not available
     const url = new URL(window.location.toString());
     const cleanedUrl = new URL(window.location?.href);
     cleanedUrl.searchParams.delete(type);
+    // console.debug("unsetParam", type, {
+    //   url: url.toString(),
+    //   cleanedUrl: cleanedUrl.toString()
+    // });
+
     if (!isEqual(cleanedUrl.searchParams, url.searchParams)) {
       history.replaceState(history.state, "", cleanedUrl);
     }
@@ -203,7 +219,7 @@ export const useQueryParams = (route?: RouteLocation) => {
       QUERY_PARAMS.CURRENCY,
       consumeParam(QUERY_PARAMS.CURRENCY_CODE)
     ),
-    coupon: consumeParam(QUERY_PARAMS.COUPONS),
+    coupon: getParam(QUERY_PARAMS.COUPONS),
     bundle: consumeParam("bundle")
   };
 };
