4112a8c2f50b425a4c2ab38b971121c72d953820
Dominic Da Costa <dominic@thecollaboration.studio>
2025-12-16 17:05:54 +0200

    refactor: improve session component logic and enhance routing state management

--- packages/client-vue/src/modules/session/Login.vue
+++ packages/client-vue/src/modules/session/Login.vue
@@ -1,5 +1,5 @@
 <template>
-  <component v-if="routingMeta.isResolved" :is="templateVariant" v-bind="props">
+  <component :is="templateVariant" v-bind="props" v-if="!isResolving">
     <template #back>
       <slot name="back">
         <Back />
@@ -65,7 +65,7 @@
 
 <script lang="ts" setup>
 // --- external
-import { computed, defineAsyncComponent, onUnmounted } from "vue";
+import { computed, defineAsyncComponent, onUnmounted, ref } from "vue";
 import { useI18n } from "vue-i18n";
 
 // --- internal
@@ -141,6 +141,8 @@ const {
 
 await isReady();
 
+const isResolving = ref(false);
+
 const templateVariant = computed(() =>
   get(
     supportedTemplates,
@@ -163,11 +165,17 @@ function doUpdate(value: SessionProps["modelValue"]) {
 }
 
 function doReject() {
-  navigateBack();
+  isResolving.value = true;
+  navigateBack().catch(() => {
+    isResolving.value = false;
+  });
 }
 
 function doResolve() {
-  navigateNext();
+  isResolving.value = true;
+  navigateNext().catch(() => {
+    isResolving.value = false;
+  });
 }
 
 onUnmounted(() => {
