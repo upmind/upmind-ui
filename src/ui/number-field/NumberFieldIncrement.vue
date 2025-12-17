8abdc7c12cf50c16caf482558738956c8f6522ed
Rhodri Jones <rhodri.jones@upmind.com>
2025-12-17 13:39:47 +0000

    number field cursor

--- src/ui/number-field/NumberFieldIncrement.vue
+++ src/ui/number-field/NumberFieldIncrement.vue
@@ -24,7 +24,7 @@ const forwarded = useForwardProps(delegatedProps);
     v-bind="forwarded"
     :class="
       cn(
-        'absolute top-1/2 right-0 flex -translate-y-1/2 items-center justify-center p-3 disabled:cursor-not-allowed disabled:opacity-20',
+        'absolute top-1/2 right-0 flex -translate-y-1/2 cursor-pointer items-center justify-center p-3 disabled:cursor-not-allowed disabled:opacity-20',
         props.class
       )
     "
