a24e7c28b26680bb9f23ccdb47c3c3b4991bbca6
Rhodri Jones <rhodri.jones@upmind.com>
2026-01-06 15:08:53 +0000

    fix product image on safari

--- packages/client-vue/src/modules/product/components/hero/ProductHero.vue
+++ packages/client-vue/src/modules/product/components/hero/ProductHero.vue
@@ -30,6 +30,7 @@
       :style="{ '--details-h': `${height}px` }"
     >
       <ProductImage
+        :class="styles.header.image"
         :product-details="props.productDetails"
         :images="props.productDetails?.images"
       />
@@ -78,6 +79,7 @@ const styles = useStyles(["header"], stylesMeta, config) as ComputedRef<{
     description: string;
     price: string;
     aside: string;
+    image: string;
   };
 }>;
 const images = computed(() => {
