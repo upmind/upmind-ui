4caced6445cabbc1b41f907a5bb42ff4c143f72a
Rhodri Jones <rhodri.jones@upmind.com>
2026-01-23 16:21:04 +0000

    fix gap

--- src/ui/table/table.config.ts
+++ src/ui/table/table.config.ts
@@ -4,7 +4,7 @@ import { cva } from "class-variance-authority";
 // -----------------------------------------------------------------------------
 export default {
   table: {
-    root: cva("w-full border-separate border-spacing-y-4"),
+    root: cva("w-full"),
     header: {
       root: cva(""),
       cell: cva(
@@ -15,7 +15,7 @@ export default {
     row: {
       root: cva(""),
       cell: cva(
-        "pl-6 text-right font-normal whitespace-nowrap first:w-full first:pl-0 first:text-left data-[emphasis=true]:font-medium"
+        "py-2 pl-6 text-right font-normal whitespace-nowrap first:w-full first:pl-0 first:text-left data-[emphasis=true]:font-medium"
       )
     }
   }
