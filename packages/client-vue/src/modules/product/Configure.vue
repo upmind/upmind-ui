7abb3d889a0785cffe0ffb3772e1c051ae719bc3
Rhodri Jones <rhodri.jones@upmind.com>
2026-02-17 16:26:50 +0000

    RTL template

--- packages/client-vue/src/modules/product/Configure.vue
+++ packages/client-vue/src/modules/product/Configure.vue
@@ -319,7 +319,7 @@ const template = computed(() =>
   validateTemplate(
     configMeta.ui.template.value || props.template,
     PRODUCT_TEMPLATE,
-    PRODUCT_TEMPLATE.TWO_COLUMN_LTR
+    PRODUCT_TEMPLATE.TWO_COLUMN_RTL
   )
 );
 
