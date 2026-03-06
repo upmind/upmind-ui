a40ea914221750c85acfbd3e079a7e6f05e9bb9a
Rhodri Jones <rhodri.jones@upmind.com>
2026-03-06 09:09:42 +0000

    Revert "deprecate subproduct card"
    This reverts commit 6fca1b5f238887c59859e8ef80a4665e78296a85.

---
+++ packages/client-vue/src/modules/product/components/subproduct/subproduct-card.config.ts
@@ -0,0 +1,85 @@
+import { cva } from "class-variance-authority";
+
+// -----------------------------------------------------------------------------
+export default {
+  card: {
+    root: cva("m-0 flex w-full flex-col gap-1 font-normal"),
+    header: {
+      root: cva("flex flex-1 flex-wrap content-start items-start gap-2"),
+      content: cva("flex grow flex-col gap-0.5 md:flex-row md:gap-x-2"),
+      titleWrapper: cva("flex flex-wrap items-center gap-2"),
+      titleInner: cva("flex items-start gap-2"),
+      title: cva("m-0", {
+        variants: {
+          isMinimal: {
+            true: ""
+          },
+          isDropdown: {
+            true: "text-muted group-hover/item:text-control-selected text-control-selected group-hover/item:font-medium group-data-[state=checked]/item:font-medium",
+            false: "font-medium"
+          }
+        },
+        compoundVariants: [
+          {
+            isMinimal: true,
+            isDropdown: false,
+            class: "text-md-tight"
+          },
+          {
+            isMinimal: false,
+            isDropdown: false,
+            class: "text-md font-medium"
+          }
+        ]
+      }),
+      tooltip: cva("control-radius max-w-72 text-center text-xs"),
+      trigger: cva("ml-1 inline-flex h-lh items-center align-top"),
+      icon: cva(
+        "text-muted hover:text-control-selected cursor-help transition-colors duration-300"
+      ),
+      actions: cva("flex items-center gap-2")
+    },
+    pricing: {
+      sm: cva("items-center gap-x-1 max-md:flex md:hidden", {
+        variants: {
+          isDropdown: {
+            true: "font-normal group-hover/item:font-medium group-data-[state=checked]/item:font-medium",
+            false: "font-medium"
+          }
+        }
+      }),
+      lg: cva("hidden flex-col gap-2 text-right md:flex md:flex-row", {
+        variants: {
+          isDropdown: {
+            true: "text-control-selected font-normal group-hover/item:font-medium group-data-[state=checked]/item:font-medium",
+            false: "font-medium"
+          }
+        }
+      }),
+      text: cva("", {
+        variants: {
+          isDropdown: {
+            true: "text-sm-tight text-control-selected font-normal transition-all duration-300 group-hover/item:font-medium group-data-[state=checked]/item:font-medium",
+            false: "text-md font-medium"
+          }
+        }
+      })
+    },
+    excerpt: cva("text-sm-tight whitespace-normal", {
+      variants: {
+        isDropdown: {
+          false: "text-muted",
+          true: "text-muted"
+        }
+      }
+    }),
+    image: {
+      root: cva(
+        "flex h-6 min-h-6 w-6 min-w-6 shrink-0 items-center justify-start"
+      ),
+      img: cva(
+        "inline-block h-5 min-h-5 w-5 min-w-5 object-cover object-center"
+      )
+    }
+  }
+};
