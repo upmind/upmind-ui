d164ccb21194d0b2dc67351965c77233ced0061f
Rhodri Jones <rhodri.jones@upmind.com>
2026-03-13 11:49:01 +0000

    layout overflow housekeeping

--- packages/client-vue/src/components/layout/components/root/root.config.ts
+++ packages/client-vue/src/components/layout/components/root/root.config.ts
@@ -1,5 +1,12 @@
 import { cva } from "class-variance-authority";
 
 export default {
-  root: cva("flex w-full grow flex-col")
+  root: cva("flex w-full grow flex-col", {
+    variants: {
+      overflow: {
+        hidden: "overflow-hidden",
+        visible: "overflow-visible"
+      }
+    }
+  })
 };
