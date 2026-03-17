b3abe9b440625fa890a84f02ca17143d7ca9ba03
Dominic da Costa <dominic@thecollaboration.studio>
2026-03-17 10:55:41 +0200

    feat(FE-1365): add useOverlayRoute composable

--- packages/headless/src/modules/routing/index.ts
+++ packages/headless/src/modules/routing/index.ts
@@ -1,6 +1,7 @@
 export * from "./useRoutingEngine";
 export * from "./useQueryParams";
 export * from "./useProductsRequiringAction";
+export * from "./useOverlayRoute";
 export * from "./overlays";
 export * from "./types";
 export * from "./utils";
