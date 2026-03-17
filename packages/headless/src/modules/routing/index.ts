8e86d8496bfdede1a20103f6b68de651228e06ec
Dominic da Costa <dominic@thecollaboration.studio>
2026-03-17 10:54:04 +0200

    feat(FE-1365): add overlay route types and registerOverlayRoutes utility

--- packages/headless/src/modules/routing/index.ts
+++ packages/headless/src/modules/routing/index.ts
@@ -1,5 +1,6 @@
 export * from "./useRoutingEngine";
 export * from "./useQueryParams";
 export * from "./useProductsRequiringAction";
+export * from "./overlays";
 export * from "./types";
 export * from "./utils";
