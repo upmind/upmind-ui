313129082d15d17f317bf6db35192db076d47a55
Dominic da Costa <dominic@thecollaboration.studio>
2026-03-26 15:16:03 +0200

    expose radix slot

--- src/ui/interstitial/index.ts
+++ src/ui/interstitial/index.ts
@@ -2,6 +2,7 @@
 import { defineCustomElement } from "vue";
 // --- vue elements
 export { default as Interstitial } from "./Interstitial.ce.vue";
+export { Slot } from "radix-vue";
 export * from "./types";
 // --- custom elements
 import Interstitial from "./Interstitial.ce.vue";
