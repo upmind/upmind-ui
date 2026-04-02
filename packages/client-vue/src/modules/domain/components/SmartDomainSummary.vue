3698b32351d820f96394f59fd1455b3cea88bb40
Rhodri Jones <rhodri.jones@upmind.com>
2026-04-02 14:31:26 +0100

    smart domain summary housekeeping

--- packages/client-vue/src/modules/domain/components/SmartDomainSummary.vue
+++ packages/client-vue/src/modules/domain/components/SmartDomainSummary.vue
@@ -1,20 +1,15 @@
 <template>
-  <div :class="styles.field.summary.root">
+  <section :class="styles.field.summary.root">
     <RadioGroup model-value="selected">
       <div :class="styles.field.summary.row">
-        <div :class="styles.field.summary.indicator">
-          <RadioGroupItem value="selected" />
-        </div>
-        <div :class="styles.field.summary.content">
-          <span :class="styles.field.summary.domain">{{ domain }}</span>
-
-          <Link :class="styles.field.summary.change" @click="emit('change')">
-            {{ t("action.change") }}
-          </Link>
-        </div>
+        <RadioGroupItem value="selected" />
+        <span :class="styles.field.summary.domain">{{ domain }}</span>
+        <Link :class="styles.field.summary.change" @click="emit('change')">
+          {{ t("action.change") }}
+        </Link>
       </div>
     </RadioGroup>
-  </div>
+  </section>
 </template>
 
 <script setup lang="ts">
@@ -29,12 +24,11 @@ import {
   Link
 } from "@upmind-automation/upmind-ui";
 import config from "../smartDomainField.config";
+import type { SmartDomainSummaryProps } from "../types";
 
 // -----------------------------------------------------------------------------
 
-defineProps<{
-  domain: string;
-}>();
+defineProps<SmartDomainSummaryProps>();
 
 const emit = defineEmits<{
   (e: "change"): void;
