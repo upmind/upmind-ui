9931c8a7242a6fbe4250e59ec73ec59e75e03b34
Rhodri Jones <rhodri.jones@upmind.com>
2026-04-09 15:18:41 +0100

    tweak subproduct card pricing styles to match designs and move to cva

--- packages/client-vue/src/modules/product/components/subproduct/SubproductCardPricing.vue
+++ packages/client-vue/src/modules/product/components/subproduct/SubproductCardPricing.vue
@@ -5,13 +5,16 @@
       :monthly-from-regular-price="props.price?.monthlyFromRegularPrice ?? ''"
       :discounted="props.meta?.discounted ?? false"
       :overridden="props.meta?.overridden"
+      :ui-config="{
+        pricing: { ex: [styles.card.pricing.ex] }
+      }"
     />
 
     <Tooltip
       v-if="props.meta?.overrides && props.price"
       :label="t('text.overrides_price')"
     >
-      <span class="flex items-center justify-center hover:cursor-help">
+      <span :class="styles.card.pricing.current">
         <Icon icon="switch-horizontal-01" size="nano" class="text-inherit" />
         <CurrentPrice
           :class="props.class"
@@ -21,7 +24,7 @@
           "
           :free="props.meta?.free ?? false"
           :ui-config="{
-            pricing: { current: ['text-md-tight'] }
+            pricing: { current: [styles.card.pricing.current] }
           }"
         />
         <span
@@ -38,7 +41,7 @@
     </Tooltip>
 
     <Tooltip v-else :label="t('text.adds_to_price')">
-      <span class="flex items-center hover:cursor-help">
+      <span :class="styles.card.pricing.current">
         <span>+</span>
         <CurrentPrice
           :class="props.class"
@@ -48,7 +51,7 @@
           "
           :free="props.meta?.free ?? false"
           :ui-config="{
-            pricing: { current: ['text-md-tight'] }
+            pricing: { current: [styles.card.pricing.current] }
           }"
         />
         <span
@@ -67,12 +70,16 @@
 </template>
 
 <script setup lang="ts">
+import { computed } from "vue";
 import { useI18n } from "vue-i18n";
-import { Icon, Tooltip } from "@upmind-automation/upmind-ui";
+import { Icon, Tooltip, useStyles } from "@upmind-automation/upmind-ui";
 import ExPrice from "../pricing/ExPrice.vue";
 import CurrentPrice from "../pricing/CurrentPrice.vue";
+import config from "./subproduct-card.config";
 import { lowerCase } from "lodash-es";
 import type { SubproductValue } from "@upmind-automation/headless";
+
+// -----------------------------------------------------------------------------
 
 const props = defineProps<{
   price: SubproductValue["price"];
@@ -80,7 +87,14 @@ const props = defineProps<{
   cycle: SubproductValue["cycle"];
   term?: number;
   class?: string;
+  dropdown?: boolean;
 }>();
 
 const { t } = useI18n();
+
+const pricingMeta = computed(() => ({
+  isDropdown: !!props.dropdown
+}));
+
+const styles = useStyles(["card.pricing"], pricingMeta, config);
 </script>
