95b7bb523ad583a734563f06cdd149d2e661c9f3
Rhodri Jones <rhodri.jones@upmind.com>
2026-04-13 18:03:02 +0100

    billing sticky footer

--- packages/client-vue/src/modules/billing/templates/BillingEnclosed.template.vue
+++ packages/client-vue/src/modules/billing/templates/BillingEnclosed.template.vue
@@ -6,10 +6,15 @@
 
     <template #content>
       <slot name="content" />
+      <slot v-if="isMobile" name="content-footer" />
     </template>
 
     <template #aside>
       <slot name="markdown" />
+    </template>
+
+    <template v-if="!isMobile" #content-footer>
+      <slot name="content-footer" />
     </template>
   </Layout>
 </template>
@@ -26,6 +31,9 @@ import { useSection } from "../../../components/section/useSection";
 
 // --- components
 import Layout from "../../../components/layout/Layout.vue";
+
+// --- utils
+import { isMobile } from "@upmind-automation/upmind-ui";
 
 // --- types
 import { HEADER_BACKGROUND } from "../../../components/header/types";
