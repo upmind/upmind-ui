441e44ada7c6135e80e0ea66ed09bb88f037968a
Rhodri Jones <rhodri.jones@upmind.com>
2026-04-14 13:17:05 +0100

    billing layout improvements

--- packages/client-vue/src/modules/billing/billing.config.ts
+++ packages/client-vue/src/modules/billing/billing.config.ts
@@ -5,7 +5,10 @@ import { cva } from "class-variance-authority";
 export default {
   billing: {
     form: {
-      sections: cva("min-h-32"),
+      sections: cva("min-h-32")
+    },
+    loading: {
+      root: cva("max-w-3xl"),
       spinner: cva("z-10! w-full rounded")
     },
     card: {
