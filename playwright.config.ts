0b76e31705953a6808f16472707654a1d40d2f7e
Nathan Robinson <nathan.robinson@upmind.com>
2026-04-24 08:52:13 +0100

    qa: testing docs and increase retirews in config

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   //captureGitInfo: { commit: true, diff: true },
 
   /*Set number of retries on a failed test*/
-  retries: 0,
+  retries: 1,
 
   /* Run tests in files in parallel */
   //fullyParallel: true,
@@ -86,7 +86,7 @@ export default defineConfig({
       name: "firefox",
       use: {
         ...devices["Desktop Firefox"],
-        browserName: "chromium",
+        browserName: "firefox",
         headless: true,
         viewport: { width: 1920, height: 1080 },
         launchOptions: {
@@ -98,7 +98,7 @@ export default defineConfig({
       name: "safari",
       use: {
         ...devices["Desktop Safari"],
-        browserName: "chromium",
+        browserName: "webkit",
         headless: true,
         viewport: { width: 1920, height: 1080 },
         launchOptions: {
