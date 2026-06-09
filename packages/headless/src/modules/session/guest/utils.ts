d7a1787b1394393767b123f2d8651418fbdf3392
Rhodri Jones <rhodri.jones@upmind.com>
2026-06-09 15:49:51 +0100

    i18n

--- packages/headless/src/modules/session/guest/utils.ts
+++ packages/headless/src/modules/session/guest/utils.ts
@@ -220,10 +220,10 @@ export const useLoginUischemaParser = () => {
       {
         type: "Control",
         scope: "#/properties/username",
-        i18n: "form.auth_email",
+        i18n: "form.auth_login",
         options: {
           autoFocus: true,
-          autocomplete: "email",
+          autocomplete: "username",
           placeholder: "name@email.com"
         }
       },
@@ -332,10 +332,10 @@ export const useRecoverUischemaParser = () => {
       {
         type: "Control",
         scope: "#/properties/username",
-        i18n: "form.auth_email",
+        i18n: "form.auth_login",
         options: {
           autoFocus: true,
-          autocomplete: "email",
+          autocomplete: "username",
           placeholder: "name@email.com"
         }
       }
