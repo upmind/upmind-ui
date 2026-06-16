551113b2d49fab61aa59f07e2a8226f03bc85ada
Dominic da Costa <dominic@thecollaboration.studio>
2026-06-16 14:12:25 +0200

    fix(ui): format data-testid binding for improved readability

--- src/ui/link/Link.ce.vue
+++ src/ui/link/Link.ce.vue
@@ -5,7 +5,10 @@
     :aria-disabled="meta.isDisabled || undefined"
     :tabindex="meta.isFocusable ? '0' : '-1'"
     :class="cn(styles.link.root, props.class)"
-    :data-testid="props.dataAttrs?.['data-testid'] ?? `link-${kebabCase(label ?? 'default')}`"
+    :data-testid="
+      props.dataAttrs?.['data-testid'] ??
+      `link-${kebabCase(label ?? 'default')}`
+    "
     @click="$emit('click', $event)"
   >
     <slot name="prepend">
